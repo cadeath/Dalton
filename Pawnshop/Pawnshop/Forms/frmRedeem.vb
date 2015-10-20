c5fb9490a432caac5de5c62e4d483a7f42d1ea2c
haxored123 <fxi.frances@gmail.com>
2015-10-20 11:13:37 +0800

    Pawning

---
+++ Pawnshop/Pawnshop/Forms/frmRedeem.vb
@@ -0,0 +1,15 @@
+﻿Public Class frmRedeem
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        txtAppraisal.ReadOnly = False
+        Me.Show()
+    End Sub
+
+    Private Sub frmRedeem_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Receiptdate.Value = Date.Now
+    End Sub
+End Class
