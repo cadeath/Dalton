ef464ee7d2dc27fe2af01dcf6eee9d8225f32dad
cadeath <ecjmaquic@gmail.com>
2015-11-05 11:05:11 +0800

    update

--- Pawnshop/Pawnshop/Forms/frmBorrowing.Designer.vb
+++ Pawnshop/Pawnshop/Forms/frmBorrowing.Designer.vb
@@ -25,14 +25,12 @@ Partial Class frmBorrowing
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmBorrowing))
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtParticulars = New System.Windows.Forms.TextBox()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.cboBranch = New System.Windows.Forms.ComboBox()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.txtStreet = New System.Windows.Forms.TextBox()
-        Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.txtAmount = New System.Windows.Forms.TextBox()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
@@ -57,64 +55,45 @@ Partial Class frmBorrowing
         Me.Button1.Text = "&Cancel"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'TextBox1
+        'txtParticulars
         '
-        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox1.Location = New System.Drawing.Point(93, 100)
-        Me.TextBox1.Multiline = True
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(431, 106)
-        Me.TextBox1.TabIndex = 26
-        Me.TextBox1.Text = resources.GetString("TextBox1.Text")
+        Me.txtParticulars.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtParticulars.Location = New System.Drawing.Point(93, 100)
+        Me.txtParticulars.Multiline = True
+        Me.txtParticulars.Name = "txtParticulars"
+        Me.txtParticulars.Size = New System.Drawing.Size(431, 106)
+        Me.txtParticulars.TabIndex = 26
+        Me.txtParticulars.Text = resources.GetString("txtParticulars.Text")
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label4.Location = New System.Drawing.Point(9, 100)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(78, 18)
+        Me.Label4.Size = New System.Drawing.Size(71, 16)
         Me.Label4.TabIndex = 25
         Me.Label4.Text = "Particulars"
         '
-        'ComboBox1
+        'cboBranch
         '
-        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(94, 40)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(185, 26)
-        Me.ComboBox1.TabIndex = 24
-        Me.ComboBox1.Text = "BRANCH HERE"
+        Me.cboBranch.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboBranch.FormattingEnabled = True
+        Me.cboBranch.Location = New System.Drawing.Point(94, 40)
+        Me.cboBranch.Name = "cboBranch"
+        Me.cboBranch.Size = New System.Drawing.Size(185, 24)
+        Me.cboBranch.TabIndex = 24
+        Me.cboBranch.Text = "BRANCH HERE"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label3.Location = New System.Drawing.Point(10, 43)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(55, 18)
+        Me.Label3.Size = New System.Drawing.Size(50, 16)
         Me.Label3.TabIndex = 23
         Me.Label3.Text = "Branch"
-        '
-        'txtStreet
-        '
-        Me.txtStreet.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtStreet.Location = New System.Drawing.Point(94, 9)
-        Me.txtStreet.Name = "txtStreet"
-        Me.txtStreet.Size = New System.Drawing.Size(126, 22)
-        Me.txtStreet.TabIndex = 22
-        Me.txtStreet.Text = "Oct. 21, 2015"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(10, 10)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(39, 18)
-        Me.Label2.TabIndex = 21
-        Me.Label2.Text = "Date"
         '
         'Label1
         '
@@ -126,22 +105,22 @@ Partial Class frmBorrowing
         Me.Label1.TabIndex = 29
         Me.Label1.Text = "Borrowings"
         '
-        'TextBox2
+        'txtAmount
         '
-        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox2.Location = New System.Drawing.Point(94, 72)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(126, 22)
-        Me.TextBox2.TabIndex = 31
-        Me.TextBox2.Text = "10000"
+        Me.txtAmount.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAmount.Location = New System.Drawing.Point(94, 72)
+        Me.txtAmount.Name = "txtAmount"
+        Me.txtAmount.Size = New System.Drawing.Size(126, 22)
+        Me.txtAmount.TabIndex = 31
+        Me.txtAmount.Text = "10000"
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label5.Location = New System.Drawing.Point(10, 73)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(59, 18)
+        Me.Label5.Size = New System.Drawing.Size(53, 16)
         Me.Label5.TabIndex = 30
         Me.Label5.Text = "Amount"
         '
@@ -161,17 +140,15 @@ Partial Class frmBorrowing
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(535, 247)
         Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.txtAmount)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtParticulars)
         Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.cboBranch)
         Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.txtStreet)
-        Me.Controls.Add(Me.Label2)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
         Me.Name = "frmBorrowing"
@@ -182,14 +159,12 @@ Partial Class frmBorrowing
     End Sub
     Friend WithEvents Button2 As System.Windows.Forms.Button
     Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtParticulars As System.Windows.Forms.TextBox
     Friend WithEvents Label4 As System.Windows.Forms.Label
-    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents cboBranch As System.Windows.Forms.ComboBox
     Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents txtStreet As System.Windows.Forms.TextBox
-    Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtAmount As System.Windows.Forms.TextBox
     Friend WithEvents Label5 As System.Windows.Forms.Label
     Friend WithEvents Button3 As System.Windows.Forms.Button
 End Class
---
+++ Pawnshop/Pawnshop/Modules/security.vb
@@ -0,0 +1,17 @@
+﻿Imports encapsulatedserialkey.clsSerialize
+
+Module security
+
+    Friend Function HashMe(ByVal src As String) As String
+        Return HashMe(src)
+    End Function
+
+    Friend Function Encrypt(ByVal src As String) As String
+        Return Encrypt(src)
+    End Function
+
+    Friend Function Decrypt(ByVal src As String) As String
+        Return Decrypt(src)
+    End Function
+
+End Module
