fb253bd00726eaf14e465ad46cf46544923fd382
cadeath <ecjmaquic@gmail.com>
2015-11-21 17:28:41 +0800

    Fixing
    Add Tables
    Displaying Accounts at the drop down box
    adding POST function
    auto change the label when click the buttons

--- Pawnshop/Pawnshop/Forms/frmCashInOut.Designer.vb
+++ Pawnshop/Pawnshop/Forms/frmCashInOut.Designer.vb
@@ -32,11 +32,13 @@ Partial Class frmCashInOut
         Me.cboTransactions = New System.Windows.Forms.ComboBox()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.btnCashOut = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtAmount = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'lblTrans
         '
-        Me.lblTrans.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.lblTrans.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.lblTrans.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblTrans.Location = New System.Drawing.Point(283, 15)
         Me.lblTrans.Name = "lblTrans"
@@ -58,38 +60,39 @@ Partial Class frmCashInOut
         'btnPost
         '
         Me.btnPost.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnPost.Location = New System.Drawing.Point(346, 204)
+        Me.btnPost.Location = New System.Drawing.Point(345, 222)
         Me.btnPost.Name = "btnPost"
         Me.btnPost.Size = New System.Drawing.Size(88, 29)
-        Me.btnPost.TabIndex = 4
+        Me.btnPost.TabIndex = 5
         Me.btnPost.Text = "&Post"
         Me.btnPost.UseVisualStyleBackColor = True
         '
         'btnCancel
         '
         Me.btnCancel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnCancel.Location = New System.Drawing.Point(440, 204)
+        Me.btnCancel.Location = New System.Drawing.Point(439, 222)
         Me.btnCancel.Name = "btnCancel"
         Me.btnCancel.Size = New System.Drawing.Size(88, 29)
-        Me.btnCancel.TabIndex = 5
+        Me.btnCancel.TabIndex = 6
         Me.btnCancel.Text = "&Cancel"
         Me.btnCancel.UseVisualStyleBackColor = True
         '
         'txtParticulars
         '
         Me.txtParticulars.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtParticulars.Location = New System.Drawing.Point(97, 92)
+        Me.txtParticulars.Location = New System.Drawing.Point(96, 110)
         Me.txtParticulars.Multiline = True
         Me.txtParticulars.Name = "txtParticulars"
+        Me.txtParticulars.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
         Me.txtParticulars.Size = New System.Drawing.Size(431, 106)
-        Me.txtParticulars.TabIndex = 3
+        Me.txtParticulars.TabIndex = 4
         Me.txtParticulars.Text = resources.GetString("txtParticulars.Text")
         '
         'Label4
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(13, 92)
+        Me.Label4.Location = New System.Drawing.Point(12, 110)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(78, 18)
         Me.Label4.TabIndex = 17
@@ -125,11 +128,30 @@ Partial Class frmCashInOut
         Me.btnCashOut.Text = "Cash Out"
         Me.btnCashOut.UseVisualStyleBackColor = True
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 84)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 18)
+        Me.Label1.TabIndex = 18
+        Me.Label1.Text = "Amount"
+        '
+        'txtAmount
+        '
+        Me.txtAmount.Location = New System.Drawing.Point(96, 84)
+        Me.txtAmount.Name = "txtAmount"
+        Me.txtAmount.Size = New System.Drawing.Size(199, 20)
+        Me.txtAmount.TabIndex = 3
+        '
         'frmCashInOut
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(539, 243)
+        Me.ClientSize = New System.Drawing.Size(539, 262)
+        Me.Controls.Add(Me.txtAmount)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.btnCashOut)
         Me.Controls.Add(Me.btnPost)
         Me.Controls.Add(Me.btnCancel)
@@ -156,4 +178,6 @@ Partial Class frmCashInOut
     Friend WithEvents cboTransactions As System.Windows.Forms.ComboBox
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents btnCashOut As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtAmount As System.Windows.Forms.TextBox
 End Class
--- Pawnshop/Pawnshop/Forms/frmCashInOut.vb
+++ Pawnshop/Pawnshop/Forms/frmCashInOut.vb
@@ -4,6 +4,7 @@
     Dim cashOUT As New Hashtable 'DISBURSEMENT
 
     Private mainTransaction As Boolean = True 'True Cash IN|False Cash OUT
+    Private fillData As String = "tblCashTrans"
 
     Private Sub frmCashInOut_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         ClearFields()
@@ -13,6 +14,7 @@
 
     Private Sub ClearFields()
         cboTransactions.Items.Clear()
+        txtAmount.Text = ""
         txtParticulars.Text = ""
     End Sub
 
@@ -81,7 +83,28 @@
         LoadTransType()
     End Sub
 
-    Private Sub cboTransactions_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cboTransactions.SelectedIndexChanged
+    Private Sub txtAmount_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtAmount.KeyPress
+        DigitOnly(e)
+    End Sub
 
+    Private Sub btnPost_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPost.Click
+
+    End Sub
+
+    Private Sub Save()
+        Dim mySql As String = "SELECT * FROM " & fillData
+        Dim ds As DataSet
+
+        'Get Variables
+        Dim cashID As Integer, tmpSQL As String
+        tmpSQL = String.Format("SELECT * FROM tblCash WHERE TransName = '{0}' AND Type = '{1}'", cboTransactions.Text, IIf(mainTransaction, "RECEIPT", "DISBURSEMENT"))
+        ds = LoadSQL(tmpSQL)
+
+
+        Dim dsNewRow As DataRow
+        dsNewRow = ds.Tables(fillData).NewRow
+        With dsNewRow
+            .Item("CashID") = cashID
+        End With
     End Sub
 End Class
