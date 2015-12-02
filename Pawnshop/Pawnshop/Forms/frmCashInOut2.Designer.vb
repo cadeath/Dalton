031fabda4a0cfd3d1a1c3eb16d12a2c0d924756a
cadeath <ecjmaquic@gmail.com>
2015-12-02 14:19:08 +0800

    update

--- Pawnshop/Pawnshop/Forms/frmCashInOut2.Designer.vb
+++ Pawnshop/Pawnshop/Forms/frmCashInOut2.Designer.vb
@@ -22,28 +22,28 @@ Partial Class frmCashInOut2
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim ListViewItem2 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem(New String() {"1", "TICKETING", "Ticketing Fund Account", "1000"}, -1)
+        Dim ListViewItem1 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem(New String() {"1", "TICKETING", "Ticketing Fund Account", "1000"}, -1)
         Me.btnCashIn = New System.Windows.Forms.Button()
         Me.btnCashOut = New System.Windows.Forms.Button()
         Me.btnBrowse = New System.Windows.Forms.Button()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.cboCat = New System.Windows.Forms.ComboBox()
         Me.cboTrans = New System.Windows.Forms.ComboBox()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.cboCat = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.txtAmt = New System.Windows.Forms.TextBox()
-        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
         Me.txtParticulars = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtAmt = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.lvDetails = New System.Windows.Forms.ListView()
         Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.ColumnHeader5 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.btnAdd = New System.Windows.Forms.Button()
-        Me.btnRemove = New System.Windows.Forms.Button()
         Me.btnCancel = New System.Windows.Forms.Button()
         Me.btnPost = New System.Windows.Forms.Button()
         Me.GroupBox1.SuspendLayout()
@@ -93,26 +93,6 @@ Partial Class frmCashInOut2
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Transaction"
         '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(21, 19)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(68, 18)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Category"
-        '
-        'cboCat
-        '
-        Me.cboCat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.cboCat.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.cboCat.FormattingEnabled = True
-        Me.cboCat.Location = New System.Drawing.Point(113, 18)
-        Me.cboCat.Name = "cboCat"
-        Me.cboCat.Size = New System.Drawing.Size(245, 24)
-        Me.cboCat.TabIndex = 0
-        '
         'cboTrans
         '
         Me.cboTrans.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
@@ -133,6 +113,26 @@ Partial Class frmCashInOut2
         Me.Label2.TabIndex = 2
         Me.Label2.Text = "Transaction"
         '
+        'cboCat
+        '
+        Me.cboCat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboCat.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboCat.FormattingEnabled = True
+        Me.cboCat.Location = New System.Drawing.Point(113, 18)
+        Me.cboCat.Name = "cboCat"
+        Me.cboCat.Size = New System.Drawing.Size(245, 24)
+        Me.cboCat.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(21, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 18)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Category"
+        '
         'GroupBox2
         '
         Me.GroupBox2.Controls.Add(Me.btnRemove)
@@ -148,34 +148,25 @@ Partial Class frmCashInOut2
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Input"
         '
-        'Label3
+        'btnRemove
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(6, 16)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(59, 18)
-        Me.Label3.TabIndex = 1
-        Me.Label3.Text = "Amount"
+        Me.btnRemove.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRemove.Location = New System.Drawing.Point(419, 59)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(84, 37)
+        Me.btnRemove.TabIndex = 3
+        Me.btnRemove.Text = "&Remove"
+        Me.btnRemove.UseVisualStyleBackColor = True
         '
-        'txtAmt
+        'btnAdd
         '
-        Me.txtAmt.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtAmt.Location = New System.Drawing.Point(113, 12)
-        Me.txtAmt.Name = "txtAmt"
-        Me.txtAmt.Size = New System.Drawing.Size(97, 22)
-        Me.txtAmt.TabIndex = 0
-        Me.txtAmt.Text = "0.00"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(6, 41)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(78, 18)
-        Me.Label4.TabIndex = 3
-        Me.Label4.Text = "Particulars"
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(419, 16)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(84, 37)
+        Me.btnAdd.TabIndex = 2
+        Me.btnAdd.Text = "&Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
         '
         'txtParticulars
         '
@@ -187,13 +178,42 @@ Partial Class frmCashInOut2
         Me.txtParticulars.TabIndex = 1
         Me.txtParticulars.Text = "0.00"
         '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(6, 41)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(78, 18)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Particulars"
+        '
+        'txtAmt
+        '
+        Me.txtAmt.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAmt.Location = New System.Drawing.Point(113, 12)
+        Me.txtAmt.Name = "txtAmt"
+        Me.txtAmt.Size = New System.Drawing.Size(97, 22)
+        Me.txtAmt.TabIndex = 0
+        Me.txtAmt.Text = "0.00"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(6, 16)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 18)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Amount"
+        '
         'lvDetails
         '
         Me.lvDetails.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4, Me.ColumnHeader5})
         Me.lvDetails.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lvDetails.FullRowSelect = True
         Me.lvDetails.GridLines = True
-        Me.lvDetails.Items.AddRange(New System.Windows.Forms.ListViewItem() {ListViewItem2})
+        Me.lvDetails.Items.AddRange(New System.Windows.Forms.ListViewItem() {ListViewItem1})
         Me.lvDetails.Location = New System.Drawing.Point(102, 221)
         Me.lvDetails.Name = "lvDetails"
         Me.lvDetails.Size = New System.Drawing.Size(746, 254)
@@ -225,26 +245,6 @@ Partial Class frmCashInOut2
         '
         Me.ColumnHeader5.Text = "Particulars"
         Me.ColumnHeader5.Width = 206
-        '
-        'btnAdd
-        '
-        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnAdd.Location = New System.Drawing.Point(419, 16)
-        Me.btnAdd.Name = "btnAdd"
-        Me.btnAdd.Size = New System.Drawing.Size(84, 37)
-        Me.btnAdd.TabIndex = 2
-        Me.btnAdd.Text = "&Add"
-        Me.btnAdd.UseVisualStyleBackColor = True
-        '
-        'btnRemove
-        '
-        Me.btnRemove.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnRemove.Location = New System.Drawing.Point(419, 59)
-        Me.btnRemove.Name = "btnRemove"
-        Me.btnRemove.Size = New System.Drawing.Size(84, 37)
-        Me.btnRemove.TabIndex = 3
-        Me.btnRemove.Text = "&Remove"
-        Me.btnRemove.UseVisualStyleBackColor = True
         '
         'btnCancel
         '
--- Pawnshop/Pawnshop/Forms/frmCashInOut2.vb
+++ Pawnshop/Pawnshop/Forms/frmCashInOut2.vb
@@ -15,7 +15,45 @@
     End Sub
 
     Private Sub frmCashInOut2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ClearFields()
+        LoadTables()
+        cashInSetup()
+    End Sub
 
+    Private Sub UpdateCategory(ByVal ht As Hashtable)
+        cboCat.Items.Clear()
+        For Each el As DictionaryEntry In ht
+            cboCat.Items.Add(el.Value)
+        Next
+    End Sub
+
+    Private Sub UpdateTransaction(ByVal ht As Hashtable)
+        cboTrans.Items.Clear()
+        For Each el As DictionaryEntry In ht
+            cboTrans.Items.Add(el.Value)
+        Next
+    End Sub
+
+    Private Sub cashInSetup() Handles btnCashIn.Click
+        On Error Resume Next
+
+        cboCat.Items.Clear()
+        UpdateCategory(cashInCat)
+        cboCat.SelectedIndex = 0
+
+        cboTrans.Items.Clear()
+        UpdateTransaction(ciTrans(0))
+        cboTrans.SelectedIndex = 0
+
+        selectedType = "Receipt"
+    End Sub
+
+    Private Sub ClearFields()
+        cboCat.Items.Clear()
+        cboTrans.Items.Clear()
+        txtAmt.Text = ""
+        txtParticulars.Text = ""
+        lvDetails.Items.Clear()
     End Sub
 
     Private Sub LoadTables()
@@ -24,7 +62,7 @@
         'Load Categories
         mySql = "SELECT DISTINCT Category FROM " & fillData
         mySql &= String.Format(" WHERE Type = '{0}'", selectedType)
-        ds = LoadSQL(mySql)
+        ds = LoadSQL(mySql, fillData)
 
         cashInCat = New Hashtable
         Dim cnt As Integer = 0
@@ -35,33 +73,92 @@
         ds.Clear()
         mySql = "SELECT DISTINCT Category FROM " & fillData
         mySql &= " WHERE Type = 'Disbursement'"
-        ds = LoadSQL(mySql)
+        ds = LoadSQL(mySql, fillData)
         cashOutCat = New Hashtable
         cnt = 0
         For Each dr As DataRow In ds.Tables(fillData).Rows
-            cashInCat.Add(cnt, dr.Item("Category")) : cnt += 1
+            cashOutCat.Add(cnt, dr.Item("Category")) : cnt += 1
         Next
 
         'Load Transaction Names
         Dim MaxRow As Integer = 0, tmpType As String = "Receipt"
         cnt = 0
+        ReDim ciTrans(cashInCat.Count - 1)
         For Each el As DictionaryEntry In cashInCat
             mySql = "SELECT * FROM " & fillData
             mySql &= String.Format(" WHERE TYPE = '{0}' AND CATEGORY = '{1}'", tmpType, el.Value)
             ds.Clear()
-            ds = LoadSQL(mySql)
+            ds = LoadSQL(mySql, fillData)
             MaxRow = ds.Tables(fillData).Rows.Count - 1
-            ReDim ciTrans(MaxRow)
+            ciTrans(cnt) = New Hashtable
             For Each dr As DataRow In ds.Tables(fillData).Rows
                 ciTrans(cnt).Add(dr.Item("CashID"), dr.Item("TransName"))
             Next
             cnt += 1
         Next
-        tmpType = "Disbursement"
 
+        tmpType = "Disbursement"
+        cnt = 0
+        ReDim coTrans(cashOutCat.Count - 1)
+        For Each el As DictionaryEntry In cashOutCat
+            mySql = "SELECT * FROM " & fillData
+            mySql &= String.Format(" WHERE TYPE = '{0}' AND CATEGORY = '{1}'", tmpType, el.Value)
+            ds.Clear()
+            ds = LoadSQL(mySql, fillData)
+            MaxRow = ds.Tables(fillData).Rows.Count - 1
+            coTrans(cnt) = New Hashtable
+            For Each dr As DataRow In ds.Tables(fillData).Rows
+                coTrans(cnt).Add(dr.Item("CashID"), dr.Item("TransName"))
+            Next
+            cnt += 1
+        Next
+
+        'Display
+        cnt = 0
+        For Each el As DictionaryEntry In cashInCat
+            Console.WriteLine(el.Value & "=========================")
+            For Each y As DictionaryEntry In ciTrans(cnt)
+                Console.WriteLine(y.Value)
+            Next
+            cnt += 1
+        Next
+        Console.WriteLine("------======================= ===============-----------")
+        cnt = 0
+        For Each el As DictionaryEntry In cashOutCat
+            Console.WriteLine(el.Value & "=========================")
+            For Each y As DictionaryEntry In coTrans(cnt)
+                Console.WriteLine(y.Value)
+            Next
+            cnt += 1
+        Next
     End Sub
 
-    Private Sub SaveHash(ByVal ht As Hashtable, ByVal ds As DataSet)
+    Private Sub cboCat_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cboCat.SelectedIndexChanged
+        Dim idx As Integer = cboCat.SelectedIndex
+        Dim selectHT As New Hashtable
 
+        Select Case selectedType
+            Case "Receipt"
+                selectHT = ciTrans(idx)
+            Case "Disbursement"
+                selectHT = coTrans(idx)
+        End Select
+
+        UpdateTransaction(selectHT)
+        cboTrans.SelectedIndex = 0
+    End Sub
+
+    Private Sub btnCashOut_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCashOut.Click
+        On Error Resume Next
+
+        cboCat.Items.Clear()
+        UpdateCategory(cashOutCat)
+        cboCat.SelectedIndex = 0
+
+        cboTrans.Items.Clear()
+        UpdateTransaction(coTrans(0))
+        cboTrans.SelectedIndex = 0
+
+        selectedType = "Disbursement"
     End Sub
 End Class
