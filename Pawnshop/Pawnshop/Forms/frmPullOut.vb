66e583aed49929eb1812fa16cf91711ffe9f31da
cadeath <ecjmaquic@gmail.com>
2015-12-10 09:00:38 +0800

    Coding PullOut

---
+++ Pawnshop/Pawnshop/Forms/frmPullOut.vb
@@ -0,0 +1,12 @@
+﻿Public Class frmPullOut
+
+    Private fillData As String = "tblPawn"
+
+    Private Sub frmPullOut_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub LoadSegregated()
+
+    End Sub
+End Class
