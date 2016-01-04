fda6ec11ffff0b5c0d94066f68b544158c038065
cadeath <ecjmaquic@gmail.com>
2016-01-04 17:04:21 +0800

    UPDATE

--- Pawnshop/Pawnshop/Reports/frmReport.vb
+++ Pawnshop/Pawnshop/Reports/frmReport.vb
@@ -21,9 +21,44 @@ Public Class frmReport
 
     Friend Sub MultiDbSetReport(ByVal mySql As Dictionary(Of String, String), ByVal rptUrl As String, _
                                 Optional ByVal addPara As Dictionary(Of String, String) = Nothing, Optional ByVal hasUser As Boolean = True)
+        Dim dsName As String, ds As DataSet, cmd As String
 
-        Dim dsName As String
-        Dim ds As New DataSet
+        With rv_display
+            .ProcessingMode = ProcessingMode.Local
+            .LocalReport.ReportPath = rptUrl
+            .LocalReport.DataSources.Clear()
+
+            For Each el In mySql
+                dsName = el.Key : cmd = el.Value
+                Console.WriteLine(String.Format("{0}: {1}", dsName, cmd))
+
+                ds = LoadSQL(cmd, dsName)
+                Dim rDS As New ReportDataSource
+                rDS = New ReportDataSource(dsName, ds.Tables(dsName))
+                .LocalReport.DataSources.Add(rDS)
+            Next
+
+            If hasUser Then
+                Dim myPara As New ReportParameter
+                myPara.Name = "txtUsername"
+                If POSuser.UserName Is Nothing Then POSuser.UserName = "Sample Eskie"
+                myPara.Values.Add(POSuser.UserName)
+                rv_display.LocalReport.SetParameters(New ReportParameter() {myPara})
+            End If
+
+            If Not addPara Is Nothing Then
+                For Each nPara In addPara
+                    Dim tmpPara As New ReportParameter
+                    tmpPara.Name = nPara.Key
+                    tmpPara.Values.Add(nPara.Value)
+                    .LocalReport.SetParameters(New ReportParameter() {tmpPara})
+                Next
+            End If
+
+            .RefreshReport()
+        End With
+
+        Exit Sub
         With rv_display
             .ProcessingMode = ProcessingMode.Local
             .LocalReport.ReportPath = rptUrl
