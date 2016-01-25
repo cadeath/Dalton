832be9170d3a8618a0a2fc33849310d1e4958d58
cadeath <ecjmaquic@gmail.com>
2016-01-25 10:34:46 +0800

    update

--- Pawnshop/Pawnshop/Reports/qryCashInOut.vb
+++ Pawnshop/Pawnshop/Reports/qryCashInOut.vb
@@ -2,6 +2,13 @@
 
     Private Sub qryCashInOut_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
         LoadCategories()
+        Disable_Functions()
+    End Sub
+
+    Private Sub Disable_Functions()
+        chkIN.Enabled = False
+        chkOUT.Enabled = False
+        cboCategory.Enabled = False
     End Sub
 
     Private Sub LoadCategories()
@@ -29,12 +36,16 @@
         Dim stDate As Date = GetFirstDate(monCal.SelectionRange.Start)
         Dim enDate As Date = GetLastDate(monCal.SelectionRange.End)
 
-        Dim mySql As String, ds As DataSet, dsName As String, rptPath As String = "Reports\rpt_CashInOutSummary.rdlc"
+        Dim mySql As String, dsName As String, rptPath As String = "Reports\rpt_CashInOutSummary.rdlc"
         dsName = "dsCIO"
         mySql = "SELECT * FROM TBLCASHTRANS"
         mySql &= String.Format(" WHERE TransDate BETWEEN '{0}' AND '{1}'", stDate.ToShortDateString, enDate.ToShortDateString)
 
-        frmReport.ReportInit(mySql, dsName, rptPath, , False)
+        Dim addParameter As New Dictionary(Of String, String)
+        addParameter.Add("branchName", branchName)
+        addParameter.Add("txtMonthOf", "FOR THE MONTH OF " & monCal.SelectionStart.ToString("MMMM yyyy").ToUpper)
+
+        frmReport.ReportInit(mySql, dsName, rptPath, addParameter)
         frmReport.Show()
     End Sub
 End Class
