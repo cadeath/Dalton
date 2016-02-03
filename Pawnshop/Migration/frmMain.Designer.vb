e63a1e7b8c43ecd31cad3c3a0284865b42e2f925
cadeath <ecjmaquic@gmail.com>
2016-02-03 17:18:23 +0800

    Migration

---
+++ Pawnshop/Migration/frmMain.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtUrl = New System.Windows.Forms.TextBox()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.btnExtract = New System.Windows.Forms.Button()
+        Me.pbLoading = New System.Windows.Forms.ProgressBar()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(32, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Path:"
+        '
+        'txtUrl
+        '
+        Me.txtUrl.Location = New System.Drawing.Point(12, 25)
+        Me.txtUrl.Name = "txtUrl"
+        Me.txtUrl.Size = New System.Drawing.Size(332, 20)
+        Me.txtUrl.TabIndex = 1
+        Me.txtUrl.Text = "D:\cadeath\Documents\RPS\AIS_JUL2010a.FDB"
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(350, 23)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(39, 23)
+        Me.btnBrowse.TabIndex = 2
+        Me.btnBrowse.Text = "..."
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'btnExtract
+        '
+        Me.btnExtract.Location = New System.Drawing.Point(12, 51)
+        Me.btnExtract.Name = "btnExtract"
+        Me.btnExtract.Size = New System.Drawing.Size(88, 34)
+        Me.btnExtract.TabIndex = 3
+        Me.btnExtract.Text = "&Extract"
+        Me.btnExtract.UseVisualStyleBackColor = True
+        '
+        'pbLoading
+        '
+        Me.pbLoading.Location = New System.Drawing.Point(0, 37)
+        Me.pbLoading.Name = "pbLoading"
+        Me.pbLoading.Size = New System.Drawing.Size(398, 16)
+        Me.pbLoading.TabIndex = 4
+        Me.pbLoading.Visible = False
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Location = New System.Drawing.Point(12, 91)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(24, 13)
+        Me.lblStatus.TabIndex = 5
+        Me.lblStatus.Text = "Idle"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(398, 113)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.pbLoading)
+        Me.Controls.Add(Me.btnExtract)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.txtUrl)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.Text = "Database Extractor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtUrl As System.Windows.Forms.TextBox
+    Friend WithEvents btnBrowse As System.Windows.Forms.Button
+    Friend WithEvents btnExtract As System.Windows.Forms.Button
+    Friend WithEvents pbLoading As System.Windows.Forms.ProgressBar
+    Friend WithEvents lblStatus As System.Windows.Forms.Label
+
+End Class
--- deployment/setup-beta.iss
+++ deployment/setup-beta.iss
@@ -2,7 +2,7 @@
 ; SEE THE DOCUMENTATION FOR DETAILS ON CREATING INNO SETUP SCRIPT FILES!
 
 #define MyAppName "Pawnshop"
-#define MyAppVersion "b1.3.3"
+#define MyAppVersion "b1.3.3.1"
 #define MyAppPublisher "Perfecto Group of Companies"
 #define MyAppExeName "pawnshop.exe"
 
@@ -18,7 +18,7 @@ AppPublisher={#MyAppPublisher}
 DefaultDirName={pf}\cdt-S0ft\Pawnshop
 DefaultGroupName=cdt-S0ft\Pawnshop
 OutputDir=D:\cadeath\Documents\DevInstaller\Pawnshop
-OutputBaseFilename=daltonpawnshop-b133
+OutputBaseFilename=daltonpawnshop-b1331
 SetupIconFile=D:\cadeath\Documents\GitHub\Dalton\RAW\Tpdkdesign.net-Refresh-Cl-System-Install-2.ico
 Compression=lzma
 SolidCompression=yes
