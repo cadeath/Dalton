f434d41601587f357c48adbc8c599462299623f7
cadeath <ecjmaquic@gmail.com>
2016-03-09 12:27:25 +0800

    update

---
+++ Pawnshop/Pawnshop/Developer/dev_print.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Data.Odbc
+Imports Microsoft.Reporting.WinForms
+
+Public Class dev_print
+
+    Private Sub dev_print_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        For Each tmpPrinterName In Printing.PrinterSettings.InstalledPrinters
+            cboPrinter.Items.Add(tmpPrinterName)
+        Next
+    End Sub
+
+    Private Sub btnPrint_Click(sender As System.Object, e As System.EventArgs) Handles btnPrint.Click
+        Dim autoPrintPT As Reporting
+
+        Dim printerName As String = cboPrinter.Text
+        Dim report As LocalReport = New LocalReport
+        autoPrintPT = New Reporting
+
+        Dim mySql As String, dsName As String = "dsPawnTicket"
+        
+        Dim ds As DataSet = LoadSQL(mySql, dsName)
+    End Sub
+
+    Private Sub Load_ID()
+
+    End Sub
+End Class
--- Pawnshop/Pawnshop/Forms/frmPrintManager.vb
+++ Pawnshop/Pawnshop/Forms/frmPrintManager.vb
@@ -1,6 +1,12 @@
 ﻿Imports System.Data.Odbc
+Imports Microsoft.Reporting.WinForms
 
 Public Class frmPrintManager
+
+    Private ORNUMBER As Integer = GetOption("ORLastNum")
+    Private PRINTER_OR As String = GetOption("PrinterOR")
+
+    Private Selected_Item As PawnTicket
 
     Private Sub btnCancel_Click(sender As System.Object, e As System.EventArgs) Handles btnCancel.Click
         Me.Close()
@@ -47,4 +53,60 @@ Public Class frmPrintManager
         End Try
         myCon.Close()
     End Sub
+
+    Private Sub btnPrint_Click(sender As System.Object, e As System.EventArgs) Handles btnPrint.Click
+        If lvPawnItems.SelectedItems.Count = 0 Then Exit Sub
+
+        AssignOR()
+
+    End Sub
+
+#Region "Procedures"
+    Private Sub PrintOR()
+        Dim autoPrintPT As Reporting
+
+        Dim printerName As String = PRINTER_OR
+        If Not canPrint(printerName) Then
+            MsgBox("Unable to print" + vbCr + "Please check if the printer is online.", MsgBoxStyle.Critical, "PRINTER ERROR")
+            Exit Sub
+        End If
+
+        Dim report As LocalReport = New LocalReport
+        autoPrintPT = New Reporting
+    End Sub
+
+    Private Sub AssignOR()
+        Dim pt As Integer = lvPawnItems.Items(0).Text
+        Dim mySql As String = "SELECT * FROM tblPAWN WHERE PAWNTICKET = " & pt, _
+            fillData As String = "tblPAWN"
+        Dim ds As New DataSet
+        ds = LoadSQL(mySql, fillData)
+        If ds.Tables(fillData).Rows.Count = 0 Then
+            MsgBox("Pawnticket not found", MsgBoxStyle.Critical, "Database Error")
+            Exit Sub
+        End If
+
+        Selected_Item.LoadTicketInRow(ds.Tables(fillData).Rows(0))
+
+        ds.Tables(fillData).Rows(0).Item("ORNUM") = ORNUMBER
+        database.SaveEntry(ds, False)
+
+        NextORNum()
+    End Sub
+
+    Private Sub NextORNum()
+        ORNUMBER += 1
+        UpdateOptions("ORLastNum", ORNUMBER)
+    End Sub
+
+    Private Function canPrint(ByVal printerName As String) As Boolean
+        Try
+            Dim printDocument As Drawing.Printing.PrintDocument = New Drawing.Printing.PrintDocument
+            printDocument.PrinterSettings.PrinterName = printerName
+            Return printDocument.PrinterSettings.IsValid
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+#End Region
 End Class
