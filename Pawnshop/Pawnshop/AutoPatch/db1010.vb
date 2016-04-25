7ad78a0d2b176d3a3ed2a7f0b9d82b4e301237d5
cadeath <ecjmaquic@gmail.com>
2016-04-25 12:03:53 +0800

    UPDATE
    Adding Auto Patchup
    Need to test PT PRINTING with FULL ADDRESS

---
+++ Pawnshop/Pawnshop/AutoPatch/db1010.vb
@@ -0,0 +1,22 @@
+﻿Module db1010
+    Const ALLOWABLE_VERSION As String = "1.098"
+    Const LATEST_VERSION As String = "1.0.10"
+
+    Sub PatchUp()
+        If Not isPatchable(ALLOWABLE_VERSION) Then Exit Sub
+
+        Dim DROP_VIEW As String = "DROP VIEW PRINT_PAWNING;"
+        Dim CREATE_NEW_PRINT_PAWNING As String
+
+        Try
+
+            RunCommand(DROP_VIEW)
+
+
+            Database_Update(LATEST_VERSION)
+            Log_Report("SYSTEM PATCHED UP FROM 1.0.9 TO 1.0.10")
+        Catch ex As Exception
+            Log_Report(ex.ToString & "[1.0.9]")
+        End Try
+    End Sub
+End Module
