29804568892e2c18a0f74db2e90f3846e48cc869
cadeath <ecjmaquic@gmail.com>
2016-05-10 17:17:12 +0800

    INCLUDE PATCHER

---
+++ Pawnshop/Patcher/db_IntHash/db_intHash.vb
@@ -0,0 +1,33 @@
+﻿Module db_intHash
+
+    Sub do_update()
+        ' Add INT_CHECK at TBLPAWN
+        Dim INT_CHECK As String = _
+            "ALTER TABLE TBLPAWN ADD INT_CHECKSUM VARCHAR(100);"
+
+        RunCommand(INT_CHECK)
+        SystemUpdate("INT_CHECKSUM INCLUDED")
+
+        ' HASH table TBLINT
+        Dim INT_HASH As String, ds As DataSet
+        Dim mySql As String = "SELECT DAYFROM, DAYTO, ITEMTYPE, INTEREST, PENALTY, REMARKS FROM TBLINT"
+        ds = LoadSQL(mySql)
+        INT_HASH = GetMD5(ds)
+
+        SystemUpdate("TABLE HASHED - UPDATING WHOLE TABLE")
+
+        mySql = "SELECT * FROM TBLPAWN WHERE STATUS <> 'V'"
+        Dim fillData As String = "TBLPAWN"
+        ds = LoadSQL(mySql, fillData)
+
+        frmMain.pb_load.Maximum = ds.Tables(0).Rows.Count
+        frmMain.pb_load.Value = 0
+        For Each dr As DataRow In ds.Tables(fillData).Rows
+            dr("INT_CHECKSUM") = INT_HASH
+            frmMain.AddProgress()
+        Next
+        mod_system.SaveEntry(ds, False)
+        SystemUpdate("DATABASE UPDATED")
+    End Sub
+
+End Module
---
+++ Pawnshop/Patcher/frmMain.vb
@@ -0,0 +1,27 @@
+﻿Public Class frmMain
+
+    Dim dbLoc As String = ""
+
+    Private Sub btnPatch_Click(sender As System.Object, e As System.EventArgs) Handles btnPatch.Click
+        If dbLoc = "" Then
+            MsgBox("Double Click the FORM to select DB Location", MsgBoxStyle.Information)
+            Exit Sub
+        End If
+
+        mod_system.dbName = dbLoc
+        db_intHash.do_update()
+    End Sub
+
+    Private Sub frmMain_DoubleClick(sender As Object, e As System.EventArgs) Handles Me.DoubleClick
+        ofdDBName.ShowDialog()
+    End Sub
+
+    Private Sub ofdDBName_FileOk(sender As System.Object, e As System.ComponentModel.CancelEventArgs) Handles ofdDBName.FileOk
+        dbLoc = ofdDBName.FileName
+    End Sub
+
+    Friend Sub AddProgress()
+        pb_load.Value += 1
+        Application.DoEvents()
+    End Sub
+End Class
