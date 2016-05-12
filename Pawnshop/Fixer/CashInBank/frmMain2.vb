7e959d5e1f0bf9f901b6aa2039be9a33e13a2e23
cadeath <ecjmaquic@gmail.com>
2016-05-12 09:37:47 +0800

    update fixer

---
+++ Pawnshop/Fixer/CashInBank/frmMain2.vb
@@ -0,0 +1,25 @@
+﻿Public Class frmMain2
+
+    Private Sub frmMain2_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Me.Text &= " - " & update_cashinbank.MOD_NAME
+    End Sub
+
+    Private Sub txtDB_DoubleClick(sender As Object, e As System.EventArgs) Handles txtDB.DoubleClick
+        ofd_db.ShowDialog()
+    End Sub
+
+    Private Sub ofd_db_FileOk(sender As System.Object, e As System.ComponentModel.CancelEventArgs) Handles ofd_db.FileOk
+        txtDB.Text = ofd_db.FileName
+    End Sub
+
+    Private Sub btnFix_Click(sender As System.Object, e As System.EventArgs) Handles btnFix.Click
+        If txtDB.Text = "" Then SystemStat("Nothing happened...") : Exit Sub
+        SystemStat("Fixing...")
+        UpdateCashInBank(txtSys.Text, txtDB.Text)
+    End Sub
+
+    Public Sub SystemStat(str As String)
+        lblStatus.Text = str
+        Application.DoEvents()
+    End Sub
+End Class
