2de602d41645f7a516672d942918e2d239e801aa
cadeath <ecjmaquic@gmail.com>
2016-05-23 16:27:30 +0800

    UPDATE
    JWL rate decrease from 4 to 3

--- Pawnshop/Pawnshop/Forms/frmCashCountV2.vb
+++ Pawnshop/Pawnshop/Forms/frmCashCountV2.vb
@@ -151,7 +151,7 @@
     End Function
 
     Private Sub btnPost_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPost.Click
-        If txtCount.Text = "" Then txtCount.Focus() : Exit Sub
+        If txtCount.Text <> "" Then txtCount.Focus() : Exit Sub
 
         Dim ans As DialogResult = MsgBox("Do you want to POST this cash count?", MsgBoxStyle.YesNo + MsgBoxStyle.DefaultButton2 + MsgBoxStyle.Information)
         If ans = Windows.Forms.DialogResult.No Then Exit Sub
