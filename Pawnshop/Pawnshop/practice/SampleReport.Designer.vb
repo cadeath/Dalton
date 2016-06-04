73a379532a8177f7612194b5807af28408dd042b
blade1992 <misionaellie@gmail.com>
2016-06-04 11:55:06 +0800

    UPDATE

---
+++ Pawnshop/Pawnshop/practice/SampleReport.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SampleReport
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtStart = New System.Windows.Forms.DateTimePicker()
+        Me.txtEnd = New System.Windows.Forms.DateTimePicker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.txtCurrency = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtStart
+        '
+        Me.txtStart.Location = New System.Drawing.Point(91, 12)
+        Me.txtStart.Name = "txtStart"
+        Me.txtStart.Size = New System.Drawing.Size(200, 20)
+        Me.txtStart.TabIndex = 0
+        '
+        'txtEnd
+        '
+        Me.txtEnd.Location = New System.Drawing.Point(318, 12)
+        Me.txtEnd.Name = "txtEnd"
+        Me.txtEnd.Size = New System.Drawing.Size(200, 20)
+        Me.txtEnd.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(59, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(30, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "From"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(295, 14)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(20, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "To"
+        '
+        'ReportViewer1
+        '
+        Me.ReportViewer1.Location = New System.Drawing.Point(12, 70)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.Size = New System.Drawing.Size(628, 373)
+        Me.ReportViewer1.TabIndex = 4
+        '
+        'txtCurrency
+        '
+        Me.txtCurrency.Location = New System.Drawing.Point(91, 38)
+        Me.txtCurrency.Name = "txtCurrency"
+        Me.txtCurrency.Size = New System.Drawing.Size(200, 20)
+        Me.txtCurrency.TabIndex = 5
+        '
+        'SampleReport
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(652, 455)
+        Me.Controls.Add(Me.txtCurrency)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtEnd)
+        Me.Controls.Add(Me.txtStart)
+        Me.Controls.Add(Me.ReportViewer1)
+        Me.Name = "SampleReport"
+        Me.Text = "SampleReport"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtStart As System.Windows.Forms.DateTimePicker
+    Friend WithEvents txtEnd As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents txtCurrency As System.Windows.Forms.TextBox
+End Class
---
+++ Pawnshop/Pawnshop/practice/frmDollarTransaction.vb
@@ -0,0 +1,144 @@
+﻿Public Class DollarTransction
+    Private mySql As String, fillData As String
+    Private Daily As String = "Daily"
+    Enum ReportType As Integer
+        DollarBuying = 0
+        DailyDollar = 1
+        DollarMonthlyAllTransaction = 2
+        DollarDailyAllTransaction = 3
+    End Enum
+    Friend FormType As ReportType = ReportType.DollarBuying
+
+    Private Sub Generate()
+        Select Case FormType
+            Case ReportType.DollarBuying
+                DollarBuying()
+            Case ReportType.DailyDollar
+                DailyDollar()
+            Case ReportType.DollarMonthlyAllTransaction
+                DollarMonthlyAllTransaction()
+            Case ReportType.DollarDailyAllTransaction
+                DollarDailyAllTransaction()
+            Case ReportType.DollarBuying
+                DollarBuying()
+         
+        End Select
+    End Sub
+    Private Sub DollarTransction_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        cbmCurrency.Focus()
+        LoadCurrency()
+
+        cboMonthlyDaily.Items.Add("Monthly")
+        cboMonthlyDaily.Items.Add("Daily")
+    End Sub
+
+    Private Sub btnGenerate_Click(sender As System.Object, e As System.EventArgs) Handles btnGenerate.Click
+        If cbmCurrency.Text = "" And cbmCurrency.SelectedIndex Then Exit Sub
+        If cboMonthlyDaily.Text = "" And cboMonthlyDaily.SelectedIndex Then Exit Sub
+        If cbmCurrency.Text = cbmCurrency.Text And cboMonthlyDaily.Text = "Monthly" Then
+            DollarBuying()
+        ElseIf cbmCurrency.Text = cbmCurrency.Text And cboMonthlyDaily.Text = "Daily" Then
+            DailyDollar()
+        End If
+
+        If cbmCurrency.Text = "ALL" And cboMonthlyDaily.Text = "Monthly" Then
+            DollarMonthlyAllTransaction()
+        End If
+    End Sub
+    Private Sub LoadCurrency()
+        mySql = "SELECT DISTINCT CURRENCY FROM TBLDOLLAR"
+        fillData = "TBLDOLLAR"
+
+        Dim ds As DataSet = LoadSQL(mySql)
+
+        cbmCurrency.Items.Clear()
+        cbmCurrency.Items.Add("ALL")
+        For Each dr As DataRow In ds.Tables(0).Rows
+            cbmCurrency.Items.Add(dr.Item("CURRENCY"))
+        Next
+    End Sub
+
+    Private Sub DollarBuying()
+
+        Dim stDay = GetFirstDate(monCal.SelectionStart)
+        Dim laDay = GetLastDate(monCal.SelectionEnd)
+        Dim fillData As String = "dsDollar"
+        Dim mySql As String = "SELECT * FROM tblDollar"
+        mySql &= String.Format(" WHERE CURRENCY= '" & cbmCurrency.Text & "' AND TRANSDATE BETWEEN '{0}' AND '{1}'", stDay.ToShortDateString, laDay.ToShortDateString)
+        mySql &= " ORDER BY TRANSDATE ASC"
+
+        Dim rptPara As New Dictionary(Of String, String)
+        rptPara.Add("txtMonthOf", "FOR THE MONTH OF " & stDay.ToString("MMMM").ToUpper & " " & stDay.Year)
+        rptPara.Add("BranchName", "BUla-Road")
+        rptPara.Add("txtUsername", "Blade")
+        rptPara.Add("CURRENCY", "Currency Name:  " & cbmCurrency.Text)
+        frmReport.ReportInit(mySql, fillData, "Reports\rptDollarTransaction.rdlc", rptPara)
+        frmReport.Show()
+    End Sub
+    Private Sub DailyDollar()
+        Dim fillData As String = "dsDollar"
+        Dim mySql As String = "SELECT * FROM tblDollar"
+        mySql &= String.Format(" WHERE CURRENCY= '" & cbmCurrency.Text & "' AND TRANSDATE = '{0}'", monCal.SelectionStart.ToShortDateString)
+        mySql &= " ORDER BY TRANSDATE ASC"
+
+        Dim rptPara As New Dictionary(Of String, String)
+        rptPara.Add("txtMonthOf", "Date: " & monCal.SelectionStart.ToLongDateString)
+        rptPara.Add("BranchName", "Bula-Road")
+        rptPara.Add("txtUsername", "Blade")
+        rptPara.Add("CURRENCY", "Currency Name:  " & cbmCurrency.Text)
+        frmReport.ReportInit(mySql, fillData, "Reports\rptDollarTransaction.rdlc", rptPara)
+        frmReport.Show()
+    End Sub
+
+    Private Sub DollarMonthlyAllTransaction()
+        Dim stDay = GetFirstDate(monCal.SelectionStart)
+        Dim laDay = GetLastDate(monCal.SelectionEnd)
+        Dim fillData As String = "dsDollartransAllCurrency"
+        Dim mySql As String = "SELECT * FROM tblDollar"
+        mySql &= String.Format(" WHERE  TRANSDATE BETWEEN '{0}' AND '{1}'", stDay.ToShortDateString, laDay.ToShortDateString)
+        mySql &= " ORDER BY TRANSDATE ASC"
+
+        Dim rptPara As New Dictionary(Of String, String)
+        rptPara.Add("txtMonthOf", "FOR THE MONTH OF " & stDay.ToString("MMMM").ToUpper & " " & stDay.Year)
+        rptPara.Add("txtBranchName", "Bula-Road")
+        rptPara.Add("txtUsername", "Blade")
+        frmReport.ReportInit(mySql, fillData, "Reports\rptDollarTransactionAllCurrency.rdlc", rptPara)
+        frmReport.Show()
+    End Sub
+
+    Private Sub DollarDailyAllTransaction()
+        Dim stDay = GetFirstDate(monCal.SelectionStart)
+        Dim laDay = GetLastDate(monCal.SelectionEnd)
+        Dim fillData As String = "dsDollartransAllCurrency"
+        Dim mySql As String = "SELECT * FROM tblDollar"
+        mySql &= String.Format(" WHERE STATUS ='A' AND TRANSDATE = '{0}'", monCal.SelectionStart.ToShortDateString)
+        mySql &= " ORDER BY TRANSDATE ASC"
+
+        Dim rptPara As New Dictionary(Of String, String)
+        rptPara.Add("txtMonthOf", "FOR THE MONTH OF " & stDay.ToString("MMMM").ToUpper & " " & stDay.Year)
+        rptPara.Add("txtBranchName", "Bula-Road")
+        rptPara.Add("txtUsername", "Blade")
+        frmReport.ReportInit(mySql, fillData, "Reports\rptDollarTransactionAllCurrency.rdlc", rptPara)
+        frmReport.Show()
+    End Sub
+
+    Private Sub cbmCurrency_KeyDown(sender As System.Object, e As System.Windows.Forms.KeyEventArgs) Handles cbmCurrency.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            cboMonthlyDaily.Focus()
+        End If
+    End Sub
+
+    Private Sub cbmCurrency_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles cbmCurrency.SelectedIndexChanged
+      
+    End Sub
+
+    Private Sub cboMonthlyDaily_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles cboMonthlyDaily.SelectedIndexChanged
+            If cbmCurrency.Text = "ALL" And cboMonthlyDaily.Text = "Monthly" Then
+                DollarMonthlyAllTransaction()
+            ElseIf cbmCurrency.Text = "ALL" And cboMonthlyDaily.Text = "Daily" Then
+                DollarDailyAllTransaction()
+            Else
+                Exit Sub
+            End If
+    End Sub
+End Class
