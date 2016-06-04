e20ab52f03d96b2e475a5ccf128fdce2f1fe05e6
blade1992 <misionaellie@gmail.com>
2016-06-04 13:11:56 +0800

    update

--- Pawnshop/Pawnshop/Modules/updateRate.vb
+++ Pawnshop/Pawnshop/Modules/updateRate.vb
@@ -51,9 +51,20 @@ Module updateRate
         Console.WriteLine("Checking excessive entries")
         ds = LoadSQL(mySql, fillData)
 
-        If MaxDS > MaxRate Then
+        If MaxDS >= MaxRate Then
             For i = MaxDS To MaxRate Step -1
-                ds.Tables(fillData).Rows(i - 1).Delete()
+                dsRate.Tables(fillData).Rows(i - 1).Delete()
+                database.SaveEntry(ds, False)
+            Next
+
+        ElseIf MaxDS <= MaxRate Then
+            For i = MaxDS To MaxRate Step -1
+                dsRate.Tables(fillData).Rows(i - 1).Delete()
+                database.SaveEntry(ds, False)
+            Next
+        ElseIf MaxDS = MaxRate Then
+            For i = MaxDS To MaxRate Step -1
+                dsRate.Tables(fillData).Rows(i - 1).Delete()
                 database.SaveEntry(ds, False)
             Next
         End If
