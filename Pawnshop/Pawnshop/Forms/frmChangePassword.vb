e418e120844635c55317e34bfc8e030734518d15
marzxxx90 <junmar_honor@ymail.com>
2016-06-09 14:30:35 +0800

    update

--- Pawnshop/Pawnshop/Forms/frmChangePassword.vb
+++ Pawnshop/Pawnshop/Forms/frmChangePassword.vb
@@ -3,16 +3,23 @@ Public Class frmChangePassword
     Private selectedUser As New ComputerUser
     Private moduleName As String = "User Management"
     Private fillData As String = "tbl_Gamit"
-
+    Private Function PasswordPolicy() As Boolean
+        If txtNewPassword.Text.Length >= 4 And txtNewPassword.Text.Length <= 8 Then
+            Return True
+        End If
+        MsgBox("Password must be atleast 4 characters but not more than 8 characters", MsgBoxStyle.Critical, moduleName)
+        Return False
+    End Function
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        If txtOldPassword.Text = "" And txtNewPassword.Text = "" And txtConfirmPassword.Text = "" Then Exit Sub
+        If Not PasswordPolicy() Then Exit Sub
+        If txtOldPassword.Text = "" Then Exit Sub
         If Encrypt(txtOldPassword.Text) <> POSuser.Password Then
-            MsgBox("Previous password not MATCHED", MsgBoxStyle.Critical, moduleName)
+            MsgBox("Previous password not MATCHED", MsgBoxStyle.Critical, "Change Password")
             txtOldPassword.Focus()
             Exit Sub
         End If
         If txtNewPassword.Text <> txtConfirmPassword.Text And Not txtConfirmPassword.Text = "" Then
-            MsgBox("Password is not MATCHED", MsgBoxStyle.Critical, moduleName)
+            MsgBox("Password is not MATCHED", MsgBoxStyle.Critical, "Change Password")
             txtNewPassword.Focus()
             Exit Sub
         End If
