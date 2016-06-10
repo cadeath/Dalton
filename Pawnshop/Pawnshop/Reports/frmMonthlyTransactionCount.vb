8cae274e79ae1c071d70a9f66138c5594855333a
marzxxx90 <junmar_honor@ymail.com>
2016-06-10 17:10:31 +0800

    New monthly transaction count

--- Pawnshop/Pawnshop/Reports/frmMonthlyTransactionCount.vb
+++ Pawnshop/Pawnshop/Reports/frmMonthlyTransactionCount.vb
@@ -13,27 +13,15 @@
         mySql &= String.Format(" TIMELY BETWEEN '{0}' AND '{1}'", StartDay.ToShortDateString, EndDay.ToShortDateString)
         mySql &= "GROUP BY MOD_NAME"
 
-        'mySql = " SELECT LOGS_ID, MOD_NAME, "
-        'mySql &= "(CASE WHEN MOD_NAME = 'INSURANCE' THEN LOGS_ID END) AS INSURANCE, "
-        'mySql &= "(CASE WHEN MOD_NAME = 'MONEYTRANSFER' THEN LOGS_ID END) AS MONEYTRANSFER, "
-        'mySql &= "(CASE WHEN MOD_NAME = 'CASHCOUNT' THEN LOGS_ID END) AS CASHCOUNT, "
-        'mySql &= "(CASE WHEN MOD_NAME = 'CASH IN/OUT' THEN LOGS_ID END) AS CASHINOUT, "
-        'mySql &= "(CASE WHEN MOD_NAME = 'NEW LOANS' THEN LOGS_ID END) AS NEWLOANS, "
-        'mySql &= "(CASE WHEN MOD_NAME = 'SMARTMONEY IN' THEN LOGS_ID END) AS SMARTMONEYIN, "
-        'mySql &= "(CASE WHEN MOD_NAME = 'BDO ATM' THEN LOGS_ID END) AS BDOATM, "
-        'mySql &= "(CASE WHEN MOD_NAME = 'DOLLAR' THEN LOGS_ID END) AS DOLLAR "
-        'mySql &= " FROM TBL_DAILYTIMELOG "
-        'mySql &= String.Format(" WHERE TIMELY BETWEEN '{0}' AND '{1}' AND HASCUSTOMER = '1'", StartDay.ToShortDateString, EndDay.ToShortDateString)
-        'mySql &= "GROUP BY MOD_NAME"
-
         Console.WriteLine(mySql)
 
-        'Dim addParameters As New Dictionary(Of String, String)
+        Dim addParameters As New Dictionary(Of String, String)
 
-        'addParameters.Add("txtMonthOf", "DATE: " & StartDay.ToShortDateString)
-        'addParameters.Add("branchName", branchName)
+        addParameters.Add("txtMonthstart", "DATE: " & StartDay.ToShortDateString)
+        addParameters.Add("txtMonthend", "DATE: " & EndDay.ToShortDateString)
+        addParameters.Add("branchName", branchName)
 
-        frmReport.ReportInit(mySql, dsName, "Reports\rpt_MonthlyTransactionCount.rdlc")
+        frmReport.ReportInit(mySql, dsName, "Reports\rpt_MonthlyTransactionCount.rdlc", addParameters)
         frmReport.Show()
     End Sub
 
