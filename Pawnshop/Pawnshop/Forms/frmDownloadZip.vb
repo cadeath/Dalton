90a7772c2ab065a3bc21b2a79f09db6a8fc99fcf
marzxxx90 <junmar_honor@ymail.com>
2016-06-14 16:14:09 +0800

    Can Download Zip

---
+++ Pawnshop/Pawnshop/Forms/frmDownloadZip.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.IO.Compression
+Public Class frmDownloadZip
+    Private Sub Downloader()
+        MessageBox.Show("Downloading Please wait.....")
+        Dim ZipUrl As String = "https:\\www.colorado.edu/conflict/peace/download/peace.zip"
+        'sample url--https:\\www.colorado.edu/conflict/peace/download/peace.zip
+        Dim ZipLocation As String = "C:\Users\MIS JunMar\Downloads\peace.zip"
+        Dim webUri = New Uri(ZipUrl)
+        Using client As New WebClient
+            client.DownloadFile(webUri, ZipLocation)
+        End Using
+        MessageBox.Show("Zip file Downloaded Successfully")
+
+    End Sub
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Downloader()
+    End Sub
+End Class
