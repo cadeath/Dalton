7f4ca095f5dbd4b11d83fafaa6315150c3f798b6
marzxxx90 <junmar_honor@ymail.com>
2016-06-16 14:42:40 +0800

    update

--- Pawnshop/Pawnshop/Forms/frmDownloadZip.vb
+++ Pawnshop/Pawnshop/Forms/frmDownloadZip.vb
@@ -2,6 +2,8 @@
 Imports System.IO
 Imports System.IO.Compression
 Imports System.Text
+Imports System
+Imports Shell32
 Public Class frmDownloadZip
     Private Sub Downloader()
         Dim ZipLocation As String = My.Application.Info.DirectoryPath & "\peace.zip"
@@ -21,8 +23,8 @@ Public Class frmDownloadZip
         'Process.Start(("cmd"), "cd \")
         Using p1 As New Process
             p1.StartInfo.FileName = "cmd.exe"
-            p1.StartInfo.Arguments = "/k cd \"
-            p1.StartInfo.Arguments = "/k ipconfig"
+            p1.StartInfo.Arguments = "/k cd C:\Program Files\WinRAR"
+            'p1.StartInfo.Arguments = "ipconfig"
             Try
                 p1.Start()
             Catch ex As Exception
@@ -31,8 +33,26 @@ Public Class frmDownloadZip
             p1.WaitForExit()
         End Using
     End Sub
+    Private Sub Extractor()
+        Dim ZipFile As String = "C:\Users\MIS JunMar\Downloads\try\honor.zip"
+        Dim ZipDir As String = "C:\Users\MIS JunMar\Downloads\try\nirc\"
+        Dim Shell As Shell32.IShellDispatch2
+        Dim ShellFolder As Shell32.Folder
+
+        If File.Exists(ZipFile) Then
+            Shell = CType(CreateObject("Shell.Application"), IShellDispatch2)
+
+            If Not Directory.Exists(ZipDir) Then Directory.CreateDirectory(ZipDir)
+            ShellFolder = Shell.NameSpace(ZipDir)
+            If ShellFolder IsNot Nothing Then
+                ShellFolder.CopyHere(Shell.NameSpace(ZipFile).Items)
+            End If
+            MessageBox.Show("Successfully Extracted")
+        End If
+    End Sub
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        DOSCommand()
+        Extractor()
+        'DOSCommand()
         'Downloader()
     End Sub
 End Class
