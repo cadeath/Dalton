ae86dc7bb43ad19655aa1be44387bf82c6e1669e
blade1992 <misionaellie@gmail.com>
2016-06-16 13:20:31 +0800

    Create rar or zip file

---
+++ Pawnshop/Pawnshop/Forms/frmDownloadZip.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDownloadZip
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblDownload = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.SuspendLayout()
+        '
+        'lblDownload
+        '
+        Me.lblDownload.AutoSize = True
+        Me.lblDownload.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, CType(((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic) _
+                Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDownload.Location = New System.Drawing.Point(52, 11)
+        Me.lblDownload.Name = "lblDownload"
+        Me.lblDownload.Size = New System.Drawing.Size(254, 25)
+        Me.lblDownload.TabIndex = 0
+        Me.lblDownload.Text = "CLICK TO DOWNLOAD"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(26, 73)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(134, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Start Download"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(26, 102)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(134, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Cancel Download"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(294, 174)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(31, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "........"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(23, 187)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "File Size"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(23, 215)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(67, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Downloaded"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(26, 131)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(314, 23)
+        Me.ProgressBar1.TabIndex = 6
+        '
+        'frmDownloadZip
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(352, 248)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblDownload)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "frmDownloadZip"
+        Me.Text = "Download Zip"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblDownload As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+End Class
---
+++ Pawnshop/Pawnshop/Forms/frmDownloadZip.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Threading
+Public Class frmDownloadZip
+    Dim wc As System.Net.WebClient
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        wc = New System.Net.WebClient
+        AddHandler wc.DownloadProgressChanged, AddressOf OnDownloadProgressChanged
+        AddHandler wc.DownloadFileCompleted, AddressOf OnDownloadFileCompleted
+        wc.DownloadFileAsync(New Uri("https://wwww.dropbox.com/s/i4gf4pz5ovzeker/Advanced%20Converter.rar?dl=1"), Application.StartupPath & "\ " & "Advanced Converter.rar")
+    End Sub
+    Private Sub OnDownloadProgressChanged(ByVal sender As Object, ByVal e As System.Net.DownloadProgressChangedEventArgs)
+        Dim totalsize As Long = e.TotalBytesToReceive
+        Dim downloadedBytes As Long = e.BytesReceived
+        Dim percentage As Integer = e.ProgressPercentage
+        ProgressBar1.Value = e.ProgressPercentage
+        Label1.Text = "%" & ProgressBar1.Value
+        Label2.Text = "File Size: " & totalsize / 1024 / 1000 & "Mb"
+        Label3.Text = "Downloaded: " & downloadedBytes / 1024 / 1000 & "Mb" & " of " & totalsize / 1024 / 1000 & "Mb"
+    End Sub
+
+    Private Sub OnDownloadFileCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.AsyncCompletedEventArgs)
+        If e.Cancelled Then
+            MsgBox("Download Cancelled!", MsgBoxStyle.Information)
+        Else
+            MsgBox("Download Completed.", MsgBoxStyle.Information)
+        End If
+
+    End Sub
+
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        wc.CancelAsync()
+        MsgBox("Download Cancelled!", MsgBoxStyle.Information)
+        Thread.Sleep("4000")
+        My.Computer.FileSystem.DeleteFile("Advanced Converter.rar")
+        Me.Close()
+    End Sub
+
+ 
+End Class
