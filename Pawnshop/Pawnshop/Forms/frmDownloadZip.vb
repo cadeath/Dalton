464e57a63069c5ed23b263c860f3525453527bec
marzxxx90 <junmar_honor@ymail.com>
2016-06-20 09:47:40 +0800

    update

--- Pawnshop/Pawnshop/Forms/frmDownloadZip.vb
+++ Pawnshop/Pawnshop/Forms/frmDownloadZip.vb
@@ -1,58 +1,61 @@
 ﻿Imports System.Net
 Imports System.IO
-Imports System.IO.Compression
-Imports System.Text
-Imports System
+'Imports System.IO.Compression
+'Imports System.Text
+'Imports System
 Imports Shell32
 Public Class frmDownloadZip
     Private Sub Downloader()
-        Dim ZipLocation As String = My.Application.Info.DirectoryPath & "\peace.zip"
-        Dim ZipUrl As String = "C:\Users\MIS JunMar\Downloads\try\peace.zip"
+        Dim ZipLocation As String = My.Application.Info.DirectoryPath & "\honor.zip"
+        Dim ZipUrl As String = "C:\Users\MIS JunMar\Downloads\try\honor.zip"
         'sample url "https:\\www.colorado.edu/conflict/peace/download/peace.zip"
         Dim webUri = New Uri(ZipUrl)
         Using client As New WebClient
             ProgressBar1.Value = ProgressBar1.Minimum
-            client.DownloadFile(webUri, ZipLocation)
+            Try
+                client.DownloadFile(webUri, ZipLocation)
+                ProgressBar1.Value = ProgressBar1.Maximum
+                MessageBox.Show("Zip file Downloaded Successfully")
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
+            client.Dispose()
         End Using
-        ProgressBar1.Value = ProgressBar1.Maximum
-        MessageBox.Show("Zip file Downloaded Successfully")
     End Sub
-    Private Sub DOSCommand()
-        ' Dim ipconfig As String = "ipconfig"
-        'Dim cmdProcess As New Process
-        'Process.Start(("cmd"), "cd \")
+    Private Sub DOSCommandExtract()
+        Dim ZipLocation2 As String = " ""C:\Users\MIS JunMar\Downloads\try\honor.zip"" "
+        Dim ZipExtracted2 As String = " ""C:\Users\MIS JunMar\Downloads\try\nirc"" "
+        Dim Zlocation As String = "C:\Program Files\WinRAR\winrar.exe"
         Using p1 As New Process
-            p1.StartInfo.FileName = "cmd.exe"
-            p1.StartInfo.Arguments = "/k cd C:\Program Files\WinRAR"
-            'p1.StartInfo.Arguments = "ipconfig"
+            p1.StartInfo.FileName = Zlocation
+            p1.StartInfo.Arguments = " x " & ZipLocation2 & " *.* " & ZipExtracted2
             Try
                 p1.Start()
             Catch ex As Exception
                 MessageBox.Show(ex.Message)
             End Try
-            p1.WaitForExit()
+            'p1.WaitForExit()
         End Using
     End Sub
-    Private Sub Extractor()
-        Dim ZipFile As String = "C:\Users\MIS JunMar\Downloads\try\honor.zip"
-        Dim ZipDir As String = "C:\Users\MIS JunMar\Downloads\try\nirc\"
-        Dim Shell As Shell32.IShellDispatch
-        Dim ShellFolder As Shell32.Folder
+    'Private Sub Extractor()
+    '    Dim ZipFile As String = My.Application.Info.DirectoryPath & "\honor.zip"
+    '    Dim ZipLocation As String = My.Application.Info.DirectoryPath
+    '    Dim Shell As Shell32.IShellDispatch
+    '    Dim ShellFolder As Shell32.Folder
 
-        If File.Exists(ZipFile) Then
-            Shell = CType(CreateObject("Shell.Application"), IShellDispatch2)
-
-            If Not Directory.Exists(ZipDir) Then Directory.CreateDirectory(ZipDir)
-            ShellFolder = Shell.NameSpace(ZipDir)
-            If ShellFolder IsNot Nothing Then
-                ShellFolder.CopyHere(Shell.NameSpace(ZipFile).Items)
-            End If
-            MessageBox.Show("Successfully Extracted")
-        End If
-    End Sub
+    '    If File.Exists(ZipFile) Then
+    '        Shell = CType(CreateObject("Shell.Application"), IShellDispatch2)
+    '        If Not Directory.Exists(ZipLocation) Then Directory.CreateDirectory(ZipLocation)
+    '        ShellFolder = Shell.NameSpace(ZipLocation)
+    '        If ShellFolder IsNot Nothing Then
+    '            ShellFolder.CopyHere(Shell.NameSpace(ZipFile).Items)
+    '        End If
+    '        MessageBox.Show("Successfully Extracted")
+    '    End If
+    'End Sub
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        Extractor()
-        'DOSCommand()
+        'Extractor()
+        DOSCommandExtract()
         'Downloader()
     End Sub
 End Class
