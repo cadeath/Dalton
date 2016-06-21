c896914da6e687558198ad6f429ed8e57cdda75e
marzxxx90 <junmar_honor@ymail.com>
2016-06-21 08:10:48 +0800

    update

--- Pawnshop/Pawnshop/Forms/frmDownloadZip.Designer.vb
+++ Pawnshop/Pawnshop/Forms/frmDownloadZip.Designer.vb
@@ -22,39 +22,108 @@ Partial Class frmDownloadZip
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.txtTime = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.dtpSetDateDownload = New System.Windows.Forms.DateTimePicker()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.dtpDateExtract = New System.Windows.Forms.DateTimePicker()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'Button1
         '
         Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(42, 125)
+        Me.Button1.Location = New System.Drawing.Point(16, 156)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(184, 69)
+        Me.Button1.Size = New System.Drawing.Size(118, 32)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Download"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'ProgressBar1
+        'Timer1
         '
-        Me.ProgressBar1.Location = New System.Drawing.Point(-2, 237)
-        Me.ProgressBar1.Name = "ProgressBar1"
-        Me.ProgressBar1.Size = New System.Drawing.Size(283, 23)
-        Me.ProgressBar1.TabIndex = 1
+        Me.Timer1.Enabled = True
+        '
+        'txtTime
+        '
+        Me.txtTime.AutoSize = True
+        Me.txtTime.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTime.Location = New System.Drawing.Point(12, 9)
+        Me.txtTime.Name = "txtTime"
+        Me.txtTime.Size = New System.Drawing.Size(66, 20)
+        Me.txtTime.TabIndex = 6
+        Me.txtTime.Text = "Current "
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.dtpSetDateDownload)
+        Me.GroupBox1.Location = New System.Drawing.Point(16, 32)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(276, 55)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Set Date Download"
+        '
+        'dtpSetDateDownload
+        '
+        Me.dtpSetDateDownload.CalendarFont = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dtpSetDateDownload.CustomFormat = "MMMM dd, yyyy hh:mm:ss"
+        Me.dtpSetDateDownload.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dtpSetDateDownload.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.dtpSetDateDownload.Location = New System.Drawing.Point(9, 19)
+        Me.dtpSetDateDownload.Name = "dtpSetDateDownload"
+        Me.dtpSetDateDownload.Size = New System.Drawing.Size(256, 26)
+        Me.dtpSetDateDownload.TabIndex = 8
+        Me.dtpSetDateDownload.Value = New Date(2016, 6, 1, 0, 0, 0, 0)
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.dtpDateExtract)
+        Me.GroupBox2.Location = New System.Drawing.Point(16, 93)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(276, 57)
+        Me.GroupBox2.TabIndex = 8
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Set Date Extract"
+        '
+        'dtpDateExtract
+        '
+        Me.dtpDateExtract.CalendarFont = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dtpDateExtract.CustomFormat = "MMMM dd, yyyy hh:mm:ss"
+        Me.dtpDateExtract.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dtpDateExtract.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.dtpDateExtract.Location = New System.Drawing.Point(9, 19)
+        Me.dtpDateExtract.Name = "dtpDateExtract"
+        Me.dtpDateExtract.Size = New System.Drawing.Size(256, 26)
+        Me.dtpDateExtract.TabIndex = 8
+        Me.dtpDateExtract.Value = New Date(2016, 6, 1, 0, 0, 0, 0)
         '
         'frmDownloadZip
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 262)
-        Me.Controls.Add(Me.ProgressBar1)
+        Me.ClientSize = New System.Drawing.Size(298, 196)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.txtTime)
         Me.Controls.Add(Me.Button1)
         Me.Name = "frmDownloadZip"
         Me.Text = "Download Zip"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents txtTime As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents dtpSetDateDownload As System.Windows.Forms.DateTimePicker
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents dtpDateExtract As System.Windows.Forms.DateTimePicker
 End Class
--- Pawnshop/Pawnshop/Forms/frmDownloadZip.vb
+++ Pawnshop/Pawnshop/Forms/frmDownloadZip.vb
@@ -1,9 +1,7 @@
 ﻿Imports System.Net
 Imports System.IO
-'Imports System.IO.Compression
-'Imports System.Text
-'Imports System
 Imports Shell32
+Imports System.Data.Odbc
 Public Class frmDownloadZip
     Private Sub Downloader()
         Dim ZipLocation As String = My.Application.Info.DirectoryPath & "\honor.zip"
@@ -11,10 +9,10 @@ Public Class frmDownloadZip
         'sample url "https:\\www.colorado.edu/conflict/peace/download/peace.zip"
         Dim webUri = New Uri(ZipUrl)
         Using client As New WebClient
-            ProgressBar1.Value = ProgressBar1.Minimum
+            'ProgressBar1.Value = ProgressBar1.Minimum
             Try
                 client.DownloadFile(webUri, ZipLocation)
-                ProgressBar1.Value = ProgressBar1.Maximum
+                'ProgressBar1.Value = ProgressBar1.Maximum
                 MessageBox.Show("Zip file Downloaded Successfully")
             Catch ex As Exception
                 MessageBox.Show(ex.Message)
@@ -53,9 +51,45 @@ Public Class frmDownloadZip
     '        MessageBox.Show("Successfully Extracted")
     '    End If
     'End Sub
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        txtTime.Text = Date.Now.ToString("MMMM dd, yyyy hh:mm:ss")   
+    End Sub
+    Private Sub DownloadSave()
+        Dim mysql As String = "SELECT * FROM ", filldata As String = "TBLDOWNLOADZIP"
+        Dim ds As DataSet = LoadSQL(mysql & filldata, filldata)
+
+        Dim dsNewRow As DataRow
+        dsNewRow = ds.Tables(filldata).NewRow
+        With dsNewRow
+            .Item("TRANSDATE") = dtpSetDateDownload.Text
+        End With
+        ds.Tables(filldata).Rows.Add(dsNewRow)
+
+        database.SaveEntry(ds)
+        MsgBox("Save")
+    End Sub
+    'Private Sub CheckSetDate()
+    '    dbOpen()
+    '    Dim mysql As String = "SELECT * FROM TBLDOWNLOADZIP WHERE TRANSDATE "
+    '    Dim cmd As OdbcCommand = New OdbcCommand(mysql, con)
+    '    Using reader As OdbcDataReader = cmd.ExecuteReader()
+    '        If reader.HasRows Then
+    '        End If
+    '        dbClose()
+    '    End Using
+    'End Sub
+    Private Sub txtTime_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtTime.TextChanged
+        If txtTime.Text = dtpSetDateDownload.Text Then
+            MessageBox.Show("Okey")
+            'Downloader()
+        End If
+        If txtTime.Text = dtpDateExtract.Text Then
+            'DOSCommandExtract()
+            MsgBox("Okey Pud")
+        End If
+    End Sub
+
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        'Extractor()
         DOSCommandExtract()
-        'Downloader()
     End Sub
 End Class
