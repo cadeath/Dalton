bd98a7721f1bccb7becf2e9601d47aacecd00a2b
cadeath <ecjmaquic@gmail.com>
2016-06-23 14:45:59 +0800

    UPDATE

---
+++ Pawnshop/Pawnshop/Forms/diagOTP.vb
@@ -0,0 +1,10 @@
+﻿Public Class diagOTP
+
+    Private Sub btnSubmit_Click(sender As System.Object, e As System.EventArgs) Handles btnSubmit.Click
+        If otp.VerifyPIN(txtPIN.Text) Then
+            Me.Close()
+        Else
+            MsgBox("INVALID PIN", MsgBoxStyle.Critical)
+        End If
+    End Sub
+End Class
---
+++ Pawnshop/Pawnshop/Modules/otp.vb
@@ -0,0 +1,13 @@
+﻿Imports TOTP
+
+Module otp
+
+    Public tfa As EskieTOTP
+
+    Friend Function VerifyPIN(pin As String) As Boolean
+        Dim isValid As Boolean = tfa.isCorrect(pin)
+
+
+        Return isValid
+    End Function
+End Module
