5e1bb879a791d7d2cc5b193d735adaeed53fae20
blade1992 <misionaellie@gmail.com>
2016-06-23 10:34:44 +0800

    Update Early Redemption Interest
    from 0.02 to 0.025

---
+++ Pawnshop/Fixer/FixInterestHistory/frmFixIntHistory.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmFixIntHistory
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmFixIntHistory))
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.pbLoading = New System.Windows.Forms.ProgressBar()
+        Me.txtDB = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnFix = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblStatus
+        '
+        Me.lblStatus.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Location = New System.Drawing.Point(9, 58)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(31, 13)
+        Me.lblStatus.TabIndex = 18
+        Me.lblStatus.Text = "IDLE"
+        '
+        'pbLoading
+        '
+        Me.pbLoading.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.pbLoading.Location = New System.Drawing.Point(0, 81)
+        Me.pbLoading.Name = "pbLoading"
+        Me.pbLoading.Size = New System.Drawing.Size(274, 17)
+        Me.pbLoading.TabIndex = 17
+        '
+        'txtDB
+        '
+        Me.txtDB.Location = New System.Drawing.Point(12, 21)
+        Me.txtDB.Name = "txtDB"
+        Me.txtDB.Size = New System.Drawing.Size(260, 20)
+        Me.txtDB.TabIndex = 15
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 5)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "Database"
+        '
+        'btnFix
+        '
+        Me.btnFix.Location = New System.Drawing.Point(182, 47)
+        Me.btnFix.Name = "btnFix"
+        Me.btnFix.Size = New System.Drawing.Size(75, 23)
+        Me.btnFix.TabIndex = 19
+        Me.btnFix.Text = "Fix"
+        Me.btnFix.UseVisualStyleBackColor = True
+        '
+        'frmFixIntHistory
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(274, 98)
+        Me.Controls.Add(Me.btnFix)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.pbLoading)
+        Me.Controls.Add(Me.txtDB)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmFixIntHistory"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Fix Interest History"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblStatus As System.Windows.Forms.Label
+    Friend WithEvents pbLoading As System.Windows.Forms.ProgressBar
+    Friend WithEvents txtDB As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnFix As System.Windows.Forms.Button
+End Class
