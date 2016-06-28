831125b9223aa467650c3fa8f3d0144a12dbaaec
marzxxx90 <junmar_honor@ymail.com>
2016-06-28 08:28:05 +0800

    Update

--- Pawnshop/Pawnshop/Forms/diagOTP.vb
+++ Pawnshop/Pawnshop/Forms/diagOTP.vb
@@ -12,4 +12,8 @@ Public Class diagOTP
     Private Sub diagOTP_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
         txtPIN.Text = ""
     End Sub
+
+    Private Sub txtPIN_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtPIN.KeyPress
+        If isEnter(e) Then btnSubmit.PerformClick()
+    End Sub
 End Class
