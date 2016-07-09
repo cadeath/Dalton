eaa2133c99420e911b30b149862b2b5bd6918794
marzxxx90 <junmar_honor@ymail.com>
2016-07-09 17:06:12 +0800

    Update

--- Pawnshop/Pawnshop/Forms/frmBackUpData.vb
+++ Pawnshop/Pawnshop/Forms/frmBackUpData.vb
@@ -10,42 +10,36 @@ Public Class frmBackUpData
         database.dbName = firebird
         txtPath.Text = firebird
     End Sub
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBrowseSavePath.Click
+    Private Sub btnBrowseSavePath_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBrowseSavePath.Click
         If Not fbdBackup.ShowDialog = Windows.Forms.DialogResult.OK Then Exit Sub
 
         txtPath.Text = fbdBackup.SelectedPath
     End Sub
 
-    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBrowseBackup.Click
+    Private Sub btnBrowseBackup_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBrowseBackup.Click
         If Not fbdBackup.ShowDialog = Windows.Forms.DialogResult.OK Then Exit Sub
 
         txtPath2.Text = fbdBackup.SelectedPath
     End Sub
 
     Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
-
         Dim path As String = txtPath.text
 
-        ' This text is added only once to the file. 
-        ' If Not File.Exists(path) Then
-        ' Create a file to write to.
         Using sw As StreamWriter = File.CreateText(path)
             sw.WriteLine("@echo off")
             sw.WriteLine("title cdt-S0ft - MANUAL BACKUP")
             sw.WriteLine("echo PLEASE CLOSE THE PAWNSHOP SYSTEM")
             sw.WriteLine("pause")
             sw.WriteLine("echo PLEASE WAIT WHILE SYSTEM IS BACKING UP")
-            sw.WriteLine("rar a " & txtPath2.Text & "\" & txtBranchName.Text & ".rar -agMMddyyyy W3W1LH4CKU.FDB rar a -ep -hpMISJUNMAR -m0")
+            sw.WriteLine("rar a " & txtPath2.Text & "\" & txtBranchName.Text & ".rar -agMMddyyyy W3W1LH4CKU.FDB rar a -ep -hpMISJUNMAR -m0") 'Password = MISJUNMAR(uppercase) 
             sw.WriteLine("cls ")
-            'sw.WriteLine("echo %" & txtBranchName.Text)
-            'sw.WriteLine("rar a -ep -hpPASSWORDGOESHERE -m0  %" & txtBranchName.Text & ".rar %" & txtBranchName.Text & "")
             sw.WriteLine("echo DONE!!! THANK YOU FOR WAITING")
             sw.WriteLine("pause")
             sw.WriteLine("exit")
 
         End Using
         ' End If
-        MessageBox.Show("Successful")
+        MessageBox.Show("Successful", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
     End Sub
 
     Private Sub frmBackUpData_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
