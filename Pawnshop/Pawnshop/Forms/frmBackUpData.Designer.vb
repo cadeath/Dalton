f3697441033e6bc4353d1617642f4425c23bced1
marzxxx90 <junmar_honor@ymail.com>
2016-07-09 16:55:05 +0800

    Add New Backup Form

---
+++ Pawnshop/Pawnshop/Forms/frmBackUpData.Designer.vb
@@ -0,0 +1,159 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBackUpData
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.fbdBackup = New System.Windows.Forms.FolderBrowserDialog()
+        Me.txtPath = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.txtPath2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.txtBranchName = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(221, 31)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Browse"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'fbdBackup
+        '
+        Me.fbdBackup.SelectedPath = "C:\"
+        '
+        'txtPath
+        '
+        Me.txtPath.Location = New System.Drawing.Point(6, 32)
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.Size = New System.Drawing.Size(209, 20)
+        Me.txtPath.TabIndex = 1
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txtBranchName)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtPath2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtPath)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(302, 163)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(106, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Select Path To Save"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(117, 220)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Back Up"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'txtPath2
+        '
+        Me.txtPath2.Location = New System.Drawing.Point(9, 77)
+        Me.txtPath2.Name = "txtPath2"
+        Me.txtPath2.Size = New System.Drawing.Size(206, 20)
+        Me.txtPath2.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Backup Path"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(221, 74)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Browse"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'txtBranchName
+        '
+        Me.txtBranchName.Location = New System.Drawing.Point(9, 127)
+        Me.txtBranchName.Name = "txtBranchName"
+        Me.txtBranchName.Size = New System.Drawing.Size(206, 20)
+        Me.txtBranchName.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 111)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Branch Name"
+        '
+        'frmBackUpData
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(323, 255)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frmBackUpData"
+        Me.Text = "BackUp Data"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents fbdBackup As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents txtPath As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtPath2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtBranchName As System.Windows.Forms.TextBox
+End Class
---
+++ Pawnshop/Pawnshop/Forms/frmBackUpData.vb
@@ -0,0 +1,55 @@
+﻿Imports System.IO
+Public Class frmBackUpData
+    Const DBPATH As String = "\BackUp.bat"
+
+    Private Sub LoadPath()
+        Dim readValue = My.Computer.Registry.GetValue(
+    "HKEY_LOCAL_MACHINE\Software\cdt-S0ft\Pawnshop", "InstallPath", Nothing)
+
+        Dim firebird As String = readValue & DBPATH
+        database.dbName = firebird
+        txtPath.Text = firebird
+    End Sub
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If Not fbdBackup.ShowDialog = Windows.Forms.DialogResult.OK Then Exit Sub
+
+        txtPath.Text = fbdBackup.SelectedPath
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        If Not fbdBackup.ShowDialog = Windows.Forms.DialogResult.OK Then Exit Sub
+
+        txtPath2.Text = fbdBackup.SelectedPath
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+        Dim path As String = txtPath.text
+
+        ' This text is added only once to the file. 
+        ' If Not File.Exists(path) Then
+        ' Create a file to write to.
+        Using sw As StreamWriter = File.CreateText(path)
+            sw.WriteLine("@echo off")
+            sw.WriteLine("title cdt-S0ft - MANUAL BACKUP")
+            sw.WriteLine("echo PLEASE CLOSE THE PAWNSHOP SYSTEM")
+            sw.WriteLine("pause")
+            sw.WriteLine("echo PLEASE WAIT WHILE SYSTEM IS BACKING UP")
+            sw.WriteLine("rar a " & txtPath2.Text & "\" & txtBranchName.Text & ".rar -agMMddyyyy W3W1LH4CKU.FDB rar a -ep -hpMISJUNMAR -m0")
+            sw.WriteLine("cls ")
+            'sw.WriteLine("echo %" & txtBranchName.Text)
+            'sw.WriteLine("rar a -ep -hpPASSWORDGOESHERE -m0  %" & txtBranchName.Text & ".rar %" & txtBranchName.Text & "")
+            sw.WriteLine("echo DONE!!! THANK YOU FOR WAITING")
+            sw.WriteLine("pause")
+            sw.WriteLine("exit")
+
+        End Using
+        ' End If
+        MessageBox.Show("Successful")
+    End Sub
+
+    Private Sub frmBackUpData_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        LoadPath()
+    End Sub
+
+End Class
