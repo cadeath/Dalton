c9e09e08e80515eb212bc4cf5cb52e18991de4e5
blade1992 <misionaellie@gmail.com>
2016-07-13 17:09:16 +0800

    update

--- Pawnshop/Pawnshop/Forms/frmDownloadZip.Designer.vb
+++ Pawnshop/Pawnshop/Forms/frmDownloadZip.Designer.vb
@@ -23,123 +23,66 @@ Partial Class frmDownloadZip
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.lblDownload = New System.Windows.Forms.Label()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
         Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
         Me.SuspendLayout()
-        '
-        'lblDownload
-        '
-        Me.lblDownload.AutoSize = True
-        Me.lblDownload.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, CType(((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic) _
-                Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblDownload.Location = New System.Drawing.Point(52, 11)
-        Me.lblDownload.Name = "lblDownload"
-        Me.lblDownload.Size = New System.Drawing.Size(254, 25)
-        Me.lblDownload.TabIndex = 0
-        Me.lblDownload.Text = "CLICK TO DOWNLOAD"
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(26, 73)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(134, 23)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Start Download"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(26, 102)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(134, 23)
-        Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Cancel Download"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(283, 267)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(31, 13)
-        Me.Label1.TabIndex = 3
-        Me.Label1.Text = "........"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(12, 280)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(46, 13)
-        Me.Label2.TabIndex = 4
-        Me.Label2.Text = "File Size"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(12, 308)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(67, 13)
-        Me.Label3.TabIndex = 5
-        Me.Label3.Text = "Downloaded"
-        '
-        'ProgressBar1
-        '
-        Me.ProgressBar1.Location = New System.Drawing.Point(26, 131)
-        Me.ProgressBar1.Name = "ProgressBar1"
-        Me.ProgressBar1.Size = New System.Drawing.Size(314, 23)
-        Me.ProgressBar1.TabIndex = 6
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(26, 160)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(165, 20)
-        Me.TextBox1.TabIndex = 7
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(26, 186)
+        Me.Button3.Location = New System.Drawing.Point(12, 12)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(82, 31)
+        Me.Button3.Size = New System.Drawing.Size(284, 31)
         Me.Button3.TabIndex = 8
         Me.Button3.Text = "Download"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(26, 223)
+        Me.Button4.Location = New System.Drawing.Point(12, 64)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(82, 23)
+        Me.Button4.Size = New System.Drawing.Size(284, 28)
         Me.Button4.TabIndex = 9
-        Me.Button4.Text = "Download1"
+        Me.Button4.Text = "Extract"
         Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 98)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(284, 20)
+        Me.TextBox1.TabIndex = 10
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 125)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(284, 31)
+        Me.Button1.TabIndex = 11
+        Me.Button1.Text = "Download"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 162)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(284, 23)
+        Me.ProgressBar1.TabIndex = 12
         '
         'frmDownloadZip
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(352, 343)
+        Me.ClientSize = New System.Drawing.Size(311, 191)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.ProgressBar1)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.lblDownload)
         Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Name = "frmDownloadZip"
         Me.Text = "Download Zip"
@@ -147,16 +90,11 @@ Partial Class frmDownloadZip
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents lblDownload As System.Windows.Forms.Label
     Friend WithEvents Timer1 As System.Windows.Forms.Timer
-    Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents Button2 As System.Windows.Forms.Button
-    Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
     Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents Button3 As System.Windows.Forms.Button
     Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
 End Class
--- Pawnshop/Pawnshop/Forms/frmDownloadZip.vb
+++ Pawnshop/Pawnshop/Forms/frmDownloadZip.vb
@@ -1,60 +1,21 @@
 ﻿Imports System.Threading
 Imports System.Net
 Imports System.IO
+Imports System.ComponentModel
 
 Public Class frmDownloadZip
     Dim wc As System.Net.WebClient
 
-    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
-        wc = New System.Net.WebClient
-        AddHandler wc.DownloadProgressChanged, AddressOf OnDownloadProgressChanged
-        AddHandler wc.DownloadFileCompleted, AddressOf OnDownloadFileCompleted
-        wc.DownloadFileAsync(New Uri("https://wwww.dropbox.com/s/i4gf4pz5ovzeker/Advanced%20Converter.rar?dl=1"), Application.StartupPath & "\ " & "Advanced Converter.rar")
-    End Sub
-    Private Sub OnDownloadProgressChanged(ByVal sender As Object, ByVal e As System.Net.DownloadProgressChangedEventArgs)
-        Dim totalsize As Long = e.TotalBytesToReceive
-        Dim downloadedBytes As Long = e.BytesReceived
-        Dim percentage As Integer = e.ProgressPercentage
-        ProgressBar1.Value = e.ProgressPercentage
-        Label1.Text = "%" & ProgressBar1.Value
-        Label2.Text = "File Size: " & totalsize / 1024 / 1000 & "Mb"
-        Label3.Text = "Downloaded: " & downloadedBytes / 1024 / 1000 & "Mb" & " of " & totalsize / 1024 / 1000 & "Mb"
-    End Sub
-
-    Private Sub OnDownloadFileCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.AsyncCompletedEventArgs)
-        If e.Cancelled Then
-            MsgBox("Download Cancelled!", MsgBoxStyle.Information)
-        Else
-            MsgBox("Download Completed.", MsgBoxStyle.Information)
-        End If
-
-    End Sub
-
-
-    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
-        wc.CancelAsync()
-        MsgBox("Download Cancelled!", MsgBoxStyle.Information)
-        Thread.Sleep("4000")
-        My.Computer.FileSystem.DeleteFile("Advanced Converter.rar")
-        Me.Close()
-    End Sub
-
-
-
     Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
-        Dim filename As String = TextBox1.Text
-        Dim dest As String = "C:\Users\MIS\Desktop\New folder (2)\syslog.zip"
 
-        Dim name As String = "https://us-mg6.mail.yahoo.com/neo/launch?.rand=03p45809leaoh/syslog.zip"
+        Dim dest As String = "C:\Users\MIS\Desktop\New folder (2)\syslogerror.rar"
+        Dim name As String = "https://us-mg6.mail.yahoo.com/neo/launch?.rand=0b562mq04194i#407331862/syslogerror.rar"
 
         Dim wr As HttpWebRequest = CType(WebRequest.Create(name), HttpWebRequest)
         Dim ws As HttpWebResponse = CType(wr.GetResponse(), HttpWebResponse)
         Dim str As Stream = ws.GetResponseStream()
 
         Dim a As Integer = ws.ContentLength
-
-
-
         Dim inBuf(a) As Byte
         Dim bytesToRead As Integer = CInt(inBuf.Length)
         Dim bytesRead As Integer = 0
@@ -71,14 +32,56 @@ Public Class frmDownloadZip
         fstr.Write(inBuf, 0, bytesRead)
         str.Close()
         fstr.Close()
-        MessageBox.Show("Downloaded Successfully")
+        MsgBox("Downloaded Successfully", MsgBoxStyle.Information, "Pawnshop")
+
+
     End Sub
 
     Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
-        Dim webClient As New System.Net.WebClient
 
-        webClient.DownloadFile("https://us-mg6.mail.yahoo.com/neo/launch?.rand=03p45809leaoh", "syslog.zip")
-        webClient.Dispose()
+        Dim ZipLocation2 As String = " ""C:\Users\MIS\Desktop\TransFerhere\syslogerror.rar"" "
+        Dim ZipExtracted2 As String = " ""C:\Users\MIS\Desktop\ExtractHere"" "
+        Dim Zlocation As String = "C:\Program Files\WinRAR\WinRAR.exe"
+        Using p1 As New Process
+            p1.StartInfo.FileName = Zlocation
+            p1.StartInfo.Arguments = " x " & ZipLocation2 & " *.* " & ZipExtracted2
+            Try
+                p1.Start()
+                '   MsgBox("Successfully Extracted Data.", MsgBoxStyle.Information, "Pawnshop")
+            Catch ex As Exception
+                MsgBox("File Type Cannot be supported", MsgBoxStyle.Exclamation, "Pawnshop")
+            End Try
+        End Using
+    End Sub
 
+    Public Sub DownloadFile(ByVal _URL As String, ByVal _SaveAs As String)
+        Try
+            Dim _WebClient As New System.Net.WebClient()
+            ' Downloads the resource with the specified URI to a local file.
+            _WebClient.DownloadFile("http://localhost/syslogerror.rar", "C:\Users\MIS\Desktop\TransFerhere\syslogerror.rar")
+        Catch _Exception As Exception
+            ' Error
+            Console.WriteLine("Exception caught in process: {0}", _Exception.ToString())
+        End Try
+    End Sub
+
+    Private Sub _DownloadFileCompleted(ByVal sender As Object, ByVal e As AsyncCompletedEventArgs)
+        ' File download completed
+        Button1.Enabled = Enabled
+        MessageBox.Show("Download completed")
+    End Sub
+
+    Private Sub _DownloadProgressChanged(ByVal sender As Object, ByVal e As System.Net.DownloadProgressChangedEventArgs)
+        ' Update progress bar
+        progressBar1.Value = e.ProgressPercentage
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Button1.Enabled = False
+
+        Dim _WebClient As New System.Net.WebClient()
+        AddHandler _WebClient.DownloadFileCompleted, AddressOf _DownloadFileCompleted
+        AddHandler _WebClient.DownloadProgressChanged, AddressOf _DownloadProgressChanged
+        _WebClient.DownloadFileAsync(New Uri("http://localhost/syslogerror.zip"), "C:\Users\MIS\Desktop\TransFerhere\syslogerror.zip")
     End Sub
 End Class
