f48cc0a3bd419ece19fcf66aea4e96800a7ca045
blade1992 <misionaellie@gmail.com>
2016-07-20 16:57:40 +0800

    update

--- Pawnshop/Pawnshop/My Project/Application.Designer.vb
+++ Pawnshop/Pawnshop/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Pawnshop.Extract_tableTo_excel
+            Me.MainForm = Global.Pawnshop.ExtractTableFromDatabase
         End Sub
     End Class
 End Namespace
