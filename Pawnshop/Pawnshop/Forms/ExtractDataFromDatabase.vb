7d05c975bd08de12ebf811752f6a967ff241f2af
blade1992 <misionaellie@gmail.com>
2016-07-21 10:53:14 +0800

    Update New

--- Pawnshop/Pawnshop/Forms/ExtractDataFromDatabase.vb
+++ Pawnshop/Pawnshop/Forms/ExtractDataFromDatabase.vb
@@ -3,7 +3,6 @@ Imports System.Data.Odbc
 Public Class ExtractDataFromDatabase
 
     Private Sub ExtractDataFromDatabase_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        'FormInit()
         txtPath.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
     End Sub
 
@@ -20,63 +19,42 @@ Public Class ExtractDataFromDatabase
     End Enum
     Friend FormType As Extract = Extract.Pawn
 
-    'Private Sub FormInit()
-    '    Dim selectedDate As Date = MonCalendar.SelectionStart
-    '    Select Case FormType
-    '        Case Extract.Pawn
-    '            Dim Pawning As String = "Pawning"
-    '            Console.WriteLine("Pawning Activated")
-    '            sfdPath.FileName = String.Format("{2}{1}{0}.xls", Pawning, selectedDate.ToString("MMddyyyy"), BranchCode)  'BranchCode + Date
-    '            Me.Text &= " - Pawning"
-    '        Case Extract.Dollar
-    '            Console.WriteLine("Dollar Activated")
-    '            sfdPath.FileName = String.Format("{1}{0}.xls", selectedDate.ToString("MMddyyyy"), BranchCode)  'BranchCode + Date
-    '            Me.Text &= " - Dollar"
-    '        Case Extract.Borrowing
-    '            Console.WriteLine("Borrowing Activated")
-    '            sfdPath.FileName = String.Format("{1}{0}.xls", selectedDate.ToString("MMddyyyy"), BranchCode)  'BranchCode + Date
-    '            Me.Text &= " - Borrowing"
-    '        Case Extract.Insurance
-    '            Console.WriteLine("Insurance Activated")
-    '            sfdPath.FileName = String.Format("{1}{0}.xls", selectedDate.ToString("MMddyyyy"), BranchCode)  'BranchCode + Date
-    '            Me.Text &= " - Insurance"
-    '        Case Extract.Remitance
-    '            Console.WriteLine("Remitance Activated")
-    '            sfdPath.FileName = String.Format("{1}{0}.xls", selectedDate.ToString("MMddyyyy"), BranchCode)  'BranchCode + Date
-    '            Me.Text &= " - Remitance"
-    '    End Select
-    'End Sub
-
+#Region "Extract Database Table"
     Private Sub btnPawnExtract_Click(sender As System.Object, e As System.EventArgs) Handles btnPawnExtract.Click
 
         Dim sd As Date = MonCalendar.SelectionStart, lineNum As Integer = 0
         Dim stDay = GetFirstDate(MonCalendar.SelectionStart)
         Dim laDay = GetLastDate(MonCalendar.SelectionEnd)
+
+        Dim str As String = "Pawning"
         sfdPath.FileName = String.Format("{1}{0}.xls", sd.ToString("MMddyyyy"), BranchCode)  'BranchCode + Date
+
+
         Dim mySql As String
         mySql = " SELECT P.ADVINT,P.APPRAISAL,G.FULLNAME AS APPRAISER," & _
-    vbCrLf & "P.AUCTIONDATE,CLASS.CATEGORY,C.FIRSTNAME || ' ' || C.LASTNAME AS CLIENT, " & _
-    vbCrLf & " P.DAYSOVERDUE, P.DELAYINT, P.DESCRIPTION,P.EARLYREDEEM ,P.ENCODERID, P.EVAT, " & _
-     vbCrLf & "P.EXPIRYDATE, P.GRAMS,P.INTEREST,P.INT_CHECKSUM, P.ITEMTYPE, P.KARAT," & _
-    vbCrLf & " P.LESSPRINCIPAL, P.LOANDATE ,P.MATUDATE, P.NETAMOUNT," & _
-    vbCrLf & " P.OLDTICKET, P.ORDATE, P.ORNUM, P.PAWNID, P.PAWNTICKET,	" & _
-    vbCrLf & " P.PENALTY, P.PRINCIPAL, P.PULLOUT, P.REDEEMDUE	,P.RENEWDUE," & _
-    vbCrLf & " P.SERVICECHARGE," & _
-      vbCrLf & "Case P.STATUS" & _
-     vbCrLf & " WHEN '0' THEN 'RENEWED' WHEN 'R' THEN 'RENEW' " & _
-         vbCrLf & "WHEN 'L' THEN 'NEW' WHEN 'V' THEN 'VOID' WHEN 'W' THEN 'WITHDRAW' " & _
-         vbCrLf & "WHEN 'X' THEN 'REDEEM' WHEN 'S' THEN 'SEGRE' " & _
-         vbCrLf & "ELSE 'N/A' END AS STATUS,P.SYSTEMINFO" & _
- vbCrLf & "FROM TBLPAWN P" & _
- vbCrLf & "INNER JOIN TBLCLASS CLASS" & _
-  vbCrLf & "ON CLASS.CLASSID = P.CATID" & _
- vbCrLf & "INNER JOIN TBL_GAMIT G" & _
-  vbCrLf & "ON G.USERID = P.APPRAISERID" & _
- vbCrLf & "INNER JOIN TBLCLIENT C" & _
-  vbCrLf & "ON C.CLIENTID =P.CLIENTID" & _
-  vbCrLf & String.Format(" WHERE LOANDATE BETWEEN '{0}' AND '{1}'", stDay.ToShortDateString, laDay.ToShortDateString) & _
-  vbCrLf & "AND ORDATE <> '01/01/0001'" & _
- vbCrLf & "ORDER BY P.LOANDATE ASC;"
+        vbCrLf & "P.AUCTIONDATE,CLASS.CATEGORY,C.FIRSTNAME || ' ' || C.LASTNAME AS CLIENT, " & _
+        vbCrLf & " P.DAYSOVERDUE, P.DELAYINT, P.DESCRIPTION,P.EARLYREDEEM ,P.ENCODERID, P.EVAT, " & _
+        vbCrLf & "P.EXPIRYDATE, P.GRAMS,P.INTEREST, P.ITEMTYPE, P.KARAT," & _
+        vbCrLf & " P.LESSPRINCIPAL, P.LOANDATE ,P.MATUDATE, P.NETAMOUNT," & _
+        vbCrLf & " P.OLDTICKET, P.ORDATE, P.ORNUM, P.PAWNID, P.PAWNTICKET,	" & _
+        vbCrLf & " P.PENALTY, P.PRINCIPAL, P.PULLOUT, P.REDEEMDUE	,P.RENEWDUE," & _
+        vbCrLf & " P.SERVICECHARGE," & _
+        vbCrLf & "Case P.STATUS" & _
+        vbCrLf & " WHEN '0' THEN 'RENEWED' WHEN 'R' THEN 'RENEW' " & _
+        vbCrLf & "WHEN 'L' THEN 'NEW' WHEN 'V' THEN 'VOID' WHEN 'W' THEN 'WITHDRAW' " & _
+        vbCrLf & "WHEN 'X' THEN 'REDEEM' WHEN 'S' THEN 'SEGRE' " & _
+        vbCrLf & "ELSE 'N/A' END AS STATUS,P.SYSTEMINFO" & _
+        vbCrLf & "FROM TBLPAWN P" & _
+        vbCrLf & "INNER JOIN TBLCLASS CLASS" & _
+        vbCrLf & "ON CLASS.CLASSID = P.CATID" & _
+        vbCrLf & "INNER JOIN TBL_GAMIT G" & _
+        vbCrLf & "ON G.USERID = P.APPRAISERID" & _
+        vbCrLf & "INNER JOIN TBLCLIENT C" & _
+        vbCrLf & "ON C.CLIENTID =P.CLIENTID" & _
+        vbCrLf & String.Format(" WHERE LOANDATE BETWEEN '{0}' AND '{1}'", stDay.ToShortDateString, laDay.ToShortDateString) & _
+        vbCrLf & "AND ORDATE <> '01/01/0001'" & _
+        vbCrLf & "ORDER BY P.LOANDATE ASC;"
+
         Dim ds As DataSet = LoadSQL(mySql)
         Dim MaxEntries As Integer = 0
         MaxEntries = ds.Tables(0).Rows.Count
@@ -114,26 +92,26 @@ Public Class ExtractDataFromDatabase
                 oSheet.Cells(lineNum + 2, 13) = .Item("EXPIRYDATE")
                 oSheet.Cells(lineNum + 2, 14) = .Item("GRAMS")
                 oSheet.Cells(lineNum + 2, 15) = .Item("INTEREST")
-                oSheet.Cells(lineNum + 2, 16) = .Item("INT_CHECKSUM")
-                oSheet.Cells(lineNum + 2, 17) = .Item("ITEMTYPE")
-                oSheet.Cells(lineNum + 2, 18) = .Item("KARAT")
-                oSheet.Cells(lineNum + 2, 19) = .Item("LESSPRINCIPAL")
-                oSheet.Cells(lineNum + 2, 20) = .Item("LOANDATE")
-                oSheet.Cells(lineNum + 2, 21) = .Item("MATUDATE")
-                oSheet.Cells(lineNum + 2, 22) = .Item("NETAMOUNT")
-                oSheet.Cells(lineNum + 2, 23) = .Item("OLDTICKET")
-                oSheet.Cells(lineNum + 2, 24) = .Item("ORDATE")
-                oSheet.Cells(lineNum + 2, 25) = .Item("ORNUM")
-                oSheet.Cells(lineNum + 2, 26) = .Item("PAWNID")
-                oSheet.Cells(lineNum + 2, 27) = .Item("PAWNTICKET")
-                oSheet.Cells(lineNum + 2, 28) = .Item("PENALTY")
-                oSheet.Cells(lineNum + 2, 29) = .Item("PRINCIPAL")
-                oSheet.Cells(lineNum + 2, 30) = .Item("PULLOUT")
-                oSheet.Cells(lineNum + 2, 31) = .Item("REDEEMDUE")
-                oSheet.Cells(lineNum + 2, 32) = .Item("RENEWDUE")
-                oSheet.Cells(lineNum + 2, 33) = .Item("SERVICECHARGE")
-                oSheet.Cells(lineNum + 2, 34) = .Item("STATUS")
-                oSheet.Cells(lineNum + 2, 35) = .Item("SYSTEMINFO")
+                ' oSheet.Cells(lineNum + 2, 16) = .Item("INT_CHECKSUM")
+                oSheet.Cells(lineNum + 2, 16) = .Item("ITEMTYPE")
+                oSheet.Cells(lineNum + 2, 17) = .Item("KARAT")
+                oSheet.Cells(lineNum + 2, 18) = .Item("LESSPRINCIPAL")
+                oSheet.Cells(lineNum + 2, 19) = .Item("LOANDATE")
+                oSheet.Cells(lineNum + 2, 20) = .Item("MATUDATE")
+                oSheet.Cells(lineNum + 2, 21) = .Item("NETAMOUNT")
+                oSheet.Cells(lineNum + 2, 22) = .Item("OLDTICKET")
+                oSheet.Cells(lineNum + 2, 23) = .Item("ORDATE")
+                oSheet.Cells(lineNum + 2, 24) = .Item("ORNUM")
+                oSheet.Cells(lineNum + 2, 25) = .Item("PAWNID")
+                oSheet.Cells(lineNum + 2, 26) = .Item("PAWNTICKET")
+                oSheet.Cells(lineNum + 2, 27) = .Item("PENALTY")
+                oSheet.Cells(lineNum + 2, 28) = .Item("PRINCIPAL")
+                oSheet.Cells(lineNum + 2, 29) = .Item("PULLOUT")
+                oSheet.Cells(lineNum + 2, 30) = .Item("REDEEMDUE")
+                oSheet.Cells(lineNum + 2, 31) = .Item("RENEWDUE")
+                oSheet.Cells(lineNum + 2, 32) = .Item("SERVICECHARGE")
+                oSheet.Cells(lineNum + 2, 33) = .Item("STATUS")
+                'oSheet.Cells(lineNum + 2, 35) = .Item("SYSTEMINFO")
                 lineNum += 1
                 recCnt2 += 1
             End With
@@ -147,7 +125,6 @@ Public Class ExtractDataFromDatabase
             Case Extract.Pawn
                 Console.WriteLine("Pawning Activated")
                 sfdPath.FileName = String.Format("{2}{1}{0}.xls", "Pawn", sd.ToString("MMddyyyy"), BranchCode)  'BranchCode + Date
-                Me.Text &= " - Pawning"
         End Select
 
 
@@ -176,16 +153,19 @@ Public Class ExtractDataFromDatabase
         Dim sd As Date = MonCalendar.SelectionStart, lineNum As Integer = 0
         Dim stDay = GetFirstDate(MonCalendar.SelectionStart)
         Dim laDay = GetLastDate(MonCalendar.SelectionEnd)
-        sfdPath.FileName = String.Format("{1}{0}.xls", sd.ToString("MMddyyyy"), BranchCode)  'BranchCode + Date
+
+        Dim str As String = "Dollar"
+        sfdPath.FileName = String.Format("{2}{1}{0}.xls", sd.ToString("MMddyyyy"), BranchCode, str)  'BranchCode + Date
+
         Dim mySql As String
         mySql = "SELECT D.CLIENTID,D.DENOMINATION,D.DOLLARID,D.FULLNAME," & _
-   vbCrLf & "D.NETAMOUNT,D.PESORATE, D.REMARKS,D.SERIAL," & _
-            vbCrLf & "Case D.STATUS" & _
-    vbCrLf & "WHEN 'A' THEN 'ACTIVE'  WHEN 'V' THEN 'VOID'  ELSE 'N/A'  " & _
-    vbCrLf & "END AS STATUS, D.SYSTEMINFO, D.TRANSDATE, D.USERID " & _
-    vbCrLf & "FROM TBLDOLLAR D" & _
-    vbCrLf & String.Format(" WHERE TRANSDATE BETWEEN'{0}' AND '{1}'", stDay.ToShortDateString, laDay.ToShortDateString) & _
-    vbCrLf & "ORDER BY TRANSDATE ASC"
+        vbCrLf & "D.NETAMOUNT,D.PESORATE, D.REMARKS,D.SERIAL," & _
+        vbCrLf & "Case D.STATUS" & _
+        vbCrLf & "WHEN 'A' THEN 'ACTIVE'  WHEN 'V' THEN 'VOID'  ELSE 'N/A'  " & _
+        vbCrLf & "END AS STATUS, D.SYSTEMINFO, D.TRANSDATE, D.USERID " & _
+        vbCrLf & "FROM TBLDOLLAR D" & _
+        vbCrLf & String.Format(" WHERE TRANSDATE BETWEEN'{0}' AND '{1}'", stDay.ToShortDateString, laDay.ToShortDateString) & _
+        vbCrLf & "ORDER BY TRANSDATE ASC"
 
         Dim ds As DataSet = LoadSQL(mySql)
         Dim MaxEntries As Integer = 0
@@ -231,11 +211,10 @@ Public Class ExtractDataFromDatabase
 
         Dim verified_url As String
 
-         Select Case FormType
+        Select Case FormType
             Case Extract.Dollar
                 Console.WriteLine("Dollar Activated")
                 sfdPath.FileName = String.Format("{1}{0}.xls", sd.ToString("MMddyyyy"), BranchCode)  'BranchCode + Date
-                Me.Text &= " - Dollar"
         End Select
 
 
@@ -265,18 +244,23 @@ Public Class ExtractDataFromDatabase
         Dim sd As Date = MonCalendar.SelectionStart, lineNum As Integer = 0
         Dim stDay = GetFirstDate(MonCalendar.SelectionStart)
         Dim laDay = GetLastDate(MonCalendar.SelectionEnd)
-        sfdPath.FileName = String.Format("{1}{0}.xls", sd.ToString("MMddyyyy"), BranchCode)  'BranchCode + Date
+
+        Dim str As String = "Borrowings"
+        sfdPath.FileName = String.Format("{2}{1}{0}.xls", sd.ToString("MMddyyyy"), BranchCode, str)  'BranchCode + Date
+
         Dim mySql As String
         mySql = "SELECT B.AMOUNT, B.BRANCHCODE, B.BRANCHNAME, " & _
-         vbCrLf & "B.BRWID,  G.FULLNAME,B.REASON,B.REFNUM, B.REMARKS," & _
-             vbCrLf & " Case B.STATUS" & _
-     vbCrLf & "WHEN 'C' THEN 'CASH-OUT'" & _
-     vbCrLf & "WHEN 'D' THEN 'CASH-IN'" & _
-     vbCrLf & "ELSE 'N/A' END AS STATUS, B.SYSTEMINFO, B.TRANSDATE" & _
-     vbCrLf & "FROM TBLBORROW B" & _
-    vbCrLf & "INNER JOIN TBL_GAMIT G ON G.ENCODERID = B.ENCODERID" & _
-vbCrLf & String.Format(" WHERE TRANSDATE BETWEEN'{0}' AND '{1}'", stDay.ToShortDateString, laDay.ToShortDateString) & _
+        vbCrLf & "B.BRWID,  G.FULLNAME,B.REASON,B.REFNUM, B.REMARKS," & _
+        vbCrLf & " Case B.STATUS" & _
+        vbCrLf & "WHEN 'C' THEN 'CASH-OUT'" & _
+        vbCrLf & "WHEN 'D' THEN 'CASH-IN'" & _
+        vbCrLf & "ELSE 'N/A' END AS STATUS, B.SYSTEMINFO, B.TRANSDATE" & _
+        vbCrLf & "FROM TBLBORROW B" & _
+        vbCrLf & "INNER JOIN TBL_GAMIT G ON G.ENCODERID = B.ENCODERID" & _
+        vbCrLf & String.Format(" WHERE TRANSDATE BETWEEN'{0}' AND '{1}'", stDay.ToShortDateString, laDay.ToShortDateString) & _
         vbCrLf & "ORDER BY TRANSDATE ASC"
+
+
         Dim ds As DataSet = LoadSQL(mySql)
         Dim MaxEntries As Integer = 0
         MaxEntries = ds.Tables(0).Rows.Count
@@ -298,7 +282,6 @@ vbCrLf & String.Format(" WHERE TRANSDATE BETWEEN'{0}' AND '{1}'", stDay.ToShortD
         Dim recCnt2 As Single = 0
         While recCnt2 < MaxEntries
             With ds.Tables(0).Rows(recCnt2)
-
                 oSheet.Cells(lineNum + 2, 1) = .Item("AMOUNT").ToString
                 oSheet.Cells(lineNum + 2, 2) = .Item("BRANCHCODE")
                 oSheet.Cells(lineNum + 2, 3) = .Item("BRANCHNAME")
@@ -319,7 +302,7 @@ vbCrLf & String.Format(" WHERE TRANSDATE BETWEEN'{0}' AND '{1}'", stDay.ToShortD
         End While
 
         Dim verified_url As String
- Select Case FormType
+        Select Case FormType
             Case Extract.Borrowing
                 Console.WriteLine("Borrowing Activated")
                 sfdPath.FileName = String.Format("{1}{0}.xls", sd.ToString("MMddyyyy"), BranchCode)  'BranchCode + Date
@@ -352,7 +335,10 @@ vbCrLf & String.Format(" WHERE TRANSDATE BETWEEN'{0}' AND '{1}'", stDay.ToShortD
         Dim sd As Date = MonCalendar.SelectionStart, lineNum As Integer = 0
         Dim stDay = GetFirstDate(MonCalendar.SelectionStart)
         Dim laDay = GetLastDate(MonCalendar.SelectionEnd)
-        sfdPath.FileName = String.Format("{1}{0}.xls", sd.ToString("MMddyyyy"), BranchCode)  'BranchCode + Date
+        Dim str As String = "Insurance"
+        sfdPath.FileName = String.Format("{2}{1}{0}.xls", sd.ToString("MMddyyyy"), BranchCode, str)  'BranchCode + Date
+
+
         Dim mySql As String
         mySql = " SELECT I.AMOUNT, I.CLIENTID,I.CLIENTNAME, I.COINO, G.FULLNAME AS ENCODER, I.INSURANCEID,I.PAWNTICKET, " & _
             vbCrLf & "Case I.STATUS " & _
@@ -361,6 +347,8 @@ vbCrLf & String.Format(" WHERE TRANSDATE BETWEEN'{0}' AND '{1}'", stDay.ToShortD
             vbCrLf & "INNER JOIN TBL_GAMIT G ON G.ENCODERID = I.ENCODERID" & _
             vbCrLf & String.Format("WHERE I.TRANSDATE BETWEEN '{0}' AND '{1}'", stDay.ToShortDateString, laDay.ToShortDateString) & _
             vbCrLf & "ORDER BY TRANSDATE ASC"
+
+
         Dim ds As DataSet = LoadSQL(mySql)
         Dim MaxEntries As Integer = 0
         MaxEntries = ds.Tables(0).Rows.Count
@@ -408,7 +396,6 @@ vbCrLf & String.Format(" WHERE TRANSDATE BETWEEN'{0}' AND '{1}'", stDay.ToShortD
             Case Extract.Insurance
                 Console.WriteLine("Insurance Activated")
                 sfdPath.FileName = String.Format("{1}{0}.xls", sd.ToString("MMddyyyy"), BranchCode)  'BranchCode + Date
-                Me.Text &= " - Insurance"
         End Select
 
 
@@ -436,32 +423,37 @@ vbCrLf & String.Format(" WHERE TRANSDATE BETWEEN'{0}' AND '{1}'", stDay.ToShortD
         Dim sd As Date = MonCalendar.SelectionStart, lineNum As Integer = 0
         Dim stDay = GetFirstDate(MonCalendar.SelectionStart)
         Dim laDay = GetLastDate(MonCalendar.SelectionEnd)
-        sfdPath.FileName = String.Format("{1}{0}.xls", sd.ToString("MMddyyyy"), BranchCode)  'BranchCode + Date
+
+
+        Dim srt As String = " Remitance"
+        sfdPath.FileName = String.Format("{2}{1}{0}.xls", sd.ToString("MMddyyyy"), BranchCode, srt)  'BranchCode + Date
+
+
         Dim mySql As String
         mySql = "SELECT M.AMOUNT, M.COMMISSION,G.FULLNAME AS ENCODER,M.ID, M.LOCATION," & _
-                vbCrLf & "Case M.MONEYTRANS" & _
-      vbCrLf & "WHEN 0 THEN 'SENDIN' WHEN 1 THEN 'PAYOUT' ELSE 'NA'" & _
-    vbCrLf & "END AS MONEYTRANS,M.NETAMOUNT,M.RECEIVERID,M.RECEIVERNAME, " & _
-  vbCrLf & "CASE WHEN M.ServiceType = 'Pera Padala' AND M.MoneyTrans = 0" & _
-      vbCrLf & "THEN 'ME# ' || LPAD(M.TransID,5,0)" & _
-    vbCrLf & "WHEN M.SERVICETYPE = 'Pera Padala' AND M.MONEYTRANS = 1" & _
-      vbCrLf & "THEN 'MR# ' || LPAD(M.TRANSID,5,0)" & _
-      vbCrLf & "ELSE RefNum END as RefNum," & _
-     vbCrLf & "M.REMARKS," & _
-   vbCrLf & " M.SENDERID,M.SENDERNAME,M.SERVICECHARGE, " & _
-    vbCrLf & "M.SERVICETYPE, " & _
-            vbCrLf & "Case M.STATUS" & _
-     vbCrLf & "WHEN 'A' THEN 'ACTIVE'" & _
-      vbCrLf & "  WHEN 'V' THEN 'VOID'" & _
-       vbCrLf & " ELSE 'N/A'" & _
-   vbCrLf & " END AS STATUS, M.SYSTEMINFO, M.TRANSDATE," & _
-       vbCrLf & " M.TRANSID" & _
- vbCrLf & " FROM TBLMONEYTRANSFER M" & _
- vbCrLf & " INNER JOIN TBL_GAMIT G ON G.ENCODERID = M.ENCODERID" & _
-  vbCrLf & "INNER JOIN TBLCLIENT C ON  M.SENDERID = C.CLIENTID" & _
-  vbCrLf & "INNER JOIN TBLCLIENT R ON  M.RECEIVERID = R.CLIENTID" & _
-vbCrLf & String.Format("WHERE M.TRANSDATE BETWEEN '{0}' AND '{1}'", stDay.ToShortDateString, laDay.ToShortDateString) & _
-vbCrLf & " ORDER BY M.TRANSDATE;"
+        vbCrLf & "Case M.MONEYTRANS" & _
+        vbCrLf & "WHEN 0 THEN 'SENDIN' WHEN 1 THEN 'PAYOUT' ELSE 'NA'" & _
+        vbCrLf & "END AS MONEYTRANS,M.NETAMOUNT,M.RECEIVERID,M.RECEIVERNAME, " & _
+        vbCrLf & "CASE WHEN M.ServiceType = 'Pera Padala' AND M.MoneyTrans = 0" & _
+        vbCrLf & "THEN 'ME# ' || LPAD(M.TransID,5,0)" & _
+        vbCrLf & "WHEN M.SERVICETYPE = 'Pera Padala' AND M.MONEYTRANS = 1" & _
+        vbCrLf & "THEN 'MR# ' || LPAD(M.TRANSID,5,0)" & _
+        vbCrLf & "ELSE RefNum END as RefNum," & _
+        vbCrLf & "M.REMARKS," & _
+        vbCrLf & " M.SENDERID,M.SENDERNAME,M.SERVICECHARGE, " & _
+        vbCrLf & "M.SERVICETYPE, " & _
+        vbCrLf & "Case M.STATUS" & _
+        vbCrLf & "WHEN 'A' THEN 'ACTIVE'" & _
+        vbCrLf & "  WHEN 'V' THEN 'VOID'" & _
+        vbCrLf & " ELSE 'N/A'" & _
+        vbCrLf & " END AS STATUS, M.SYSTEMINFO, M.TRANSDATE," & _
+        vbCrLf & " M.TRANSID" & _
+        vbCrLf & " FROM TBLMONEYTRANSFER M" & _
+        vbCrLf & " INNER JOIN TBL_GAMIT G ON G.ENCODERID = M.ENCODERID" & _
+        vbCrLf & "INNER JOIN TBLCLIENT C ON  M.SENDERID = C.CLIENTID" & _
+        vbCrLf & "INNER JOIN TBLCLIENT R ON  M.RECEIVERID = R.CLIENTID" & _
+        vbCrLf & String.Format("WHERE M.TRANSDATE BETWEEN '{0}' AND '{1}'", stDay.ToShortDateString, laDay.ToShortDateString) & _
+        vbCrLf & " ORDER BY M.TRANSDATE;"
 
         Dim ds As DataSet = LoadSQL(mySql)
         Dim MaxEntries As Integer = 0
@@ -513,11 +505,10 @@ vbCrLf & " ORDER BY M.TRANSDATE;"
 
         Dim verified_url As String
 
-          Select Case FormType
-            Case Extract.Remitance
+        Select Case FormType
+            Case Extract.Dollar
                 Console.WriteLine("Remitance Activated")
                 sfdPath.FileName = String.Format("{1}{0}.xls", sd.ToString("MMddyyyy"), BranchCode)  'BranchCode + Date
-                Me.Text &= " - Remitance"
         End Select
 
 
@@ -540,4 +531,5 @@ vbCrLf & " ORDER BY M.TRANSDATE;"
 
         MsgBox("Remitance Extracted", MsgBoxStyle.Information)
     End Sub
+#End Region
 End Class
