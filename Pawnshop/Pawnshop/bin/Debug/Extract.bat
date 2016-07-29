aede62d0b017f41b53a86fa983303fb178814187
blade1992 <misionaellie@gmail.com>
2016-07-29 08:22:00 +0800

    UPDATE

--- Pawnshop/Pawnshop/bin/Debug/Extract.bat
+++ Pawnshop/Pawnshop/bin/Debug/Extract.bat
@@ -3,7 +3,7 @@ title cdt-S0ft - Extract
 echo Extracting. . .
 pause
 echo PLEASE WAIT WHILE SYSTEM Extracting...
-rar a C:\Users\MIS\Desktop\JCA.rar -agMMddyyyy RemitanceJCA07282016.xlsx -hpJCAMIS
+rar a C:\Users\MIS\Desktop\DailyROX.rar -agMMddyyyy PawningROX07292016.xlsx -hpROXMIS
 cls 
 echo DONE!!! THANK YOU FOR WAITING
 pause
