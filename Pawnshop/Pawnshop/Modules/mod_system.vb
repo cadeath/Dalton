72b83357e2e10dd23d60f905d9385efdd2ec1e05
blade1992 <misionaellie@gmail.com>
2016-07-29 10:07:29 +0800

    UPDATE

--- Pawnshop/Pawnshop/Modules/mod_system.vb
+++ Pawnshop/Pawnshop/Modules/mod_system.vb
@@ -18,7 +18,7 @@ Module mod_system
     ''' </summary>
     ''' <remarks></remarks>
 #Region "Global Variables"
-    Public DEV_MODE As Boolean = True
+    Public DEV_MODE As Boolean = False
     Public PROTOTYPE As Boolean = False
     Public ADS_ESKIE As Boolean = True
     Public ADS_SHOW As Boolean = False
