7fcb52f26a2c5f691746a5e56f10be90455e4005
cadeath <ecjmaquic@gmail.com>
2016-08-08 18:13:57 +0800

    UPDATE

--- Pawnshop/Pawnshop/AuditConsole/frmAuditConsole.Designer.vb
+++ Pawnshop/Pawnshop/AuditConsole/frmAuditConsole.Designer.vb
@@ -23,58 +23,160 @@ Partial Class frmAuditConsole
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmAuditConsole))
-        Me.btnCashCount = New System.Windows.Forms.Button()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.btnCCSheet = New System.Windows.Forms.Button()
+        Me.btnCashCount = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.cboType = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnVault = New System.Windows.Forms.Button()
+        Me.txtPrincipal = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.MonVault = New System.Windows.Forms.MonthCalendar()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
-        'btnCashCount
+        'GroupBox1
         '
-        Me.btnCashCount.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnCashCount.Location = New System.Drawing.Point(12, 12)
-        Me.btnCashCount.Name = "btnCashCount"
-        Me.btnCashCount.Size = New System.Drawing.Size(126, 69)
-        Me.btnCashCount.TabIndex = 0
-        Me.btnCashCount.Text = "Cash Count"
-        Me.btnCashCount.UseVisualStyleBackColor = True
-        '
-        'Button1
-        '
-        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(12, 162)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(126, 69)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "VAULT INVENTORY"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.GroupBox1.Controls.Add(Me.btnCCSheet)
+        Me.GroupBox1.Controls.Add(Me.btnCashCount)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(467, 105)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Cash Count"
         '
         'btnCCSheet
         '
         Me.btnCCSheet.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnCCSheet.Location = New System.Drawing.Point(12, 87)
+        Me.btnCCSheet.Location = New System.Drawing.Point(144, 19)
         Me.btnCCSheet.Name = "btnCCSheet"
         Me.btnCCSheet.Size = New System.Drawing.Size(126, 69)
-        Me.btnCCSheet.TabIndex = 2
+        Me.btnCCSheet.TabIndex = 4
         Me.btnCCSheet.Text = "Cash Count Sheet"
         Me.btnCCSheet.UseVisualStyleBackColor = True
+        '
+        'btnCashCount
+        '
+        Me.btnCashCount.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCashCount.Location = New System.Drawing.Point(12, 19)
+        Me.btnCashCount.Name = "btnCashCount"
+        Me.btnCashCount.Size = New System.Drawing.Size(126, 69)
+        Me.btnCashCount.TabIndex = 3
+        Me.btnCashCount.Text = "Cash Count"
+        Me.btnCashCount.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.cboType)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.btnVault)
+        Me.GroupBox2.Controls.Add(Me.txtPrincipal)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Controls.Add(Me.MonVault)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 123)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(467, 220)
+        Me.GroupBox2.TabIndex = 4
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Vault Inventory"
+        '
+        'cboType
+        '
+        Me.cboType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboType.FormattingEnabled = True
+        Me.cboType.Location = New System.Drawing.Point(254, 118)
+        Me.cboType.Name = "cboType"
+        Me.cboType.Size = New System.Drawing.Size(121, 21)
+        Me.cboType.TabIndex = 12
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(251, 99)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 16)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Item Type"
+        '
+        'btnVault
+        '
+        Me.btnVault.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnVault.Location = New System.Drawing.Point(370, 171)
+        Me.btnVault.Name = "btnVault"
+        Me.btnVault.Size = New System.Drawing.Size(84, 37)
+        Me.btnVault.TabIndex = 10
+        Me.btnVault.Text = "&View"
+        Me.btnVault.UseVisualStyleBackColor = True
+        '
+        'txtPrincipal
+        '
+        Me.txtPrincipal.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPrincipal.Location = New System.Drawing.Point(254, 65)
+        Me.txtPrincipal.Name = "txtPrincipal"
+        Me.txtPrincipal.Size = New System.Drawing.Size(200, 22)
+        Me.txtPrincipal.TabIndex = 9
+        Me.txtPrincipal.Text = "0.00"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(251, 46)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(203, 16)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Principal (Equal or Greater Than)"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(9, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 16)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Vault as Of"
+        '
+        'MonVault
+        '
+        Me.MonVault.Location = New System.Drawing.Point(12, 46)
+        Me.MonVault.Name = "MonVault"
+        Me.MonVault.TabIndex = 6
         '
         'frmAuditConsole
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(893, 385)
-        Me.Controls.Add(Me.btnCCSheet)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.btnCashCount)
+        Me.ClientSize = New System.Drawing.Size(490, 352)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
         Me.Name = "frmAuditConsole"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Audit Console"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
-    Friend WithEvents btnCashCount As System.Windows.Forms.Button
-    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
     Friend WithEvents btnCCSheet As System.Windows.Forms.Button
+    Friend WithEvents btnCashCount As System.Windows.Forms.Button
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents MonVault As System.Windows.Forms.MonthCalendar
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtPrincipal As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnVault As System.Windows.Forms.Button
+    Friend WithEvents cboType As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
 End Class
--- Pawnshop/Pawnshop/AuditConsole/frmAuditConsole.vb
+++ Pawnshop/Pawnshop/AuditConsole/frmAuditConsole.vb
@@ -1,6 +1,33 @@
 ﻿Public Class frmAuditConsole
 
-    Private Sub btnCashCount_Click(sender As System.Object, e As System.EventArgs) Handles btnCashCount.Click
+    Private MEMO_MINIMUM As Double = 5000
+
+    Private Sub btnVault_Click(sender As System.Object, e As System.EventArgs) Handles btnVault.Click
+        Dim AMOUNT_MIN As Double
+        AMOUNT_MIN = CDbl(txtPrincipal.Text)
+
+        AuditReports.Min_Principal(AMOUNT_MIN, MonVault.SelectionRange.Start.ToShortDateString, cboType.Text)
+    End Sub
+
+    Private Sub Load_ItemType()
+        Dim mySql As String = "SELECT DISTINCT TYPE FROM TBLCLASS ORDER BY TYPE ASC"
+        Dim ds As DataSet = LoadSQL(mySql)
+
+        cboType.Items.Clear()
+        cboType.Items.Add("ALL")
+        For Each dr As DataRow In ds.Tables(0).Rows
+            cboType.Items.Add(dr("TYPE"))
+        Next
+
+        cboType.SelectedIndex = 0
+    End Sub
+
+    Private Sub frmAuditConsole_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        txtPrincipal.Text = MEMO_MINIMUM.ToString("0.00")
+        Load_ItemType()
+    End Sub
+
+    Private Sub btnCashCount_Click_1(sender As System.Object, e As System.EventArgs) Handles btnCashCount.Click
         frmCashCountV2.isAuditing = True
         frmCashCountV2.Show()
     End Sub
