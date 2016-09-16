d52a0a2a32f36dfbf079c057ea67a92447fc5176
marzxxx90 <junmar_honor@ymail.com>
2016-09-16 14:08:11 +0800

    Update

---
+++ Pawnshop/Pawnshop/Developer/frm_PanelTextbox.vb
@@ -0,0 +1,15 @@
+﻿Public Class frm_PanelTextbox
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSubmit.Click
+        Dim tmpResult As String = txtSearch.Text
+        With dev_NewPawning.lvItem
+            .SelectedItems(0).SubItems(3).Text = tmpResult
+        End With
+    End Sub
+
+    Private Sub txtSearch_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtSearch.KeyPress
+        If isEnter(e) Then
+            btnSubmit.PerformClick()
+        End If
+    End Sub
+End Class
---
+++ Pawnshop/Pawnshop/Developer/frm_PanelYesNo.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_PanelYesNo
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox8 = New System.Windows.Forms.GroupBox()
+        Me.rbYes = New System.Windows.Forms.RadioButton()
+        Me.rbNo = New System.Windows.Forms.RadioButton()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox8.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox8
+        '
+        Me.GroupBox8.Controls.Add(Me.Button1)
+        Me.GroupBox8.Controls.Add(Me.rbYes)
+        Me.GroupBox8.Controls.Add(Me.rbNo)
+        Me.GroupBox8.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox8.Name = "GroupBox8"
+        Me.GroupBox8.Size = New System.Drawing.Size(152, 85)
+        Me.GroupBox8.TabIndex = 22
+        Me.GroupBox8.TabStop = False
+        Me.GroupBox8.Text = "Select Fields"
+        '
+        'rbYes
+        '
+        Me.rbYes.AutoSize = True
+        Me.rbYes.Checked = True
+        Me.rbYes.Location = New System.Drawing.Point(6, 19)
+        Me.rbYes.Name = "rbYes"
+        Me.rbYes.Size = New System.Drawing.Size(43, 17)
+        Me.rbYes.TabIndex = 0
+        Me.rbYes.TabStop = True
+        Me.rbYes.Text = "Yes"
+        Me.rbYes.UseVisualStyleBackColor = True
+        '
+        'rbNo
+        '
+        Me.rbNo.AutoSize = True
+        Me.rbNo.Location = New System.Drawing.Point(96, 19)
+        Me.rbNo.Name = "rbNo"
+        Me.rbNo.Size = New System.Drawing.Size(39, 17)
+        Me.rbNo.TabIndex = 1
+        Me.rbNo.Text = "No"
+        Me.rbNo.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(71, 56)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Submit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'frm_PanelYesNo
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(168, 103)
+        Me.Controls.Add(Me.GroupBox8)
+        Me.Name = "frm_PanelYesNo"
+        Me.GroupBox8.ResumeLayout(False)
+        Me.GroupBox8.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox8 As System.Windows.Forms.GroupBox
+    Friend WithEvents rbYes As System.Windows.Forms.RadioButton
+    Friend WithEvents rbNo As System.Windows.Forms.RadioButton
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+End Class
