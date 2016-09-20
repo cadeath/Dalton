b65cd67f98b28f84b8b7c4fb889a99c32d877210
cadeath <ecjmaquic@gmail.com>
2016-09-20 14:15:21 +0800

    UPDATE

--- Pawnshop/Pawnshop/InterestScheme/dev_Scheme.vb
+++ Pawnshop/Pawnshop/InterestScheme/dev_Scheme.vb
@@ -3,4 +3,15 @@
     Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
         Load_Scheme_Data()
     End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Dim mySql As String = "SELECT * FROM TBLINTSCHEMES"
+        Dim ds As DataSet = LoadSQL(mySql)
+
+        For Each dr As DataRow In ds.Tables(0).Rows
+
+
+
+        Next
+    End Sub
 End Class
