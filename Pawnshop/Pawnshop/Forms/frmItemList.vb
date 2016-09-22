9b0bfdd17fef9f8490f77d716035547ce2a2b82d
Bladier <misionaellie@gmail.com>
2016-09-22 16:01:43 +0800

    update

--- Pawnshop/Pawnshop/Forms/frmItemList.vb
+++ Pawnshop/Pawnshop/Forms/frmItemList.vb
@@ -162,4 +162,7 @@
         txtSearch.Text = ""
     End Sub
 
+    Private Sub btnClose_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
 End Class
