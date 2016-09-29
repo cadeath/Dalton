c7691809b24a7e6ed857ee7a1337ac5dfe4e3210
marzxxx90 <junmar_honor@ymail.com>
2016-09-29 14:35:39 +0800

    Update

---
+++ Pawnshop/Pawnshop/Forms/Extract.Designer.vb
@@ -0,0 +1,187 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Extract
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtPath = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtQuery = New System.Windows.Forms.TextBox()
+        Me.btnExtract = New System.Windows.Forms.Button()
+        Me.ofd = New System.Windows.Forms.OpenFileDialog()
+        Me.sfdPath = New System.Windows.Forms.SaveFileDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.cboTablename = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtSavePath = New System.Windows.Forms.TextBox()
+        Me.lbTableName = New System.Windows.Forms.ListBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtPath
+        '
+        Me.txtPath.Location = New System.Drawing.Point(65, 23)
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.ReadOnly = True
+        Me.txtPath.Size = New System.Drawing.Size(327, 20)
+        Me.txtPath.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Database"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtSavePath)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.cboTablename)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtQuery)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtPath)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(398, 208)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Extract To Excel"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 84)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Query"
+        '
+        'txtQuery
+        '
+        Me.txtQuery.Location = New System.Drawing.Point(65, 81)
+        Me.txtQuery.Multiline = True
+        Me.txtQuery.Name = "txtQuery"
+        Me.txtQuery.Size = New System.Drawing.Size(327, 88)
+        Me.txtQuery.TabIndex = 3
+        '
+        'btnExtract
+        '
+        Me.btnExtract.Location = New System.Drawing.Point(262, 226)
+        Me.btnExtract.Name = "btnExtract"
+        Me.btnExtract.Size = New System.Drawing.Size(142, 36)
+        Me.btnExtract.TabIndex = 3
+        Me.btnExtract.Text = "Extract"
+        Me.btnExtract.UseVisualStyleBackColor = True
+        '
+        'ofd
+        '
+        Me.ofd.FileName = "OpenFileDialog1"
+        Me.ofd.Filter = "Data | *.fdb"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(77, 226)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(145, 36)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Display Table Names"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'cboTablename
+        '
+        Me.cboTablename.FormattingEnabled = True
+        Me.cboTablename.Location = New System.Drawing.Point(65, 175)
+        Me.cboTablename.Name = "cboTablename"
+        Me.cboTablename.Size = New System.Drawing.Size(327, 21)
+        Me.cboTablename.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 178)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Header"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 54)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(57, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Save Path"
+        '
+        'txtSavePath
+        '
+        Me.txtSavePath.Location = New System.Drawing.Point(65, 54)
+        Me.txtSavePath.Name = "txtSavePath"
+        Me.txtSavePath.Size = New System.Drawing.Size(323, 20)
+        Me.txtSavePath.TabIndex = 8
+        '
+        'lbTableName
+        '
+        Me.lbTableName.FormattingEnabled = True
+        Me.lbTableName.Location = New System.Drawing.Point(416, 23)
+        Me.lbTableName.Name = "lbTableName"
+        Me.lbTableName.Size = New System.Drawing.Size(251, 186)
+        Me.lbTableName.TabIndex = 6
+        '
+        'Extract
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(748, 268)
+        Me.Controls.Add(Me.lbTableName)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnExtract)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Extract"
+        Me.Text = "Extractor"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents txtPath As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtQuery As System.Windows.Forms.TextBox
+    Friend WithEvents btnExtract As System.Windows.Forms.Button
+    Friend WithEvents ofd As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents sfdPath As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents cboTablename As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtSavePath As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents lbTableName As System.Windows.Forms.ListBox
+End Class
---
+++ Pawnshop/Pawnshop/Forms/Extract.vb
@@ -0,0 +1,158 @@
+﻿Imports Microsoft.Office.Interop
+Imports System.IO
+Public Class Extract
+    Const DBPATH As String = "\W3W1LH4CKU.FDB"
+    Private path As String
+
+    Private Sub LoadQuery()
+        Dim mysql As String = "" & txtQuery.Text & ""
+        Dim tmpTableName As New TextBox, tmp As String
+        For Each obj In lbTableName.Items
+            tmpTableName.AppendText(obj.ToString & " ")
+        Next
+        tmp = tmpTableName.Text
+        'Dim split As String() = tmp.Split(New [Char]() {" "})
+        'Dim tmpJoin() As String = {String.Join(" ,", split)}
+        tmp = tmp.Replace(" ", """ ,""")
+        Dim tmpString() As String = {(tmp)}
+        sfdPath.FileName = String.Format("{0}.xlsx", GetOption("BranchCode"))
+        path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) & "\" & sfdPath.FileName
+        ExtractToExcell(tmpString, mysql, path)
+
+        MsgBox("Successfully Extracted", MsgBoxStyle.Information, "Extract")
+    End Sub
+
+    Private Sub LoadPath()
+        Dim readValue = My.Computer.Registry.GetValue(
+    "HKEY_LOCAL_MACHINE\Software\cdt-S0ft\Pawnshop", "InstallPath", Nothing)
+
+        Dim firebird As String = readValue & DBPATH
+        database.dbName = firebird
+        txtPath.Text = firebird
+    End Sub
+
+    Private Sub Extract_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        LoadPath()
+        txtSavePath.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+    End Sub
+
+    Private Sub btnExtract_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExtract.Click
+        LoadQuery()
+    End Sub
+
+    Friend Sub ExtractToExcell(ByVal headers As String(), ByVal mySql As String, ByVal dest As String)
+        If dest = "" Then Exit Sub
+
+        Dim ds As DataSet = LoadSQL(mySql)
+
+        'Load Excel
+        Dim oXL As New Excel.Application
+        If oXL Is Nothing Then
+            MessageBox.Show("Excel is not properly installed!!")
+            Return
+        End If
+
+        Dim oWB As Excel.Workbook
+        Dim oSheet As Excel.Worksheet
+
+        oXL = CreateObject("Excel.Application")
+        oXL.Visible = False
+
+        oWB = oXL.Workbooks.Add
+        oSheet = oWB.ActiveSheet
+        oSheet.Name = GetOption("BranchCode") & "_DAILY"
+
+        ' ADD BRANCHCODE
+        InsertArrayElement(headers, 0, "BRANCHCODE")
+
+        ' HEADERS
+        Dim cnt As Integer = 0
+        For Each hr In headers
+            cnt += 1 : oSheet.Cells(1, cnt).value = hr
+        Next
+
+        ' EXTRACTING
+        Console.Write("Extracting")
+        Dim rowCnt As Integer = 2
+        For Each dr As DataRow In ds.Tables(0).Rows
+            For colCnt As Integer = 0 To headers.Count - 1
+                If colCnt = 0 Then
+                    oSheet.Cells(rowCnt, colCnt + 1).value = GetOption("BranchCode")
+                Else
+                    oSheet.Cells(rowCnt, colCnt + 1).value = dr(colCnt - 1) 'dr(colCnt - 1) move the column by -1
+                End If
+            Next
+            rowCnt += 1
+
+            Console.Write(".")
+            Application.DoEvents()
+        Next
+
+        oWB.SaveAs(dest)
+        oSheet = Nothing
+        oWB.Close(False)
+        oWB = Nothing
+        oXL.Quit()
+        oXL = Nothing
+
+        Console.WriteLine("Data Extracted")
+    End Sub
+
+    Private Function GetOption(ByVal keys As String) As String
+        Dim mySql As String = "SELECT * FROM tblmaintenance WHERE opt_keys = '" & keys & "'"
+        Dim ret As String
+        Try
+            Dim ds As DataSet = LoadSQL(mySql)
+            ret = ds.Tables(0).Rows(0).Item("opt_values")
+        Catch ex As Exception
+            ret = 0
+        End Try
+
+        Return ret
+    End Function
+
+    Private Sub InsertArrayElement(Of T)( _
+          ByRef sourceArray() As T, _
+          ByVal insertIndex As Integer, _
+          ByVal newValue As T)
+
+        Dim newPosition As Integer
+        Dim counter As Integer
+
+        newPosition = insertIndex
+        If (newPosition < 0) Then newPosition = 0
+        If (newPosition > sourceArray.Length) Then _
+           newPosition = sourceArray.Length
+
+        Array.Resize(sourceArray, sourceArray.Length + 1)
+
+        For counter = sourceArray.Length - 2 To newPosition Step -1
+            sourceArray(counter + 1) = sourceArray(counter)
+        Next counter
+
+        sourceArray(newPosition) = newValue
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Try
+
+        Dim mysql As String = " " & txtQuery.Text & ""
+        Dim ds As DataSet = LoadSQL(mysql)
+
+        For Each dt In ds.Tables
+            For Each column In dt.Columns
+                    'Console.WriteLine(column.ColumnName)
+                    'cboTablename.Items.Add(column.ColumnName)
+                    lbTableName.Items.Add(column.ColumnName)
+            Next
+            Next
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+    Private Sub btnBrowse_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        sfdPath.ShowDialog()
+    End Sub
+
+End Class
