d72076cd8a9925c3de4f3ec9a7ef2e9ce90af0ec
marzxxx90 <junmar_honor@ymail.com>
2016-10-07 15:03:49 +0800

    Update

--- Pawnshop/Pawnshop/Developer/Dev_Migrate.vb
+++ Pawnshop/Pawnshop/Developer/Dev_Migrate.vb
@@ -21,69 +21,6 @@
             Dim mysql2 As String = "Select * from OPI where PawnItemID = '" & tmpPawnID & "'"
             Dim filldata2 As String = "OPI"
             Dim ds2 As DataSet = LoadSQL(mysql2, filldata2)
-            ' Dim FinalSchemeID As Integer = 0
-
-            'With Early Redeem
-            'If tmpSchemeID = "0.03" And tmpIntcheckSum = "2UeDR8f2bFX1c+rsLG8v7g==" Then
-            '    FinalSchemeID = 6
-
-            'ElseIf tmpSchemeID = "0.04" And tmpIntcheckSum = "2UeDR8f2bFX1c+rsLG8v7g==" Then
-            '    FinalSchemeID = 7
-
-            'ElseIf tmpSchemeID = "0.05" And tmpIntcheckSum = "2UeDR8f2bFX1c+rsLG8v7g==" Then
-            '    FinalSchemeID = 8
-
-            'ElseIf tmpSchemeID = "0.03" And tmpIntcheckSum = "aWFXwwYbqLmins1KPvmmIw==" Then
-            '    FinalSchemeID = 6
-
-            'ElseIf tmpSchemeID = "0.05" And tmpIntcheckSum = "aWFXwwYbqLmins1KPvmmIw==" Then
-            '    FinalSchemeID = 8
-
-            'ElseIf tmpSchemeID = "0.06" And tmpIntcheckSum = "aWFXwwYbqLmins1KPvmmIw==" Then
-            '    FinalSchemeID = 9
-
-            'ElseIf tmpSchemeID = "0.05" And tmpIntcheckSum = "Vw4p+Hj+9pc/U1EYpf8ffA==" Then
-            '    FinalSchemeID = 8
-
-            'ElseIf tmpSchemeID = "0.06" And tmpIntcheckSum = "Vw4p+Hj+9pc/U1EYpf8ffA==" Then
-            '    FinalSchemeID = 9
-            '    'Without Early Redeem
-            'ElseIf tmpSchemeID = "0.05" And tmpIntcheckSum = "6zixr/PwMkDjdRTSktCeJA==" Then
-            '    FinalSchemeID = 3
-
-            'ElseIf tmpSchemeID = "0.06" And tmpIntcheckSum = "6zixr/PwMkDjdRTSktCeJA==" Then
-            '    FinalSchemeID = 4
-
-            'ElseIf tmpSchemeID = "0.07" And tmpIntcheckSum = "6zixr/PwMkDjdRTSktCeJA==" Then
-            '    FinalSchemeID = 5
-
-            'ElseIf tmpSchemeID = "0.04" And tmpIntcheckSum = "H8o+NbYmV80tU9Zsk+3Vag==" Then
-            '    FinalSchemeID = 2
-
-            'ElseIf tmpSchemeID = "0.05" And tmpIntcheckSum = "H8o+NbYmV80tU9Zsk+3Vag==" Then
-            '    FinalSchemeID = 3
-
-            'ElseIf tmpSchemeID = "0.06" And tmpIntcheckSum = "H8o+NbYmV80tU9Zsk+3Vag==" Then
-            '    FinalSchemeID = 4
-
-            'ElseIf tmpSchemeID = "0.07" And tmpIntcheckSum = "H8o+NbYmV80tU9Zsk+3Vag==" Then
-            '    FinalSchemeID = 5
-
-            'ElseIf tmpSchemeID = "0.03" And tmpIntcheckSum = "THnOlQUa3C8IMnha27YfsQ==" Then
-            '    FinalSchemeID = 1
-
-            'ElseIf tmpSchemeID = "0.05" And tmpIntcheckSum = "THnOlQUa3C8IMnha27YfsQ==" Then
-            '    FinalSchemeID = 3
-
-            'ElseIf tmpSchemeID = "0.06" And tmpIntcheckSum = "THnOlQUa3C8IMnha27YfsQ==" Then
-            '    FinalSchemeID = 4
-
-            'ElseIf tmpSchemeID = "0.07" And tmpIntcheckSum = "THnOlQUa3C8IMnha27YfsQ==" Then
-            '    FinalSchemeID = 5
-
-            'Else
-            '    FinalSchemeID = 99
-            'End If
             ds2.Tables(filldata2).Rows(0).Item("Scheme_ID") = tmpSchemeID
             database.SaveEntry(ds2, False)
         Next
