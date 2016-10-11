3f42013b2f4e33d93aa9b1c97573867766545e2f
marzxxx90 <junmar_honor@ymail.com>
2016-10-11 17:08:18 +0800

    Update

--- Pawnshop/Pawnshop/Developer/Dev_Migrate.vb
+++ Pawnshop/Pawnshop/Developer/Dev_Migrate.vb
@@ -88,16 +88,15 @@
     End Function
 
     Private Sub Dev_Migrate_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Try
         Dim mysql As String = "Select * from tblPawn"
         Dim filldata As String = "tblPawn"
         Dim ds As DataSet = LoadSQL(mysql, filldata)
-        If ds.Tables(0).Rows.Count <= 3000 Then
-            pbProgressBar.Minimum = 0
-            pbProgressBar.Maximum = 3000
-        Else
-            pbProgressBar.Minimum = 0
-            pbProgressBar.Maximum = 6000
-        End If
-       
+        Dim tmpMax As Integer = ds.Tables(0).Rows.Count
+        pbProgressBar.Minimum = 0
+        pbProgressBar.Maximum = tmpMax
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
     End Sub
 End Class
