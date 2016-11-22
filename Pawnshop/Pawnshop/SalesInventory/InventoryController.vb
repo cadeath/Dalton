acc96f85b9e776b03a288d6c3dedb8f1e45f9558
James Zerdick <ecjmaquic@gmail.com>
2016-11-22 16:00:15 +0800

    UPDATE

---
+++ Pawnshop/Pawnshop/SalesInventory/InventoryController.vb
@@ -0,0 +1,32 @@
+﻿Module InventoryController
+    Dim mySql As String, ds As DataSet
+
+    Friend Sub AddInventory(ByVal itemCode As String, Optional ByVal Qty As Integer = 1)
+        mySql = String.Format("SELECT * FROM ITEMMASTER WHERE ITEMCODE = '{0}'", itemCode)
+        ds = LoadSQL(mySql, "ITEMMASTER")
+
+        If Not ds.Tables(0).Rows.Count = 1 Then
+            Console.WriteLine("PROBLEM ON ITEM " & itemCode)
+            MsgBox("PROBLEM ON ITEM " & itemCode, MsgBoxStyle.Critical)
+            Exit Sub
+        End If
+
+        ds.Tables("ITEMMASTER").Rows(0).Item("ONHAND") += Qty
+        database.SaveEntry(ds)
+    End Sub
+
+    Friend Sub DeductInventory(ByVal itemCode As String, Optional ByVal Qty As Integer = 1)
+        mySql = String.Format("SELECT * FROM ITEMMASTER WHERE ITEMCODE = '{0}'", itemCode)
+        ds = LoadSQL(mySql, "ITEMMASTER")
+
+        If Not ds.Tables(0).Rows.Count = 1 Then
+            Console.WriteLine("PROBLEM ON ITEM " & itemCode)
+            MsgBox("PROBLEM ON ITEM " & itemCode, MsgBoxStyle.Critical)
+            Exit Sub
+        End If
+
+        ds.Tables("ITEMMASTER").Rows(0).Item("ONHAND") -= Qty
+        database.SaveEntry(ds)
+    End Sub
+
+End Module
