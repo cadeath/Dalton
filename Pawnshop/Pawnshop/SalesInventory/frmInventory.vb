4ddbe41c2aa33887cabe9b062e7d908f8d9bea53
James Zerdick <ecjmaquic@gmail.com>
2016-11-22 15:46:33 +0800

    UPDATE
    Finish upload STOFILE

--- Pawnshop/Pawnshop/SalesInventory/frmInventory.vb
+++ Pawnshop/Pawnshop/SalesInventory/frmInventory.vb
@@ -29,8 +29,19 @@ Public Class frmInventory
         STOFile.Tables.Add(dt)
     End Sub
 
-    Private Sub AddToSTO(ByVal inv As cItemData)
-
+    Private Sub AddToDataset(ByVal lvItem As ListViewItem)
+        Dim dsNewRow As DataRow
+        dsNewRow = STOFile.Tables(0).NewRow
+        With dsNewRow
+            .Item("ITEMCODE") = lvItem.Text
+            .Item("DESCRIPTION") = lvItem.SubItems(1).Text
+            .Item("CATEGORY") = lvItem.SubItems(2).Text
+            .Item("UOM") = lvItem.SubItems(3).Text
+            .Item("STOCKS") = lvItem.SubItems(4).Text
+            .Item("PRICE") = lvItem.SubItems(5).Text
+            .Item("STO") = CInt(lvItem.SubItems(6).Text)
+        End With
+        STOFile.Tables(0).Rows.Add(dsNewRow)
     End Sub
 
     Private Function isSTOExisting(ByVal stono As Integer) As Boolean
@@ -52,6 +63,9 @@ Public Class frmInventory
 
         ofdInv.ShowDialog()
         ptuFile = ofdInv.FileName
+        If ptuFile = "" Then
+            Exit Sub
+        End If
 
         ClearFields()
         STOFile_Init()
@@ -61,6 +75,9 @@ Public Class frmInventory
             While rvsr.Peek <> -1
                 vText = rvsr.ReadLine
                 vstring = vText.Split({vbTab}, StringSplitOptions.RemoveEmptyEntries)
+                If vstring.Count <> 6 Then
+                    GoTo FAILED_VER
+                End If
 
                 If idx > 1 Then
                     'EXCLUDE HEADERS
@@ -112,8 +129,6 @@ Public Class frmInventory
                         STONum = vstring(4)
                         invItem.DocID = STONum
                         AddItem(invItem, STONum)
-
-
                     End If
                 End If
 
@@ -125,6 +140,13 @@ Public Class frmInventory
         lblWHS.Text = WhsCode
         lblSTODate.Text = STODate
         lblFilename.Text = ofdInv.FileName
+
+        Dim sortedRows As DataRow()
+        sortedRows = STOFile.Tables(0).Select("", "STO ASC")
+
+        For Each dr As DataRow In sortedRows
+            Console.WriteLine(dr.Item("ITEMCODE") & " - " & dr.Item("STO"))
+        Next
 
         Exit Sub
 FAILED_VER:
@@ -143,10 +165,13 @@ FAILED_VER:
         lv.SubItems.Add(itm.Qty)
         lv.SubItems.Add(itm.SalePrice.ToString("#,#0.00"))
         lv.SubItems.Add(STO.ToString("00000000"))
+
+        AddToDataset(lv)
     End Sub
 
     Private Sub Label3_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles Label3.DoubleClick
         SeedItemData.Populate2()
+        Console.WriteLine("Populated")
     End Sub
 
     Private Sub frmInventory_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
@@ -154,51 +179,70 @@ FAILED_VER:
     End Sub
 
     Private Sub ClearFields()
-        lblFilename.Text = ""
+        lblFilename.Text = "No file yet"
         lblWHS.Text = ""
         lblSTODate.Text = ""
 
         lvInventory.Items.Clear()
+        STOFile.Clear()
     End Sub
 
     Private Sub btnAccept_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAccept.Click
-        Create_Document()
-    End Sub
+        If Not lvInventory.Items.Count > 0 Then
+            MsgBox("Nothing to accept" + vbCr + "Data already been uploaded", MsgBoxStyle.Information)
+            Exit Sub
+        End If
 
-    Private Sub Create_Document()
-        Dim STONum As Integer = 0
+        Dim STONum As Integer = 0, DocID As Integer = 0
         Dim mySql As String
         Dim ds As DataSet
         Dim dsNewRow As DataRow
 
-        'For Each ht_ent As DictionaryEntry In ht_inv
-        '    Dim invItem As cItem
-        '    invItem = ht_ent.Value
+        Dim SortedDS As DataRow()
+        SortedDS = STOFile.Tables(0).Select("", "STO ASC")
 
-        '    If STONum <> invItem.DocID Then
-        '        STONum = invItem.DocID
+        For Each dr As DataRow In SortedDS
 
-        '        mySql = "SELECT * FROM INV WHERE DOCNUM = " & STONum
-        '        ds = LoadSQL(mySql, "INV")
+            ' Add Document
+            If STONum <> dr.Item("STO") Then
+                mySql = "SELECT * FROM INV ROWS 1"
 
-        '        dsNewRow = ds.Tables("INV").NewRow
-        '        With dsNewRow
-        '            .Item("DOCNUM") = STONum
-        '            .Item("DOCDATE") = STODate
-        '            .Item("PARTNER") = "Possible Head Office"
-        '            .Item("REMARKS") = String.Format("Imported {0}", STODate.ToString("MMM/dd/yyyy"))
-        '            .Item("REFNUM") = STONum
-        '        End With
-        '        ds.Tables("INV").Rows.Add(dsNewRow)
-        '        database.SaveEntry(ds)
+                ds = LoadSQL(mySql, "INV")
+                STONum = dr.Item("STO")
 
-        '        mySql = "SELECT * FROM INV ORDER BY DOCID DESC ROWS 1"
-        '        ds = LoadSQL(mySql)
+                dsNewRow = ds.Tables(0).NewRow
+                With dsNewRow
+                    .Item("DOCNUM") = STONum
+                    .Item("DOCDATE") = STODate
+                    .Item("PARTNER") = "HEAD OFFICE"
+                    .Item("REFNUM") = STONum
+                End With
+                ds.Tables("INV").Rows.Add(dsNewRow)
+                database.SaveEntry(ds)
 
-        '        DocID = ds.Tables("INV").Rows(0).Item("DOCID")
+                mySql = "SELECT * FROM INV ORDER BY DOCID DESC ROWS 1"
+                ds = LoadSQL(mySql)
+                DocID = ds.Tables(0).Rows(0).Item("DOCID")
+            End If
 
-        '    End If
-        'Next
+            ' Add Document Lines
+            mySql = "SELECT * FROM INVLINES ROWS 1"
+            ds = LoadSQL(mySql, "INVLINES")
+            dsNewRow = ds.Tables(0).NewRow
+            With dsNewRow
+                .Item("DOCID") = DocID
+                .Item("ITEMCODE") = dr("ITEMCODE")
+                .Item("DESCRIPTION") = dr("DESCRIPTION")
+                .Item("QTY") = dr("STOCKS")
+                .Item("UOM") = dr("UOM")
+                .Item("REMARKS") = "UPLOADED DATED " & CurrentDate
+            End With
+            ds.Tables("INVLINES").Rows.Add(dsNewRow)
+            database.SaveEntry(ds)
+        Next
 
+        MsgBox("STO File imported", MsgBoxStyle.Information)
+        ClearFields()
     End Sub
+
 End Class
