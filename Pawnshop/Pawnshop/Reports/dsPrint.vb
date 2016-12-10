07d3bc7800b3c02cde86f65265f2d4937d75faf4
marzxxx90 <junmar_honor@ymail.com>
2016-12-10 11:50:39 +0800

    Merge remote-tracking branch 'refs/remotes/origin/1.4-POS' into 1.4-POS-Reprint
    # Conflicts:
    #	Pawnshop/Pawnshop.suo
    #	Pawnshop/Pawnshop/Pawnshop.vbproj
    #	Pawnshop/Pawnshop/Reports/dsPrint.Designer.vb
    #	Pawnshop/Pawnshop/Reports/dsPrint.xsd
    #	Pawnshop/Pawnshop/Reports/dsPrint.xss
    #	Pawnshop/Pawnshop/SalesInventory/frmSalesOptions.vb
    #	Pawnshop/Pawnshop/obj/x86/Debug/TempPE/Reports.dsPrint.Designer.vb.dll

--- Pawnshop/Pawnshop/Reports/dsPrint.vb
+++ Pawnshop/Pawnshop/Reports/dsPrint.vb
@@ -1,12 +1,4 @@
 ﻿
 
 Partial Public Class dsPrint
-    Partial Class ReceiptDataTable
-
-        Private Sub ReceiptDataTable_ReceiptRowChanging(ByVal sender As System.Object, ByVal e As ReceiptRowChangeEvent) Handles Me.ReceiptRowChanging
-
-        End Sub
-
-    End Class
-
 End Class
