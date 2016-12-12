741f823a65d54d31effb529bd7544115d567ed9c
marzxxx90 <junmar_honor@ymail.com>
2016-12-12 08:52:15 +0800

    Update

--- Pawnshop/Pawnshop/Forms/frmSettings.vb
+++ Pawnshop/Pawnshop/Forms/frmSettings.vb
@@ -31,12 +31,14 @@
         txtBal.Text = GetOption("MaintainingBalance")
         txtRevolving.Text = GetOption("RevolvingFund")
         txtCashInBank.Text = GetSAPAccount("Cash in Bank")
+        txtCustomerCode.Text = GetOption("CustomerCode")
 
         If locked Then
             txtCode.Enabled = False
             txtName.Enabled = False
             txtArea.Enabled = False
             txtRevolving.Enabled = False
+            txtCustomerCode.Enabled = False
         End If
 
         'Second
@@ -85,6 +87,7 @@
             UpdateOptions("RevolvingFund", txtRevolving.Text, isOTPEnable)
             UpdateOptions("LOCKED", "YES", isOTPEnable)
             InsertSAPCount(txtRevolving.Text)
+            UpdateOptions("CustomerCode", txtCustomerCode.Text, isOTPEnable)
 
             BranchCode = txtCode.Text
             branchName = txtName.Text
