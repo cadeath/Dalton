66f6a1c94f1c42f75a08633836f8bb338536a3bf
marzxxx90 <junmar_honor@ymail.com>
2016-12-17 11:11:05 +0800

    Update

---
+++ Pawnshop/Fixer/FixDoclineDecimal/frmFixDoclineDecimal.vb
@@ -0,0 +1,25 @@
+﻿Public Class frmFixDoclineDecimal
+
+    Private Sub frmFixDoclineDecimal_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub LoadPath()
+        Dim readValue = My.Computer.Registry.GetValue(
+    "HKEY_LOCAL_MACHINE\Software\cdt-S0ft\Pawnshop", "InstallPath", Nothing)
+
+        Dim firebird As String = readValue & DBPATH
+        database.dbName = firebird
+        txtData.Text = firebird
+    End Sub
+
+    Private Sub btnFix_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnFix.Click
+        Try
+            RunCommand("")
+            MsgBox("Succesful")
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "E R R O R")
+        End Try
+    End Sub
+
+End Class
