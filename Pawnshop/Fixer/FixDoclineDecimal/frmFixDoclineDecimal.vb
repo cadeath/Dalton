74e867835a99ed16db53edc66316896735eb6630
marzxxx90 <junmar_honor@ymail.com>
2016-12-17 14:20:43 +0800

    Update

--- Pawnshop/Fixer/FixDoclineDecimal/frmFixDoclineDecimal.vb
+++ Pawnshop/Fixer/FixDoclineDecimal/frmFixDoclineDecimal.vb
@@ -16,8 +16,23 @@
 
     Private Sub btnFix_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnFix.Click
         Try
-            RunCommand("")
-            MsgBox("Succesful")
+            Dim NewUnitPrice As String = "ALTER TABLE DOCLINES ADD UNITNEW DECIMAL(12, 6) DEFAULT '0.0' NOT NULL;"
+            Dim NewSalePrice As String = "ALTER TABLE DOCLINES ADD SALENEW DECIMAL(12, 6) DEFAULT '0.0' NOT NULL;"
+            Dim UpdateNew As String = "Update Doclines set UnitNew = UnitPrice, SaleNew = SalePrice where DLID <> 0"
+            Dim DropUnit As String = "ALTER TABLE DOCLINES DROP UNITPRICE;"
+            Dim DropSale As String = "ALTER TABLE DOCLINES DROP SALEPRICE;"
+            Dim ChangeUnit As String = "ALTER TABLE DOCLINES ALTER COLUMN UNITNEW TO UNITPRICE;"
+            Dim ChangeSale As String = "ALTER TABLE DOCLINES ALTER COLUMN SALENEW TO SALEPRICE;"
+
+            RunCommand(NewUnitPrice)
+            RunCommand(NewSalePrice)
+            RunCommand(UpdateNew)
+            RunCommand(DropUnit)
+            RunCommand(DropSale)
+            RunCommand(ChangeUnit)
+            RunCommand(ChangeSale)
+
+            MsgBox("Succesful Updated", MsgBoxStyle.Information, "System")
         Catch ex As Exception
             MsgBox(ex.Message, MsgBoxStyle.Critical, "E R R O R")
         End Try
