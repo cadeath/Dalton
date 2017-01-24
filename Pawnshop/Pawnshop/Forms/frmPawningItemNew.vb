8013cb26fb70d018bfced707eea17b963a135de8
marzxxx90 <junmar_honor@ymail.com>
2017-01-24 13:49:26 +0800

    Update

--- Pawnshop/Pawnshop/Forms/frmPawningItemNew.vb
+++ Pawnshop/Pawnshop/Forms/frmPawningItemNew.vb
@@ -1557,4 +1557,8 @@ Public Class frmPawningItemNew
                 lblTransaction.Text = "Redeem"
         End Select
     End Sub
+
+    Private Sub txtAppr_Leave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtAppr.Leave
+        ReComputeInterest()
+    End Sub
 End Class
