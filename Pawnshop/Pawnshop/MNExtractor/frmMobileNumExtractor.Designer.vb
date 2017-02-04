f423315bcd7004002b2f6a42835a34761c8b9147
Bladier <misionaellie@gmail.com>
2017-02-04 13:48:12 +0800

    UPDATE

---
+++ Pawnshop/Pawnshop/MNExtractor/frmMobileNumExtractor.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMobileNumExtractor
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.lblPath = New System.Windows.Forms.Label()
+        Me.txtPath = New System.Windows.Forms.TextBox()
+        Me.btnExtract = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(367, 13)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(40, 23)
+        Me.btnBrowse.TabIndex = 0
+        Me.btnBrowse.Text = " . . ."
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'lblPath
+        '
+        Me.lblPath.AutoSize = True
+        Me.lblPath.Location = New System.Drawing.Point(5, 18)
+        Me.lblPath.Name = "lblPath"
+        Me.lblPath.Size = New System.Drawing.Size(89, 15)
+        Me.lblPath.TabIndex = 1
+        Me.lblPath.Text = "Database Path"
+        '
+        'txtPath
+        '
+        Me.txtPath.Location = New System.Drawing.Point(98, 14)
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.Size = New System.Drawing.Size(261, 21)
+        Me.txtPath.TabIndex = 2
+        '
+        'btnExtract
+        '
+        Me.btnExtract.Location = New System.Drawing.Point(327, 42)
+        Me.btnExtract.Name = "btnExtract"
+        Me.btnExtract.Size = New System.Drawing.Size(80, 23)
+        Me.btnExtract.TabIndex = 3
+        Me.btnExtract.Text = "&Extract"
+        Me.btnExtract.UseVisualStyleBackColor = True
+        '
+        'frmMobileNumExtractor
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.ClientSize = New System.Drawing.Size(421, 70)
+        Me.Controls.Add(Me.btnExtract)
+        Me.Controls.Add(Me.txtPath)
+        Me.Controls.Add(Me.lblPath)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "frmMobileNumExtractor"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Mobile Number Extractor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnBrowse As System.Windows.Forms.Button
+    Friend WithEvents lblPath As System.Windows.Forms.Label
+    Friend WithEvents txtPath As System.Windows.Forms.TextBox
+    Friend WithEvents btnExtract As System.Windows.Forms.Button
+End Class
---
+++ Pawnshop/Pawnshop/MNExtractor/frmMobileNumExtractor.vb
@@ -0,0 +1,112 @@
+﻿Imports Microsoft.Office.Interop
+Public Class frmMobileNumExtractor
+    Dim OFD As New OpenFileDialog
+    Dim dest As String = My.Computer.FileSystem.SpecialDirectories.Desktop
+
+
+    Private Sub btnBrowse_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBrowse.Click
+        OFD.ShowDialog()
+
+        txtPath.Text = OFD.FileName
+        database.dbName = txtPath.Text
+    End Sub
+
+    Private Sub btnExtract_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExtract.Click
+        Me.Enabled = False
+        Dim mysql As String = "SELECT DISTINCT (FirstName ||' '|| LastName ||' '|| MiddleName) as Fullname,PHONE1,PHONE2,PHONE3," & _
+                              "PHONE_OTHERS FROM TBLCLIENT WHERE PHONE1 <>''"
+        Dim ds As DataSet = LoadSQL(mysql, "TBLCLIENT")
+
+        Dim Headers As String() = {"Fullname", "PHONE1", "PHONE2", "PHONE2", "PHONE_OTHERS"}
+
+        dest = dest & "\" & String.Format("{2}{1}{0}.xlsx", Now.ToString("MMddyyyy"), BranchCode, "MNEXTRACT")  'BranchCode + Date
+        MNExtract(Headers, mysql, dest)
+
+        MsgBox("Finished", MsgBoxStyle.Information, Me.Text)
+        txtPath.Text = ""
+        database.dbName = ""
+        Me.Enabled = True
+    End Sub
+
+
+
+    Friend Sub MNExtract(ByVal headers As String(), ByVal mySql As String, ByVal dest As String)
+        If dest = "" Then Exit Sub
+
+        Dim ds As DataSet = LoadSQL(mySql)
+
+        'Load Excel
+        Dim oXL As New Excel.Application
+        If oXL Is Nothing Then
+            MessageBox.Show("Excel is not properly installed!!")
+            Return
+        End If
+
+        Dim oWB As Excel.Workbook
+        Dim oSheet As Excel.Worksheet
+
+        oXL = CreateObject("Excel.Application")
+        oXL.Visible = False
+
+        oWB = oXL.Workbooks.Add
+        oSheet = oWB.ActiveSheet
+        oSheet.Name = ExtractDataFromDatabase.lbltransaction.Text
+
+        ' ADD BRANCHCODE
+        InsertArrayElement(headers, 0, "BRANCHCODE")
+
+        ' HEADERS
+        Dim cnt As Integer = 0
+        For Each hr In headers
+            cnt += 1 : oSheet.Cells(1, cnt).value = hr
+        Next
+
+        ' EXTRACTING
+        Console.Write("Extracting")
+        Dim rowCnt As Integer = 2
+        For Each dr As DataRow In ds.Tables(0).Rows
+            For colCnt As Integer = 0 To headers.Count - 1
+                If colCnt = 0 Then
+                    oSheet.Cells(rowCnt, colCnt + 1).value = BranchCode
+                Else
+                    oSheet.Cells(rowCnt, colCnt + 1).value = dr(colCnt - 1) 'dr(colCnt - 1) move the column by -1
+                End If
+            Next
+            rowCnt += 1
+
+            Console.Write(".")
+            Application.DoEvents()
+        Next
+
+        oWB.SaveAs(dest)
+        oSheet = Nothing
+        oWB.Close(False)
+        oWB = Nothing
+        oXL.Quit()
+        oXL = Nothing
+
+        Console.WriteLine("Data Extracted")
+    End Sub
+
+    Private Sub InsertArrayElement(Of T)( _
+          ByRef sourceArray() As T, _
+          ByVal insertIndex As Integer, _
+          ByVal newValue As T)
+
+        Dim newPosition As Integer
+        Dim counter As Integer
+
+        newPosition = insertIndex
+        If (newPosition < 0) Then newPosition = 0
+        If (newPosition > sourceArray.Length) Then _
+           newPosition = sourceArray.Length
+
+        Array.Resize(sourceArray, sourceArray.Length + 1)
+
+        For counter = sourceArray.Length - 2 To newPosition Step -1
+            sourceArray(counter + 1) = sourceArray(counter)
+        Next counter
+
+        sourceArray(newPosition) = newValue
+    End Sub
+End Class
