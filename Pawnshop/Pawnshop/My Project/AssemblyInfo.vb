23b2f42dcadf7edc80968ffb17622479eacc5e82
marzxxx90 <junmar_honor@ymail.com>
2017-02-13 11:09:02 +0800

    Update

--- Pawnshop/Pawnshop/My Project/AssemblyInfo.vb
+++ Pawnshop/Pawnshop/My Project/AssemblyInfo.vb
@@ -33,6 +33,6 @@ Imports System.Runtime.InteropServices
 
 
 
-<Assembly: AssemblyVersion("1.4.1.14")> 
-<Assembly: AssemblyFileVersion("1.4.1.14")> 
+<Assembly: AssemblyVersion("1.4.2.1")> 
+<Assembly: AssemblyFileVersion("1.4.2.1")> 
 
