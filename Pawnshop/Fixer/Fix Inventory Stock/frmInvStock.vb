15fafd64566155e638814a78ef8343f909837297
marzxxx90 <junmar_honor@ymail.com>
2017-02-28 10:07:26 +0800

    Add New Fixer for Inventory Stock

---
+++ Pawnshop/Fixer/Fix Inventory Stock/frmInvStock.vb
@@ -0,0 +1,42 @@
+﻿Public Class frmInvStock
+    Const DBPATH As String = "\W3W1LH4CKU.FDB"
+
+    Private Sub frmInvStock_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        LoadPath()
+    End Sub
+
+    Private Sub LoadPath()
+        Dim readValue = My.Computer.Registry.GetValue(
+    "HKEY_LOCAL_MACHINE\Software\cdt-S0ft\Pawnshop", "InstallPath", Nothing)
+
+        Dim firebird As String = readValue & DBPATH
+        database.dbName = firebird
+        txtData.Text = firebird
+    End Sub
+
+    Private Sub btnFix_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnFix.Click
+        Disable(1)
+        Try
+            FixInventory()
+
+            MsgBox("Succesful Updated", MsgBoxStyle.Information, "System")
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "E R R O R")
+        End Try
+        Disable(0)
+    End Sub
+
+    Private Sub Disable(ByVal st As Boolean)
+        btnFix.Enabled = Not st
+    End Sub
+
+    Private Sub FixInventory()
+        Dim fix As String = "Update ItemMaster set onHand = 1 Where onHand >= 2 And "
+        fix &= "ItemCode Not Like '%SMT%' "
+        fix &= "And ItemCode Not Like '%IND%' "
+
+        RunCommand(fix)
+
+    End Sub
+
+End Class
--- Pawnshop/Fixer/My Project/Application.Designer.vb
+++ Pawnshop/Fixer/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Fixer.frmAddItemMasterCOI
+            Me.MainForm = Global.Fixer.frmInvStock
         End Sub
     End Class
 End Namespace
