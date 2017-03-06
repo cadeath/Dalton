13c51bca3aa7b32c4ba5e37cc54a27767ad9e604
James Zerdick <ecjmaquic@gmail.com>
2017-03-06 16:50:38 +0800

    UPDATE

--- Pawnshop/Pawnshop/SMS/smsUtil.vb
+++ Pawnshop/Pawnshop/SMS/smsUtil.vb
@@ -1,3 +1,23 @@
-﻿Module smsUtil
+﻿Imports OneApi
+Imports OneApi.Model
+Imports OneApi.Config
+
+Module smsUtil
+
+    Private _user As String = "Eskie"
+    Private _password As String = "eskie123"
+    Private _sender As String = "PGC"
+
+    Friend Sub SendSMS(ByVal num As String, ByVal msg As String)
+        Dim config As New Configuration(_user, _password)
+        Dim smsClient As New OneApi.Client.Impl.SMSClient(config)
+
+        Dim smsRequest As SMSRequest
+        smsRequest = New SMSRequest(_sender, msg, num)
+
+        Dim rqId As String = smsClient.SmsMessagingClient.SendSMS(smsRequest).ToString
+        Console.WriteLine("Sent")
+        Console.WriteLine("ID:" & rqId)
+    End Sub
 
 End Module
