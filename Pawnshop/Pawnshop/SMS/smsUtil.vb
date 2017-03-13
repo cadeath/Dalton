f96bdfdf9eb640dd75f6d507f0ccb6bea78b2dae
James Zerdick <ecjmaquic@gmail.com>
2017-03-13 13:06:29 -0700

    UPDATE

--- Pawnshop/Pawnshop/SMS/smsUtil.vb
+++ Pawnshop/Pawnshop/SMS/smsUtil.vb
@@ -12,7 +12,7 @@ Module smsUtil
 
     Friend Sub SendSMS(ByVal num As String, ByVal msg As String)
         If Not isReady() Then
-
+            MsgBox("SMS Initialization not yet ready", MsgBoxStyle.Critical)
             Exit Sub
         End If
         Dim config As New Configuration(_smsUser, _smsPassword)
@@ -52,4 +52,86 @@ Module smsUtil
         End If
     End Sub
 
+    Friend Sub displayExpiry(ByVal lvExp As ListView, ByVal notiForm As Form)
+        Dim mySql As String = _
+            String.Format("SELECT * FROM PAWN_LIST WHERE EXPIRYDATE = '{0}' AND STATUS <> 'V'", CurrentDate)
+        mySql &= vbCrLf & "ORDER BY LOANDATE ASC"
+        Console.WriteLine(mySql)
+
+        Dim ds As DataSet = LoadSQL(mySql)
+        If ds.Tables(0).Rows.Count = 0 Then Exit Sub
+
+        For Each dr As DataRow In ds.Tables(0).Rows
+            Dim ptExpired As New PawnTicket2
+            ptExpired.Load_PTid(dr("PAWNID"))
+
+            With ptExpired
+                Dim lv As ListViewItem = lvExp.Items.Add(.PawnTicket)
+                lv.SubItems.Add(String.Format("{0} {1}", .Pawner.FirstName, .Pawner.LastName))
+                lv.SubItems.Add(cleanup_contact(ptExpired))
+                lv.SubItems.Add(.PawnItem.ItemClass.ClassName)
+                lv.SubItems.Add(.Principal.ToString("#,##0.00"))
+            End With
+        Next
+
+        notiForm.Show()
+    End Sub
+
+    Private Function cleanup_contact(ByVal pt As PawnTicket2) As String
+        Dim c1 As String, c2 As String, c3 As String
+
+        'Validate Contact Numbers
+        c1 = validate_cp(pt.Pawner.Cellphone1)
+        c2 = validate_cp(pt.Pawner.Cellphone2)
+        c3 = validate_cp(pt.Pawner.OtherNumber)
+
+        If c1 = "INVALID" And c2 = "INVALID" And c3 = "INVALID" Then
+            Return String.Format("INVALID>{0}|{1}|{2}", c1, c2, c3)
+        End If
+
+        If c1 <> "INVALID" Then
+            Return c1
+        End If
+        If c2 <> "INVALID" Then
+            Return c2
+        End If
+        If c3 <> "INVALID" Then
+            Return c3
+        End If
+
+        Return "INVALID"
+    End Function
+
+    Private Function validate_cp(ByVal str As String) As String
+        '+639257977559
+        If str.Length = 13 Then
+            If str.Substring(0, 4) = "+639" Then
+                Return str
+            End If
+        End If
+
+        '639257977559
+        If str.Length = 12 Then
+            If str.Substring(0, 3) = "639" Then
+                Return "+" & str
+            End If
+        End If
+
+        '09259797559
+        If str.Length = 11 Then
+            If str.Substring(0, 2) = "09" Then
+                Return "+63" & str.Substring(1)
+            End If
+        End If
+
+        '9257977559
+        If str.Length = 10 Then
+            If str.Substring(0, 1) = "9" Then
+                Return "+63" & str
+            End If
+        End If
+
+
+        Return "INVALID"
+    End Function
 End Module
