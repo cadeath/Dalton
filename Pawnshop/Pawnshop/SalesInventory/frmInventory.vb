687e82a19491f4b1013ffe2c48520b5358f953ac
marzxxx90 <junmar_honor@ymail.com>
2017-04-04 11:32:11 +0800

    Update

--- Pawnshop/Pawnshop/SalesInventory/frmInventory.vb
+++ Pawnshop/Pawnshop/SalesInventory/frmInventory.vb
@@ -212,10 +212,7 @@ FAILED_VER:
         Dim hash = InputBox("HOT CODE", "ENTER HOT CODE")
         If hash = "" Then Exit Sub
         If Not hash = security.GetFileMD5(lblFilename.Text) Then
-            ' TODO: JUNMAR
-            ' RECORD HASH VALUE THAT WAS ENCODED IN THE HOT CODE
-            ' SAVE IN AT DAILYTIMELOG
-
+            AddTimelyLogs("IMPORT INVENTORY DATA", "INVALID HOT CODE", , False, "HOT CODE: " & hash, )
             MsgBox("Invalid HOT CODE", MsgBoxStyle.Critical, "HOT CODE")
             Exit Sub
         End If
