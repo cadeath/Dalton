da99064b78b91367a02966e4be23e560f2572578
James Zerdick <ecjmaquic@gmail.com>
2017-04-08 12:11:01 +0800

    UPDATE
    Testing SMS Reporting

---
+++ Pawnshop/Pawnshop/SMS/devSMS.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class devSMS
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtUser = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtMsg = New System.Windows.Forms.TextBox()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "USER:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 35)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "PASSWORD:"
+        '
+        'txtUser
+        '
+        Me.txtUser.Location = New System.Drawing.Point(96, 6)
+        Me.txtUser.Name = "txtUser"
+        Me.txtUser.Size = New System.Drawing.Size(147, 20)
+        Me.txtUser.TabIndex = 2
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(96, 32)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassword.Size = New System.Drawing.Size(147, 20)
+        Me.txtPassword.TabIndex = 3
+        '
+        'txtNumber
+        '
+        Me.txtNumber.Location = New System.Drawing.Point(15, 80)
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(269, 20)
+        Me.txtNumber.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 64)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "NUMBER:"
+        '
+        'txtMsg
+        '
+        Me.txtMsg.Location = New System.Drawing.Point(15, 106)
+        Me.txtMsg.Multiline = True
+        Me.txtMsg.Name = "txtMsg"
+        Me.txtMsg.Size = New System.Drawing.Size(269, 108)
+        Me.txtMsg.TabIndex = 6
+        Me.txtMsg.Text = "THIS IS JUST A TEST"
+        '
+        'btnSend
+        '
+        Me.btnSend.Location = New System.Drawing.Point(203, 220)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(81, 36)
+        Me.btnSend.TabIndex = 7
+        Me.btnSend.Text = "&Send"
+        Me.btnSend.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(15, 220)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(81, 36)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "S&tatus"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'devSMS
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(297, 266)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.btnSend)
+        Me.Controls.Add(Me.txtMsg)
+        Me.Controls.Add(Me.txtNumber)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUser)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "devSMS"
+        Me.Text = "devSMS"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtUser As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents txtNumber As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtMsg As System.Windows.Forms.TextBox
+    Friend WithEvents btnSend As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+End Class
---
+++ Pawnshop/Pawnshop/SMS/devSMS.vb
@@ -0,0 +1,38 @@
+﻿Imports System
+Imports OneApi
+Imports OneApi.Model
+Imports OneApi.Config
+
+Public Class devSMS
+
+    Dim reqID
+    Dim smsClient
+
+    Private Sub btnSend_Click(sender As System.Object, e As System.EventArgs) Handles btnSend.Click
+        Dim config As New Configuration(txtUser.Text, txtPassword.Text)
+        Dim smsClient As New OneApi.Client.Impl.SMSClient(config)
+
+
+        Dim smsRequest As SMSRequest
+        smsRequest = New SMSRequest("PGC", txtMsg.Text, txtNumber.Text)
+        Try
+            smsClient.SmsMessagingClient.SendSMS(smsRequest)
+            reqID = smsClient.SmsMessagingClient.GetDeliveryReports
+        Catch ex As Exception
+            reqID = ex.ToString
+        End Try
+
+        Console.WriteLine("Send ID: " & reqID.ToString)
+
+        'SendSMS(txtNumber.Text, txtMsg.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Dim deliveryInfoList As DeliveryInfoList
+        deliveryInfoList = smsClient.SmsMessagingClient.QueryDeliveryStatus("PGC", reqID)
+
+        Dim deliveryStatus As String
+        deliveryStatus = deliveryInfoList.DeliveryInfos(0).DeliveryStatus
+        Console.WriteLine(deliveryStatus)
+    End Sub
+End Class
