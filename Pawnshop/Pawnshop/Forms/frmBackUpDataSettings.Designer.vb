942fe1dd265e64cd9ca659f470a0cb19276fb737
marzxxx90 <junmar_honor@ymail.com>
2017-04-12 09:26:07 +0800

    Allow backup Compliance File

--- Pawnshop/Pawnshop/Forms/frmBackUpDataSettings.Designer.vb
+++ Pawnshop/Pawnshop/Forms/frmBackUpDataSettings.Designer.vb
@@ -24,9 +24,14 @@ Partial Class frmBackUpDataSettings
     Private Sub InitializeComponent()
         Me.fbdBackup = New System.Windows.Forms.FolderBrowserDialog()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
-        Me.txtPath2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCompliance = New System.Windows.Forms.TextBox()
+        Me.btnBrowseComp = New System.Windows.Forms.Button()
+        Me.txtData = New System.Windows.Forms.TextBox()
         Me.btnBrowseBackup = New System.Windows.Forms.Button()
         Me.btnBackup = New System.Windows.Forms.Button()
+        Me.fbdComp = New System.Windows.Forms.FolderBrowserDialog()
         Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -36,36 +41,75 @@ Partial Class frmBackUpDataSettings
         '
         'GroupBox2
         '
-        Me.GroupBox2.Controls.Add(Me.txtPath2)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Controls.Add(Me.txtCompliance)
+        Me.GroupBox2.Controls.Add(Me.btnBrowseComp)
+        Me.GroupBox2.Controls.Add(Me.txtData)
         Me.GroupBox2.Controls.Add(Me.btnBrowseBackup)
         Me.GroupBox2.Location = New System.Drawing.Point(12, 12)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(302, 54)
+        Me.GroupBox2.Size = New System.Drawing.Size(302, 135)
         Me.GroupBox2.TabIndex = 5
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Backup Directory"
         '
-        'txtPath2
+        'Label2
         '
-        Me.txtPath2.Location = New System.Drawing.Point(6, 19)
-        Me.txtPath2.Name = "txtPath2"
-        Me.txtPath2.ReadOnly = True
-        Me.txtPath2.Size = New System.Drawing.Size(206, 20)
-        Me.txtPath2.TabIndex = 5
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 86)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Compliance"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(30, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Data"
+        '
+        'txtCompliance
+        '
+        Me.txtCompliance.Location = New System.Drawing.Point(9, 102)
+        Me.txtCompliance.Name = "txtCompliance"
+        Me.txtCompliance.ReadOnly = True
+        Me.txtCompliance.Size = New System.Drawing.Size(245, 20)
+        Me.txtCompliance.TabIndex = 7
+        '
+        'btnBrowseComp
+        '
+        Me.btnBrowseComp.Location = New System.Drawing.Point(260, 102)
+        Me.btnBrowseComp.Name = "btnBrowseComp"
+        Me.btnBrowseComp.Size = New System.Drawing.Size(36, 23)
+        Me.btnBrowseComp.TabIndex = 8
+        Me.btnBrowseComp.Text = " . . ."
+        Me.btnBrowseComp.UseVisualStyleBackColor = True
+        '
+        'txtData
+        '
+        Me.txtData.Location = New System.Drawing.Point(9, 46)
+        Me.txtData.Name = "txtData"
+        Me.txtData.ReadOnly = True
+        Me.txtData.Size = New System.Drawing.Size(245, 20)
+        Me.txtData.TabIndex = 5
         '
         'btnBrowseBackup
         '
-        Me.btnBrowseBackup.Location = New System.Drawing.Point(218, 19)
+        Me.btnBrowseBackup.Location = New System.Drawing.Point(260, 46)
         Me.btnBrowseBackup.Name = "btnBrowseBackup"
-        Me.btnBrowseBackup.Size = New System.Drawing.Size(75, 23)
+        Me.btnBrowseBackup.Size = New System.Drawing.Size(36, 23)
         Me.btnBrowseBackup.TabIndex = 6
-        Me.btnBrowseBackup.Text = "Browse"
+        Me.btnBrowseBackup.Text = " . . ."
         Me.btnBrowseBackup.UseVisualStyleBackColor = True
         '
         'btnBackup
         '
         Me.btnBackup.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnBackup.Location = New System.Drawing.Point(12, 72)
+        Me.btnBackup.Location = New System.Drawing.Point(12, 153)
         Me.btnBackup.Name = "btnBackup"
         Me.btnBackup.Size = New System.Drawing.Size(302, 36)
         Me.btnBackup.TabIndex = 3
@@ -76,7 +120,7 @@ Partial Class frmBackUpDataSettings
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(321, 116)
+        Me.ClientSize = New System.Drawing.Size(321, 195)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.btnBackup)
         Me.Name = "frmBackUpDataSettings"
@@ -88,7 +132,12 @@ Partial Class frmBackUpDataSettings
     End Sub
     Friend WithEvents fbdBackup As System.Windows.Forms.FolderBrowserDialog
     Friend WithEvents btnBackup As System.Windows.Forms.Button
-    Friend WithEvents txtPath2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtData As System.Windows.Forms.TextBox
     Friend WithEvents btnBrowseBackup As System.Windows.Forms.Button
     Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtCompliance As System.Windows.Forms.TextBox
+    Friend WithEvents btnBrowseComp As System.Windows.Forms.Button
+    Friend WithEvents fbdComp As System.Windows.Forms.FolderBrowserDialog
 End Class
--- Pawnshop/Pawnshop/Forms/frmBackUpDataSettings.vb
+++ Pawnshop/Pawnshop/Forms/frmBackUpDataSettings.vb
@@ -4,11 +4,12 @@ Public Class frmBackUpDataSettings
 
     Private Sub btnBrowseBackup_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBrowseBackup.Click
         If Not fbdBackup.ShowDialog = Windows.Forms.DialogResult.OK Then Exit Sub
-        txtPath2.Text = fbdBackup.SelectedPath
-        txtPath2.Focus()
+        txtData.Text = fbdBackup.SelectedPath
+        txtData.Focus()
     End Sub
 
     Private Sub btnbackup_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBackup.Click
+        If txtData.Text = "" OrElse txtCompliance.Text = "" Then Exit Sub
 
         Using sw As StreamWriter = File.CreateText("backup.bat")
             sw.WriteLine("@echo off")
@@ -16,8 +17,10 @@ Public Class frmBackUpDataSettings
             sw.WriteLine("echo PLEASE CLOSE THE PAWNSHOP SYSTEM")
             sw.WriteLine("pause")
             sw.WriteLine("echo PLEASE WAIT WHILE SYSTEM IS BACKING UP")
-            sw.WriteLine("rar a " & txtPath2.Text & "\" & mod_system.BranchCode & ".rar -agMMddyyyy W3W1LH4CKU.FDB -hp" _
+            sw.WriteLine("rar a " & txtData.Text & "\" & mod_system.BranchCode & ".rar -agMMddyyyy W3W1LH4CKU.FDB -hp" _
                          & BranchCode & "MIS") 'Password = MISJUNMAR(uppercase)
+            sw.WriteLine("rar a " & txtCompliance.Text & "\" & mod_system.BranchCode & "_COMP" & ".rar -agMMddyyyy W3W1LH4CKU.FDB -hp" _
+                        & BranchCode & "MIS")
             sw.WriteLine("cls ")
             sw.WriteLine("echo DONE!!! THANK YOU FOR WAITING")
             sw.WriteLine("pause")
@@ -27,9 +30,16 @@ Public Class frmBackUpDataSettings
         MessageBox.Show("Successful", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
         Me.Close()
     End Sub
-    Private Sub txtPath2_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtPath2.KeyPress
+
+    Private Sub txtPath2_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtData.KeyPress
         If isEnter(e) Then
             btnBackup.PerformClick()
         End If
+    End Sub
+
+    Private Sub btnBrowseComp_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBrowseComp.Click
+        If Not fbdComp.ShowDialog = Windows.Forms.DialogResult.OK Then Exit Sub
+        txtCompliance.Text = fbdComp.SelectedPath
+        txtCompliance.Focus()
     End Sub
 End Class
