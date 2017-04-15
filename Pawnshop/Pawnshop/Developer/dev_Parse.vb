bf5f8878cf5ceefa2a4e52c959d2a950428b7bcb
marzxxx90 <junmar_honor@ymail.com>
2017-04-15 11:59:52 +0800

    Update

--- Pawnshop/Pawnshop/Developer/dev_Parse.vb
+++ Pawnshop/Pawnshop/Developer/dev_Parse.vb
@@ -2,58 +2,61 @@
 Imports System.Text
 
 Public Class dev_Parse
+
     Private SMSHash As New Hashtable
+    Private pattern As String = "\[.*?\]"
+    Private strParse As String, strTrim As String
+    Private mc As System.Text.RegularExpressions.MatchCollection
+
     Private Sub btnParse_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnParse.Click
         Parser(txtParse.Text)
     End Sub
 
-    Private Sub Parser(ByVal StrText As String)
-        Dim pattern As String = "\[.*?\]"
-        Dim strParse As String, strTrim As String
-        Dim mc As System.Text.RegularExpressions.MatchCollection
-        ' Dim m As System.Text.RegularExpressions.Match
-
+    Private Function Parser(ByVal StrText As String) As ArrayList
         Dim regx As New System.Text.RegularExpressions.Regex(pattern)
-
+        Dim strArray As New ArrayList
         If Regex.IsMatch(StrText, pattern) Then
-            mc = regx.Matches(StrText)
-            For index As Integer = 0 To mc.Count - 1
+            mc = regx.Matches(StrText) 'Check kong pila ang bracket
+            For index As Integer = 0 To mc.Count - 1 ' Count sa Bracket
                 'Nagkuha ug Text Fields
-                Console.WriteLine("Parse String: " & mc(index).Value.Substring(0, mc(index).Value.IndexOf(":")).Trim({"["c}))
                 strParse = mc(index).Value.Substring(0, mc(index).Value.IndexOf(":")).Trim({"["c})
+                Console.WriteLine("Fields " & strParse)
 
                 'Nagkuha ug Fields Information
-                Console.WriteLine("Trim String: " & mc(index).Value.Substring(mc(index).Value.IndexOf(":") + 1).Trim({"]"c}))
                 strTrim = mc(index).Value.Substring(mc(index).Value.IndexOf(":") + 1).Trim({"]"c}).Trim({" "c})
+                Console.WriteLine("Value " & strTrim)
 
-                If Not isValidKey(strParse) Then Exit Sub
+                If Not isValidKey(strParse) Then Exit Function
 
-                SMSHash.Add(strParse, strTrim)
+                'SMSHash.Add(strParse, strTrim)
+                strArray.Add(strParse)
+                strArray.Add(strTrim)
             Next
         End If
 
-        If SMSHash.Count = 0 Then Exit Sub
+        'If SMSHash.Count = 0 Then Exit Sub
 
-        Dim mysql As String = "Select * from SmsRegister"
-        Dim ds As DataSet = LoadSQL(mysql, "SmsRegister")
-        Dim dsNewRow As DataRow
-        dsNewRow = ds.Tables("SmsRegister").NewRow
-        With dsNewRow
-            .Item("Fname") = SMSHash("FNAME")
-            .Item("Lname") = SMSHash("LNAME")
-            .Item("Mname") = SMSHash("MNAME")
-            .Item("SMSNumber") = SMSHash("NUMBER")
-        End With
-        ds.Tables("SmsRegister").Rows.Add(dsNewRow)
-        SaveEntry(ds)
+        'Dim mysql As String = "Select * from SmsRegister"
+        'Dim ds As DataSet = LoadSQL(mysql, "SmsRegister")
+        'Dim dsNewRow As DataRow
+        'dsNewRow = ds.Tables("SmsRegister").NewRow
+        'With dsNewRow
+        '    .Item("Fname") = SMSHash("FNAME")
+        '    .Item("Lname") = SMSHash("LNAME")
+        '    .Item("Mname") = SMSHash("MNAME")
+        '    .Item("SMSNumber") = SMSHash("NUMBER")
+        'End With
+        'ds.Tables("SmsRegister").Rows.Add(dsNewRow)
+        'SaveEntry(ds)
 
-        MsgBox("Success")
-        SMSHash.Clear()
-    End Sub
+        'MsgBox("Success")
+        'SMSHash.Clear()
+        Return strArray
+    End Function
 
 
     Private Function isValidKey(ByVal Keys As String) As Boolean
-        Dim KeysCollection() As String = {"FNAME", "MNAME", "LNAME", "NUMBER"}
+        Dim KeysCollection() As String = {"FNAME", "MNAME", "LNAME", "NUMBER", "REG_KEY"}
 
         If KeysCollection.Contains(Keys) Then Return True
 
