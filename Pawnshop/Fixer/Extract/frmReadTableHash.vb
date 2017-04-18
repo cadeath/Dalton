b767423174f2ede38d414e531cd53294bf2476c9
marzxxx90 <junmar_honor@ymail.com>
2017-04-18 15:40:51 +0800

    Update

--- Pawnshop/Fixer/Extract/frmReadTableHash.vb
+++ Pawnshop/Fixer/Extract/frmReadTableHash.vb
@@ -17,10 +17,22 @@
         Dim ds As DataSet = LoadSQL(mysql)
 
         For Each dt In ds.Tables
-            For Each column In dt.Columns
-                'lbTableName.Items.Add(column.ColumnName)
-                 lvData .Columns.Add(New ColumnHeader() column.ColumnName)
+
+            For Each column As DataColumn In dt.Columns
+                Dim NewCH As New ColumnHeader
+
+                NewCH.Text = column.ColumnName
+                lvData.Columns.Add(NewCH)
+
+            Next
+            'Dim lv As ListViewItem = lvData.Items.Add()
+            For Each row As DataRow In dt.rows
+                For i As Integer = 0 To dt.row.count - 1
+                    Console.WriteLine("rows " & row.Item(i))
+                Next
+
             Next
         Next
+
     End Sub
 End Class
