2c4e55638faac2f881de376a5e7822ed6ddcb529
James Zerdick <ecjmaquic@gmail.com>
2017-04-24 15:20:50 +0800

    UPDATE

--- Pawnshop/AutoDeploy/My Project/AssemblyInfo.vb
+++ Pawnshop/AutoDeploy/My Project/AssemblyInfo.vb
@@ -9,7 +9,7 @@ Imports System.Runtime.InteropServices
 ' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("AutoDeploy")> 
-<Assembly: AssemblyDescription("Deployment Manager")> 
+<Assembly: AssemblyDescription("Deployment Manager v1.001")> 
 <Assembly: AssemblyCompany("Stealth Assassin")> 
 <Assembly: AssemblyProduct("deploy-mngr")> 
 <Assembly: AssemblyCopyright("Copyright ©  2017")> 
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("1.0.0.1")> 
+<Assembly: AssemblyFileVersion("1.0.0.1")> 
