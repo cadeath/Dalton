220c779967fc10afae6ed96d95673820256df2b5
marzxxx90 <junmar_honor@ymail.com>
2017-05-22 13:59:47 +0800

    Update

--- Pawnshop/Pawnshop/AutoPatch/db132.vb
+++ Pawnshop/Pawnshop/AutoPatch/db132.vb
@@ -8,6 +8,8 @@
         If Not isPatchable(ALLOWABLE_VERSION) Then Exit Sub
         Try
             Update_ItemMasterTbl()
+            OTPControl()
+
             Database_Update(LATEST_VERSION)
             Log_Report(String.Format("SYSTEM PATCHED UP FROM {0} TO {1}", ALLOWABLE_VERSION, LATEST_VERSION))
         Catch ex As Exception
@@ -18,5 +20,17 @@
     Private Sub Update_ItemMasterTbl()
         Dim myql As String = "ALTER TABLE ITEMMASTER ADD DISCOUNT NUMERIC(12, 2);"
         RunCommand(myql)
+    End Sub
+
+    Private Sub OTPControl()
+        Dim OTP As String = "CREATE TABLE OTPCONTROL ( "
+        OTP &= "OTPID SMALLINT NOT NULL, "
+        OTP &= "MODNAME VARCHAR(50) NOT NULL, "
+        OTP &= "OTPCODE VARCHAR(20) NOT NULL, "
+        OTP &= "APPNAME VARCHAR(150) NOT NULL, "
+        OTP &= "STATUS SMALLINT DEFAULT '1' NOT NULL);"
+
+        RunCommand(OTP)
+        AutoIncrement_ID("OTPCONTROL", "OTPID")
     End Sub
 End Module
