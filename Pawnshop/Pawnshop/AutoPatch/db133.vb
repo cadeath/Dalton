da31861f9837a6e163fe5d2c4b2006102914f698
Bladier <misionaellie@gmail.com>
2017-05-27 15:24:23 +0800

    UPDATE

--- Pawnshop/Pawnshop/AutoPatch/db133.vb
+++ Pawnshop/Pawnshop/AutoPatch/db133.vb
@@ -31,7 +31,7 @@
 
         createView = "	CREATE VIEW PAWN_LIST(	"
         createView &= vbCrLf & "	  PAWNID,	"
-        createView &= vbCrLf & "	PAWNTICKET,	"
+        createView &= vbCrLf & "	  PAWNTICKET,	"
         createView &= vbCrLf & "	  LOANDATE,	"
         createView &= vbCrLf & "	  MATUDATE,	"
         createView &= vbCrLf & "	  EXPIRYDATE,	"
@@ -62,13 +62,14 @@
         createView &= vbCrLf & "	AS	"
         createView &= vbCrLf & "	SELECT 	"
         createView &= vbCrLf & "	P.PAWNID, P.PAWNTICKET, P.LOANDATE, P.MATUDATE, P.EXPIRYDATE, P.AUCTIONDATE, 	"
-        createView &= vbCrLf & "	C.ID, C.FIRSTNAME || ' ' || C.LASTNAME || ' ' ||	"
+        createView &= vbCrLf & "	C.ID, C.FIRSTNAME || ' ' || C.LASTNAME || ' ' || 	"
         createView &= vbCrLf & "	CASE WHEN C.SUFFIX is Null THEN '' ELSE C.SUFFIX END AS CLIENT, 	"
-        createView &= vbCrLf & "		"
-        createView &= vbCrLf & "	CASE WHEN CHAR_LENGTH(PH.PHONENUMBER)=11 AND PH.ISPRIMARY = 1	"
-        createView &= vbCrLf & "	THEN PH.PHONENUMBER WHEN CHAR_LENGTH(PH.PHONENUMBER)=11 THEN PH.PHONENUMBER	"
-        createView &= vbCrLf & "	ELSE NULL END AS CONTACTNUMBER,	"
-        createView &= vbCrLf & "		"
+        createView &= vbCrLf & "	(SELECT (CASE WHEN (CHAR_LENGTH(PH.PHONENUMBER)=11)AND PH.ISPRIMARY = 1	"
+        createView &= vbCrLf & "	THEN PH.PHONENUMBER WHEN (CHAR_LENGTH(PH.PHONENUMBER)=11) THEN PH.PHONENUMBER	"
+        createView &= vbCrLf & "	ELSE PH.PHONENUMBER END)AS CONTACTNUMBER FROM KYC_PHONE PH  	"
+        createView &= vbCrLf & "	 WHERE 	"
+        createView &= vbCrLf & "	    C.ID = PH.CUSTID 	"
+        createView &= vbCrLf & "	    ORDER BY PH.ISPRIMARY DESC ROWS 1),	"
         createView &= vbCrLf & "	C.STREET1 || ' ' || C.CITY1 || ' ' || C.PROVINCE1 || ' ' || C.ZIP1 as FULLADDRESS, 	"
         createView &= vbCrLf & "	ITM.ITEMCLASS, CLASS.ITEMCATEGORY, P.DESCRIPTION, 	"
         createView &= vbCrLf & "	P.OLDTICKET, P.ORNUM, P.ORDATE, P.PRINCIPAL, P.DELAYINTEREST, P.ADVINT, P.SERVICECHARGE, 	"
@@ -77,8 +78,8 @@
         createView &= vbCrLf & "	FROM OPT P INNER JOIN KYC_CUSTOMERS C 	"
         createView &= vbCrLf & "	ON P.CLIENTID = C.ID INNER JOIN OPI ITM 	"
         createView &= vbCrLf & "	ON ITM.PAWNITEMID = P.PAWNITEMID INNER JOIN TBLITEM CLASS 	"
-        createView &= vbCrLf & "	ON CLASS.ITEMID = ITM.ITEMID LEFT JOIN TBL_GAMIT USR ON USR.USERID = P.APPRAISERID	"
-        createView &= vbCrLf & "	INNER JOIN KYC_PHONE PH ON PH.CUSTID=C.ID;	"
+        createView &= vbCrLf & "	ON CLASS.ITEMID = ITM.ITEMID LEFT JOIN TBL_GAMIT USR ON USR.USERID = P.APPRAISERID;	"
+
 
         RunCommand(dropView)
         RunCommand(createView)
