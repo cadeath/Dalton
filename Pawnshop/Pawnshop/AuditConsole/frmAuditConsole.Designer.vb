96382a4d0f2313fde6fa05e33aec855b990464c5
marzxxx90 <junmar_honor@ymail.com>
2017-06-10 10:24:07 +0800

    Added Audit Tracker

--- Pawnshop/Pawnshop/AuditConsole/frmAuditConsole.Designer.vb
+++ Pawnshop/Pawnshop/AuditConsole/frmAuditConsole.Designer.vb
@@ -34,33 +34,35 @@ Partial Class frmAuditConsole
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.MonVault = New System.Windows.Forms.MonthCalendar()
-        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
-        Me.txtQRURL = New System.Windows.Forms.TextBox()
-        Me.txtManual = New System.Windows.Forms.TextBox()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.btnGenerate = New System.Windows.Forms.Button()
-        Me.txtEmail = New System.Windows.Forms.TextBox()
-        Me.Label4 = New System.Windows.Forms.Label()
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
         Me.TabPage3 = New System.Windows.Forms.TabPage()
         Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.chkZeroOut = New System.Windows.Forms.CheckBox()
         Me.btnImport = New System.Windows.Forms.Button()
         Me.txtPath = New System.Windows.Forms.TextBox()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.btnBrowse = New System.Windows.Forms.Button()
         Me.ofdINV_AD = New System.Windows.Forms.OpenFileDialog()
-        Me.chkZeroOut = New System.Windows.Forms.CheckBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.lvItem = New System.Windows.Forms.ListView()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader5 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader6 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
-        Me.GroupBox3.SuspendLayout()
         Me.TabControl1.SuspendLayout()
         Me.TabPage1.SuspendLayout()
-        Me.TabPage2.SuspendLayout()
         Me.TabPage3.SuspendLayout()
         Me.GroupBox4.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
         Me.SuspendLayout()
         '
         'GroupBox1
@@ -174,96 +176,11 @@ Partial Class frmAuditConsole
         Me.MonVault.Name = "MonVault"
         Me.MonVault.TabIndex = 6
         '
-        'GroupBox3
-        '
-        Me.GroupBox3.Controls.Add(Me.txtQRURL)
-        Me.GroupBox3.Controls.Add(Me.txtManual)
-        Me.GroupBox3.Controls.Add(Me.Label6)
-        Me.GroupBox3.Controls.Add(Me.Label5)
-        Me.GroupBox3.Controls.Add(Me.btnGenerate)
-        Me.GroupBox3.Controls.Add(Me.txtEmail)
-        Me.GroupBox3.Controls.Add(Me.Label4)
-        Me.GroupBox3.Location = New System.Drawing.Point(6, 6)
-        Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(477, 155)
-        Me.GroupBox3.TabIndex = 5
-        Me.GroupBox3.TabStop = False
-        Me.GroupBox3.Text = "One-Time Password"
-        '
-        'txtQRURL
-        '
-        Me.txtQRURL.BackColor = System.Drawing.Color.White
-        Me.txtQRURL.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtQRURL.ForeColor = System.Drawing.SystemColors.WindowText
-        Me.txtQRURL.Location = New System.Drawing.Point(98, 73)
-        Me.txtQRURL.Name = "txtQRURL"
-        Me.txtQRURL.ReadOnly = True
-        Me.txtQRURL.Size = New System.Drawing.Size(373, 22)
-        Me.txtQRURL.TabIndex = 6
-        Me.txtQRURL.Text = "eskie@pgc-itdept.org"
-        '
-        'txtManual
-        '
-        Me.txtManual.BackColor = System.Drawing.Color.White
-        Me.txtManual.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtManual.Location = New System.Drawing.Point(98, 48)
-        Me.txtManual.Name = "txtManual"
-        Me.txtManual.ReadOnly = True
-        Me.txtManual.Size = New System.Drawing.Size(373, 22)
-        Me.txtManual.TabIndex = 5
-        Me.txtManual.Text = "eskie@pgc-itdept.org"
-        '
-        'Label6
-        '
-        Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(16, 78)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(32, 13)
-        Me.Label6.TabIndex = 4
-        Me.Label6.Text = "URL:"
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(16, 53)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(45, 13)
-        Me.Label5.TabIndex = 3
-        Me.Label5.Text = "Manual:"
-        '
-        'btnGenerate
-        '
-        Me.btnGenerate.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnGenerate.Location = New System.Drawing.Point(389, 101)
-        Me.btnGenerate.Name = "btnGenerate"
-        Me.btnGenerate.Size = New System.Drawing.Size(82, 44)
-        Me.btnGenerate.TabIndex = 2
-        Me.btnGenerate.Text = "&Generate"
-        Me.btnGenerate.UseVisualStyleBackColor = True
-        '
-        'txtEmail
-        '
-        Me.txtEmail.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtEmail.Location = New System.Drawing.Point(98, 20)
-        Me.txtEmail.Name = "txtEmail"
-        Me.txtEmail.Size = New System.Drawing.Size(373, 22)
-        Me.txtEmail.TabIndex = 1
-        Me.txtEmail.Text = "eskie@pgc-itdept.org"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(6, 25)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(73, 13)
-        Me.Label4.TabIndex = 0
-        Me.Label4.Text = "Email Address"
-        '
         'TabControl1
         '
         Me.TabControl1.Controls.Add(Me.TabPage1)
-        Me.TabControl1.Controls.Add(Me.TabPage2)
         Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
         Me.TabControl1.Location = New System.Drawing.Point(6, 5)
         Me.TabControl1.Name = "TabControl1"
         Me.TabControl1.SelectedIndex = 0
@@ -281,17 +198,6 @@ Partial Class frmAuditConsole
         Me.TabPage1.TabIndex = 0
         Me.TabPage1.Text = "Audit"
         Me.TabPage1.UseVisualStyleBackColor = True
-        '
-        'TabPage2
-        '
-        Me.TabPage2.Controls.Add(Me.GroupBox3)
-        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(489, 345)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "Access Code"
-        Me.TabPage2.UseVisualStyleBackColor = True
         '
         'TabPage3
         '
@@ -316,6 +222,16 @@ Partial Class frmAuditConsole
         Me.GroupBox4.Size = New System.Drawing.Size(477, 83)
         Me.GroupBox4.TabIndex = 0
         Me.GroupBox4.TabStop = False
+        '
+        'chkZeroOut
+        '
+        Me.chkZeroOut.AutoSize = True
+        Me.chkZeroOut.Location = New System.Drawing.Point(42, 52)
+        Me.chkZeroOut.Name = "chkZeroOut"
+        Me.chkZeroOut.Size = New System.Drawing.Size(115, 17)
+        Me.chkZeroOut.TabIndex = 4
+        Me.chkZeroOut.Text = "Inventory Zero Out"
+        Me.chkZeroOut.UseVisualStyleBackColor = True
         '
         'btnImport
         '
@@ -357,15 +273,83 @@ Partial Class frmAuditConsole
         '
         Me.ofdINV_AD.Filter = "Excel 2007|*.xlsx"
         '
-        'chkZeroOut
+        'TabPage2
         '
-        Me.chkZeroOut.AutoSize = True
-        Me.chkZeroOut.Location = New System.Drawing.Point(42, 52)
-        Me.chkZeroOut.Name = "chkZeroOut"
-        Me.chkZeroOut.Size = New System.Drawing.Size(115, 17)
-        Me.chkZeroOut.TabIndex = 4
-        Me.chkZeroOut.Text = "Inventory Zero Out"
-        Me.chkZeroOut.UseVisualStyleBackColor = True
+        Me.TabPage2.Controls.Add(Me.GroupBox3)
+        Me.TabPage2.Controls.Add(Me.lvItem)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Size = New System.Drawing.Size(489, 345)
+        Me.TabPage2.TabIndex = 3
+        Me.TabPage2.Text = "Audit Tracker"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'lvItem
+        '
+        Me.lvItem.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4, Me.ColumnHeader5, Me.ColumnHeader6})
+        Me.lvItem.FullRowSelect = True
+        Me.lvItem.GridLines = True
+        Me.lvItem.Location = New System.Drawing.Point(3, 58)
+        Me.lvItem.Name = "lvItem"
+        Me.lvItem.Size = New System.Drawing.Size(483, 283)
+        Me.lvItem.TabIndex = 0
+        Me.lvItem.UseCompatibleStateImageBehavior = False
+        Me.lvItem.View = System.Windows.Forms.View.Details
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.txtSearch)
+        Me.GroupBox3.Controls.Add(Me.btnSearch)
+        Me.GroupBox3.Location = New System.Drawing.Point(3, 3)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(483, 49)
+        Me.GroupBox3.TabIndex = 1
+        Me.GroupBox3.TabStop = False
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(402, 16)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearch.TabIndex = 0
+        Me.btnSearch.Text = "S&earch"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(6, 18)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(390, 20)
+        Me.txtSearch.TabIndex = 1
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "ItemCode"
+        Me.ColumnHeader1.Width = 72
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Description"
+        Me.ColumnHeader2.Width = 138
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Sold Date"
+        Me.ColumnHeader3.Width = 68
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Price"
+        Me.ColumnHeader4.Width = 63
+        '
+        'ColumnHeader5
+        '
+        Me.ColumnHeader5.Text = "Qty"
+        Me.ColumnHeader5.Width = 39
+        '
+        'ColumnHeader6
+        '
+        Me.ColumnHeader6.Text = "Remarks"
         '
         'frmAuditConsole
         '
@@ -382,14 +366,14 @@ Partial Class frmAuditConsole
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox2.ResumeLayout(False)
         Me.GroupBox2.PerformLayout()
-        Me.GroupBox3.ResumeLayout(False)
-        Me.GroupBox3.PerformLayout()
         Me.TabControl1.ResumeLayout(False)
         Me.TabPage1.ResumeLayout(False)
-        Me.TabPage2.ResumeLayout(False)
         Me.TabPage3.ResumeLayout(False)
         Me.GroupBox4.ResumeLayout(False)
         Me.GroupBox4.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -404,17 +388,8 @@ Partial Class frmAuditConsole
     Friend WithEvents btnVault As System.Windows.Forms.Button
     Friend WithEvents cboType As System.Windows.Forms.ComboBox
     Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
-    Friend WithEvents txtQRURL As System.Windows.Forms.TextBox
-    Friend WithEvents txtManual As System.Windows.Forms.TextBox
-    Friend WithEvents Label6 As System.Windows.Forms.Label
-    Friend WithEvents Label5 As System.Windows.Forms.Label
-    Friend WithEvents btnGenerate As System.Windows.Forms.Button
-    Friend WithEvents txtEmail As System.Windows.Forms.TextBox
-    Friend WithEvents Label4 As System.Windows.Forms.Label
     Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
     Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
-    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
     Friend WithEvents TabPage3 As System.Windows.Forms.TabPage
     Friend WithEvents GroupBox4 As System.Windows.Forms.GroupBox
     Friend WithEvents btnImport As System.Windows.Forms.Button
@@ -423,4 +398,15 @@ Partial Class frmAuditConsole
     Friend WithEvents btnBrowse As System.Windows.Forms.Button
     Friend WithEvents ofdINV_AD As System.Windows.Forms.OpenFileDialog
     Friend WithEvents chkZeroOut As System.Windows.Forms.CheckBox
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtSearch As System.Windows.Forms.TextBox
+    Friend WithEvents btnSearch As System.Windows.Forms.Button
+    Friend WithEvents lvItem As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader4 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader5 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader6 As System.Windows.Forms.ColumnHeader
 End Class
